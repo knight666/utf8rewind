606212c1b6010e7f4e0834ac72b83dad2af46325
Katja <knight666@gmail.com>
2014-04-17 17:41:31 +0200

    Added generated documentation.

---
+++ documentation/html/utf8rewind_8h.js
@@ -0,0 +1,22 @@
+var utf8rewind_8h =
+[
+    [ "UTF8_ERR_INVALID_CHARACTER", "utf8rewind_8h.html#aa3a118249a9ae08e152079f4989ca588", null ],
+    [ "UTF8_ERR_INVALID_DATA", "utf8rewind_8h.html#aabf4ed55c67112f79abe7275ee2ecc9b", null ],
+    [ "UTF8_ERR_NOT_ENOUGH_SPACE", "utf8rewind_8h.html#a2c80a4201b65d282db98489204f6d5d8", null ],
+    [ "UTF8_ERR_OUT_OF_RANGE", "utf8rewind_8h.html#a26c37b1e55b873bc49642319dc3d19e5", null ],
+    [ "UTF8_ERR_UNHANDLED_SURROGATE_PAIR", "utf8rewind_8h.html#aa0c5762ac11727331a3538f8e0a56104", null ],
+    [ "UTF8_ERR_UNMATCHED_HIGH_SURROGATE_PAIR", "utf8rewind_8h.html#a0661d820dd006e49adfbdf0c7a9dec14", null ],
+    [ "UTF8_ERR_UNMATCHED_LOW_SURROGATE_PAIR", "utf8rewind_8h.html#a892fe61a47f8258d88e292553a459a1c", null ],
+    [ "ucs2_t", "utf8rewind_8h.html#a367c7c085f27100ec4bbfc8f220aeaab", null ],
+    [ "unicode_t", "utf8rewind_8h.html#a1fe7d84695f910c23ceca80b12983323", null ],
+    [ "utf16_t", "utf8rewind_8h.html#a4dce96cad338d9281612277b2d80950c", null ],
+    [ "utf8charlen", "utf8rewind_8h.html#a4c456e1c3d8637bdf3f999aa0377b2f2", null ],
+    [ "utf8charvalid", "utf8rewind_8h.html#a9d19522cd09e2dff6fca8db6a3a5cacf", null ],
+    [ "utf8convertucs2", "utf8rewind_8h.html#a9c0b98a672e469ea9da31fac51fc321e", null ],
+    [ "utf8decode", "utf8rewind_8h.html#a4d0c2482832b035a189e96a0d3d80e4c", null ],
+    [ "utf8encode", "utf8rewind_8h.html#ab936e1848e79cdff95c834a294a7539c", null ],
+    [ "utf8len", "utf8rewind_8h.html#ab34ee783c503a651da04128ec8086b4b", null ],
+    [ "utf8seek", "utf8rewind_8h.html#a13d55ea3402d6d91cfb965f3605ec3eb", null ],
+    [ "utf8towc", "utf8rewind_8h.html#afe10f10be61afc896374cac318c8494d", null ],
+    [ "wctoutf8", "utf8rewind_8h.html#ac472a3dfa44e41bda08513860b61317d", null ]
+];
