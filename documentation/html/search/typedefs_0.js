dc2f570e756dac1e1a2408e0e4fd033b9dfce1c0
Katja <knight666+github@gmail.com>
2014-06-14 18:29:48 +0200

    Updated documentation due to changes in typedefs.

--- documentation/html/search/typedefs_0.js
+++ documentation/html/search/typedefs_0.js
@@ -1,6 +1,6 @@
 var searchData=
 [
-  ['ucs2_5ft',['ucs2_t',['../utf8rewind_8h.html#a367c7c085f27100ec4bbfc8f220aeaab',1,'utf8rewind.h']]],
-  ['unicode_5ft',['unicode_t',['../utf8rewind_8h.html#a1fe7d84695f910c23ceca80b12983323',1,'utf8rewind.h']]],
-  ['utf16_5ft',['utf16_t',['../utf8rewind_8h.html#a4dce96cad338d9281612277b2d80950c',1,'utf8rewind.h']]]
+  ['ucs2_5ft',['ucs2_t',['../utf8rewind_8h.html#aaed921fc64460bb8f0039ba06c5abd17',1,'utf8rewind.h']]],
+  ['unicode_5ft',['unicode_t',['../utf8rewind_8h.html#a5fc61747372b4165739ae5a52384468f',1,'utf8rewind.h']]],
+  ['utf16_5ft',['utf16_t',['../utf8rewind_8h.html#a1e9e182e040e6f408cfb7188eaafb48a',1,'utf8rewind.h']]]
 ];
