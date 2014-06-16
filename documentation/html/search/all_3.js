1dc1ec6d460a81ce9b5b68f1a1e198f1d7f9837f
Anne <knight666@gmail.com>
2014-06-16 08:28:04 +0200

    Updated documentation.

--- documentation/html/search/all_3.js
+++ documentation/html/search/all_3.js
@@ -1,4 +1,28 @@
 var searchData=
 [
-  ['wctoutf8',['wctoutf8',['../utf8rewind_8h.html#ac472a3dfa44e41bda08513860b61317d',1,'utf8rewind.h']]]
+  ['utf8rewind',['utf8rewind',['../index.html',1,'']]],
+  ['ucs2_5ft',['ucs2_t',['../utf8rewind_8h.html#aaed921fc64460bb8f0039ba06c5abd17',1,'utf8rewind.h']]],
+  ['unicode_5ft',['unicode_t',['../utf8rewind_8h.html#a5fc61747372b4165739ae5a52384468f',1,'utf8rewind.h']]],
+  ['utf16_5ft',['utf16_t',['../utf8rewind_8h.html#a1e9e182e040e6f408cfb7188eaafb48a',1,'utf8rewind.h']]],
+  ['utf8_5fbyte_5forder',['UTF8_BYTE_ORDER',['../group__configuration.html#ga4b27e4b4ad7452f9fc74eb65d69ef46a',1,'utf8rewind.h']]],
+  ['utf8_5fbyte_5forder_5fbig_5fendian',['UTF8_BYTE_ORDER_BIG_ENDIAN',['../group__configuration.html#ga59fcd7b30d89adce1e6cb9fd37fac02e',1,'utf8rewind.h']]],
+  ['utf8_5fbyte_5forder_5flittle_5fendian',['UTF8_BYTE_ORDER_LITTLE_ENDIAN',['../group__configuration.html#ga2d6e2dbaba1c22d506dde38154f579f1',1,'utf8rewind.h']]],
+  ['utf8_5ferr_5finvalid_5fcharacter',['UTF8_ERR_INVALID_CHARACTER',['../utf8rewind_8h.html#aa3a118249a9ae08e152079f4989ca588',1,'utf8rewind.h']]],
+  ['utf8_5ferr_5finvalid_5fdata',['UTF8_ERR_INVALID_DATA',['../utf8rewind_8h.html#aabf4ed55c67112f79abe7275ee2ecc9b',1,'utf8rewind.h']]],
+  ['utf8_5ferr_5fnot_5fenough_5fspace',['UTF8_ERR_NOT_ENOUGH_SPACE',['../utf8rewind_8h.html#a2c80a4201b65d282db98489204f6d5d8',1,'utf8rewind.h']]],
+  ['utf8_5ferr_5fout_5fof_5frange',['UTF8_ERR_OUT_OF_RANGE',['../utf8rewind_8h.html#a26c37b1e55b873bc49642319dc3d19e5',1,'utf8rewind.h']]],
+  ['utf8_5ferr_5funhandled_5fsurrogate_5fpair',['UTF8_ERR_UNHANDLED_SURROGATE_PAIR',['../utf8rewind_8h.html#aa0c5762ac11727331a3538f8e0a56104',1,'utf8rewind.h']]],
+  ['utf8_5ferr_5funmatched_5fhigh_5fsurrogate_5fpair',['UTF8_ERR_UNMATCHED_HIGH_SURROGATE_PAIR',['../utf8rewind_8h.html#a0661d820dd006e49adfbdf0c7a9dec14',1,'utf8rewind.h']]],
+  ['utf8_5ferr_5funmatched_5flow_5fsurrogate_5fpair',['UTF8_ERR_UNMATCHED_LOW_SURROGATE_PAIR',['../utf8rewind_8h.html#a892fe61a47f8258d88e292553a459a1c',1,'utf8rewind.h']]],
+  ['utf8charlen',['utf8charlen',['../utf8rewind_8h.html#a4c456e1c3d8637bdf3f999aa0377b2f2',1,'utf8rewind.h']]],
+  ['utf8charvalid',['utf8charvalid',['../utf8rewind_8h.html#a9d19522cd09e2dff6fca8db6a3a5cacf',1,'utf8rewind.h']]],
+  ['utf8convertucs2',['utf8convertucs2',['../utf8rewind_8h.html#a8734b4622d2f5eef17b9ca1e435fe99b',1,'utf8rewind.h']]],
+  ['utf8convertucs2_5fbe',['utf8convertucs2_be',['../utf8rewind_8h.html#a209e35301f1a5b0374a82823b84c1fee',1,'utf8rewind.h']]],
+  ['utf8convertucs2_5fle',['utf8convertucs2_le',['../utf8rewind_8h.html#af670b5db47da7257ab6d6c2ab10edee4',1,'utf8rewind.h']]],
+  ['utf8decode',['utf8decode',['../utf8rewind_8h.html#a4d0c2482832b035a189e96a0d3d80e4c',1,'utf8rewind.h']]],
+  ['utf8encode',['utf8encode',['../utf8rewind_8h.html#ab936e1848e79cdff95c834a294a7539c',1,'utf8rewind.h']]],
+  ['utf8len',['utf8len',['../utf8rewind_8h.html#aa6a3ebdecf49591cff736ccf054c048e',1,'utf8rewind.h']]],
+  ['utf8rewind_2eh',['utf8rewind.h',['../utf8rewind_8h.html',1,'']]],
+  ['utf8seek',['utf8seek',['../utf8rewind_8h.html#a13d55ea3402d6d91cfb965f3605ec3eb',1,'utf8rewind.h']]],
+  ['utf8towc',['utf8towc',['../utf8rewind_8h.html#afe10f10be61afc896374cac318c8494d',1,'utf8rewind.h']]]
 ];
