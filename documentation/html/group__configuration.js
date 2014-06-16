07dc50fd51b04ac4e410c877a63d8060fdadfb8c
Anne <knight666+github@gmail.com>
2014-06-16 08:28:04 +0200

    Updated documentation.

---
+++ documentation/html/group__configuration.js
@@ -0,0 +1,6 @@
+var group__configuration =
+[
+    [ "UTF8_BYTE_ORDER", "group__configuration.html#ga4b27e4b4ad7452f9fc74eb65d69ef46a", null ],
+    [ "UTF8_BYTE_ORDER_BIG_ENDIAN", "group__configuration.html#ga59fcd7b30d89adce1e6cb9fd37fac02e", null ],
+    [ "UTF8_BYTE_ORDER_LITTLE_ENDIAN", "group__configuration.html#ga2d6e2dbaba1c22d506dde38154f579f1", null ]
+];
--- documentation/html/search/functions_0.js
+++ documentation/html/search/functions_0.js
@@ -2,7 +2,8 @@ var searchData=
 [
   ['utf8charlen',['utf8charlen',['../utf8rewind_8h.html#a4c456e1c3d8637bdf3f999aa0377b2f2',1,'utf8rewind.h']]],
   ['utf8charvalid',['utf8charvalid',['../utf8rewind_8h.html#a9d19522cd09e2dff6fca8db6a3a5cacf',1,'utf8rewind.h']]],
-  ['utf8convertucs2',['utf8convertucs2',['../utf8rewind_8h.html#a9c0b98a672e469ea9da31fac51fc321e',1,'utf8rewind.h']]],
+  ['utf8convertucs2_5fbe',['utf8convertucs2_be',['../utf8rewind_8h.html#a209e35301f1a5b0374a82823b84c1fee',1,'utf8rewind.h']]],
+  ['utf8convertucs2_5fle',['utf8convertucs2_le',['../utf8rewind_8h.html#af670b5db47da7257ab6d6c2ab10edee4',1,'utf8rewind.h']]],
   ['utf8decode',['utf8decode',['../utf8rewind_8h.html#a4d0c2482832b035a189e96a0d3d80e4c',1,'utf8rewind.h']]],
   ['utf8encode',['utf8encode',['../utf8rewind_8h.html#ab936e1848e79cdff95c834a294a7539c',1,'utf8rewind.h']]],
   ['utf8len',['utf8len',['../utf8rewind_8h.html#aa6a3ebdecf49591cff736ccf054c048e',1,'utf8rewind.h']]],
