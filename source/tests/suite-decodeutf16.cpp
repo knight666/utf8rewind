9b47e2ec38b56692c3ac68570b5c54cb65084845
Anne <knight666@gmail.com>
2014-06-26 17:42:28 +0200

    suite-decodeutf16: Added length tests. suite-decodeutf16: Added missing string tests.

--- source/tests/suite-decodeutf16.cpp
+++ source/tests/suite-decodeutf16.cpp
@@ -56,6 +56,34 @@ TEST(DecodeUtf16, StringBufferTooSmall)
 	EXPECT_EQ(0xDFFF, o[3]);
 }
 
+TEST(DecodeUtf16, StringDataSizeUnder)
+{
+	const char* i = "p\xC3\xA5 xylofon";
+	const size_t s = 256;
+	utf16_t o[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(6, utf8decodeutf16(i, 4, o, s * sizeof(utf16_t), &errors));
+	EXPECT_EQ(0, errors);
+	EXPECT_EQ('p', o[0]);
+	EXPECT_EQ(0x00E5, o[1]);
+	EXPECT_EQ(' ', o[2]);
+}
+
+TEST(DecodeUtf16, StringDataSizeOver)
+{
+	const char* i = "\xCE\x93\xCE\xB1\xCE\xB6";
+	const size_t s = 256;
+	utf16_t o[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(6, utf8decodeutf16(i, 18, o, s * sizeof(utf16_t), &errors));
+	EXPECT_EQ(0, errors);
+	EXPECT_EQ(0x0393, o[0]);
+	EXPECT_EQ(0x03B1, o[1]);
+	EXPECT_EQ(0x03B6, o[2]);
+}
+
 TEST(DecodeUtf16, Ascii)
 {
 	const char* i = "k";
@@ -319,4 +347,147 @@ TEST(DecodeUtf16, SurrogatePairNotEnoughSpace)
 	EXPECT_EQ(0, utf8decodeutf16(i, strlen(i), o, s * sizeof(utf16_t), &errors));
 	EXPECT_EQ(UTF8_ERR_NOT_ENOUGH_SPACE, errors);
 	EXPECT_EQ(0x0000, o[0]);
+}
+
+TEST(DecodeUtf16, LengthAscii)
+{
+	const char* i = "I";
+	int32_t errors = 0;
+
+	EXPECT_EQ(2, utf8decodeutf16(i, strlen(i), nullptr, 0, &errors));
+	EXPECT_EQ(0, errors);
+}
+
+TEST(DecodeUtf16, LengthAsciiFirst)
+{
+	const char* i = "\0";
+	int32_t errors = 0;
+
+	EXPECT_EQ(0, utf8decodeutf16(i, 1, nullptr, 0, &errors));
+	EXPECT_EQ(0, errors);
+}
+
+TEST(DecodeUtf16, LengthAsciiLast)
+{
+	const char* i = "\x7F";
+	int32_t errors = 0;
+
+	EXPECT_EQ(2, utf8decodeutf16(i, 1, nullptr, 0, &errors));
+	EXPECT_EQ(0, errors);
+}
+
+TEST(DecodeUtf16, LengthTwoBytes)
+{
+	const char* i = "\xCB\x9B";
+	int32_t errors = 0;
+
+	EXPECT_EQ(2, utf8decodeutf16(i, strlen(i), nullptr, 0, &errors));
+	EXPECT_EQ(0, errors);
+}
+
+TEST(DecodeUtf16, LengthTwoBytesFirst)
+{
+	const char* i = "\xC2\x80";
+	int32_t errors = 0;
+
+	EXPECT_EQ(2, utf8decodeutf16(i, strlen(i), nullptr, 0, &errors));
+	EXPECT_EQ(0, errors);
+}
+
+TEST(DecodeUtf16, LengthTwoBytesLast)
+{
+	const char* i = "\xDF\xBF";
+	int32_t errors = 0;
+
+	EXPECT_EQ(2, utf8decodeutf16(i, strlen(i), nullptr, 0, &errors));
+	EXPECT_EQ(0, errors);
+}
+
+TEST(DecodeUtf16, LengthTwoBytesNotEnoughData)
+{
+	const char* i = "\xC8";
+	int32_t errors = 0;
+
+	EXPECT_EQ(0, utf8decodeutf16(i, strlen(i), nullptr, 0, &errors));
+	EXPECT_EQ(UTF8_ERR_INVALID_DATA, errors);
+}
+
+TEST(DecodeUtf16, LengthThreeBytes)
+{
+	const char* i = "\xE0\xB4\x8B";
+	int32_t errors = 0;
+
+	EXPECT_EQ(2, utf8decodeutf16(i, strlen(i), nullptr, 0, &errors));
+	EXPECT_EQ(0, errors);
+}
+
+TEST(DecodeUtf16, LengthThreeBytesFirst)
+{
+	const char* i = "\xE0\xA0\x80";
+	int32_t errors = 0;
+
+	EXPECT_EQ(2, utf8decodeutf16(i, strlen(i), nullptr, 0, &errors));
+	EXPECT_EQ(0, errors);
+}
+
+TEST(DecodeUtf16, LengthThreeBytesLast)
+{
+	const char* i = "\xEF\xBF\xBF";
+	int32_t errors = 0;
+
+	EXPECT_EQ(2, utf8decodeutf16(i, strlen(i), nullptr, 0, &errors));
+	EXPECT_EQ(0, errors);
+}
+
+TEST(DecodeUtf16, LengthThreeBytesNotEnoughData)
+{
+	const char* i = "\xE0\xB4";
+	int32_t errors = 0;
+
+	EXPECT_EQ(0, utf8decodeutf16(i, strlen(i), nullptr, 0, &errors));
+	EXPECT_EQ(UTF8_ERR_INVALID_DATA, errors);
+}
+
+TEST(DecodeUtf16, LengthFourBytes)
+{
+	const char* i = "\xF0\x90\xB0\xAC";
+	int32_t errors = 0;
+
+	EXPECT_EQ(4, utf8decodeutf16(i, strlen(i), nullptr, 0, &errors));
+	EXPECT_EQ(0, errors);
+}
+
+TEST(DecodeUtf16, LengthFourBytesFirst)
+{
+	const char* i = "\xF0\x90\x80\x80";
+	int32_t errors = 0;
+
+	EXPECT_EQ(4, utf8decodeutf16(i, strlen(i), nullptr, 0, &errors));
+	EXPECT_EQ(0, errors);
+}
+
+TEST(DecodeUtf16, LengthFourBytesLast)
+{
+	const char* i = "\xF4\x80\x83\xBF";
+	int32_t errors = 0;
+
+	EXPECT_EQ(4, utf8decodeutf16(i, strlen(i), nullptr, 0, &errors));
+	EXPECT_EQ(0, errors);
+}
+
+TEST(DecodeUtf16, LengthFourBytesNotEnoughData)
+{
+	const char* i = "\xF0\x90\xB1";
+	int32_t errors = 0;
+
+	EXPECT_EQ(0, utf8decodeutf16(i, strlen(i), nullptr, 0, &errors));
+	EXPECT_EQ(UTF8_ERR_INVALID_DATA, errors);
+}
+
+TEST(DecodeUtf16, LengthNotEnoughData)
+{
+	int32_t errors = 0;
+
+	EXPECT_EQ(0, utf8decodeutf16(nullptr, 1, nullptr, 0, &errors));
+	EXPECT_EQ(UTF8_ERR_INVALID_DATA, errors);
 }
