b99dc82a3e1d2c5d2b389189ccf0900fe460975e
Anne <knight666@gmail.com>
2014-07-03 08:26:19 +0200

    Updated documentation.

--- documentation/html/search/all_3.js
+++ documentation/html/search/all_3.js
@@ -17,8 +17,6 @@ var searchData=
   ['utf8_5fwchar_5futf16',['UTF8_WCHAR_UTF16',['../group__configuration.html#gaf41b9c54e9a94ae8ef41a8c17e7c2d51',1,'utf8rewind.h']]],
   ['utf8charlen',['utf8charlen',['../utf8rewind_8h.html#a46a4b4215a32429f0726dd0dffb6e718',1,'utf8rewind.h']]],
   ['utf8charvalid',['utf8charvalid',['../utf8rewind_8h.html#a548f6f67979ffd8576a6f45b2a3455e1',1,'utf8rewind.h']]],
-  ['utf8convertucs2',['utf8convertucs2',['../utf8rewind_8h.html#ac08ff5b9be438e67b8e0af6a4c56543f',1,'utf8rewind.h']]],
-  ['utf8decode',['utf8decode',['../utf8rewind_8h.html#a33dcf979fa8ab17d40c0b7c7645a5dfa',1,'utf8rewind.h']]],
   ['utf8len',['utf8len',['../utf8rewind_8h.html#aa6a3ebdecf49591cff736ccf054c048e',1,'utf8rewind.h']]],
   ['utf8rewind_2eh',['utf8rewind.h',['../utf8rewind_8h.html',1,'']]],
   ['utf8seek',['utf8seek',['../utf8rewind_8h.html#a13d55ea3402d6d91cfb965f3605ec3eb',1,'utf8rewind.h']]],
--- documentation/html/search/functions_0.js
+++ documentation/html/search/functions_0.js
@@ -4,8 +4,6 @@ var searchData=
   ['utf32toutf8',['utf32toutf8',['../utf8rewind_8h.html#a8b5f39b5fe6fd55a6fc40dd54eaa8a80',1,'utf8rewind.h']]],
   ['utf8charlen',['utf8charlen',['../utf8rewind_8h.html#a46a4b4215a32429f0726dd0dffb6e718',1,'utf8rewind.h']]],
   ['utf8charvalid',['utf8charvalid',['../utf8rewind_8h.html#a548f6f67979ffd8576a6f45b2a3455e1',1,'utf8rewind.h']]],
-  ['utf8convertucs2',['utf8convertucs2',['../utf8rewind_8h.html#ac08ff5b9be438e67b8e0af6a4c56543f',1,'utf8rewind.h']]],
-  ['utf8decode',['utf8decode',['../utf8rewind_8h.html#a33dcf979fa8ab17d40c0b7c7645a5dfa',1,'utf8rewind.h']]],
   ['utf8len',['utf8len',['../utf8rewind_8h.html#aa6a3ebdecf49591cff736ccf054c048e',1,'utf8rewind.h']]],
   ['utf8seek',['utf8seek',['../utf8rewind_8h.html#a13d55ea3402d6d91cfb965f3605ec3eb',1,'utf8rewind.h']]],
   ['utf8toutf16',['utf8toutf16',['../utf8rewind_8h.html#a384129f817fbbcc7cad6c08b2db354f5',1,'utf8rewind.h']]],
