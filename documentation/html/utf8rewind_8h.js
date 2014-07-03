3c478b1101b3e1cf726b91daeacc79a752c65ea3
Anne <knight666+github@gmail.com>
2014-07-03 08:26:19 +0200

    Updated documentation.

--- documentation/html/utf8rewind_8h.js
+++ documentation/html/utf8rewind_8h.js
@@ -16,8 +16,6 @@ var utf8rewind_8h =
     [ "utf32toutf8", "utf8rewind_8h.html#a8b5f39b5fe6fd55a6fc40dd54eaa8a80", null ],
     [ "utf8charlen", "utf8rewind_8h.html#a46a4b4215a32429f0726dd0dffb6e718", null ],
     [ "utf8charvalid", "utf8rewind_8h.html#a548f6f67979ffd8576a6f45b2a3455e1", null ],
-    [ "utf8convertucs2", "utf8rewind_8h.html#ac08ff5b9be438e67b8e0af6a4c56543f", null ],
-    [ "utf8decode", "utf8rewind_8h.html#a33dcf979fa8ab17d40c0b7c7645a5dfa", null ],
     [ "utf8len", "utf8rewind_8h.html#aa6a3ebdecf49591cff736ccf054c048e", null ],
     [ "utf8seek", "utf8rewind_8h.html#a13d55ea3402d6d91cfb965f3605ec3eb", null ],
     [ "utf8toutf16", "utf8rewind_8h.html#a384129f817fbbcc7cad6c08b2db354f5", null ],
