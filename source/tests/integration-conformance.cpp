a08dff5cb041ac4afb0a910315eff651bf2d91c4
Anne <knight666+github@gmail.com>
2014-08-19 08:24:36 +0200

    integration-conformance: Refactored reading of data into memory.

--- source/tests/integration-conformance.cpp
+++ source/tests/integration-conformance.cpp
@@ -31,6 +31,37 @@ protected:
 		file.read(input, input_size);
 
 		file.close();
+
+		// output has to be stitched together due to null character in data
+
+		size_t decoded_size = 0;
+
+		output_size = 80624;
+		output = new unicode_t[output_size + 1];
+
+		const char* src = input;
+		size_t src_size = input_size;
+		unicode_t* dst = output;
+		size_t dst_size = output_size;
+
+		decoded_size = utf8toutf32(src, src_size, nullptr, 0, &errors);
+		ASSERT_EQ(16436, decoded_size);
+		ASSERT_EQ(0, errors);
+
+		utf8toutf32(src, src_size, dst, dst_size, &errors);
+		ASSERT_EQ(0, errors);
+
+		src += 4118;
+		src_size -= 4118;
+		dst += decoded_size / sizeof(unicode_t);
+		dst_size -= decoded_size;
+
+		decoded_size = utf8toutf32(src, src_size, nullptr, 0, &errors);
+		ASSERT_EQ(64188, decoded_size);
+		ASSERT_EQ(0, errors);
+
+		utf8toutf32(src, src_size, dst, dst_size, &errors);
+		ASSERT_EQ(0, errors);
 	}
 
 	void TearDown()
@@ -46,82 +77,18 @@ protected:
 		}
 	}
 
-	void ReadSection(size_t position, size_t length)
-	{
-		ASSERT_TRUE(position < 20377 && position + length < 20377);
-
-		size_t decoded_size = utf8towide(input + position, length, nullptr, 0, &errors);
-		ASSERT_TRUE(decoded_size > 0);
-		ASSERT_EQ(0, errors);
-
-		if (output != nullptr)
-		{
-			delete [] output;
-		}
-
-		size_t output_length = decoded_size / sizeof(wchar_t);
-
-		output = new wchar_t[output_length + 1];
-		output[output_length] = 0;
-
-		utf8towide(input + position, length, output, decoded_size, &errors);
-		ASSERT_EQ(0, errors);
-	}
-
-	void ReadCodepoint(size_t position, size_t length)
-	{
-		ASSERT_TRUE(position < 20377 && position + length < 20377);
-
-		codepoint = 0;
-
-		size_t decoded_size = utf8toutf32(input + position, length, &codepoint, sizeof(unicode_t), &errors);
-		ASSERT_EQ(4, decoded_size);
-		ASSERT_EQ(0, errors);
-	}
-
 	char* input;
 	size_t input_size;
-	wchar_t* output;
-	unicode_t codepoint;
+	unicode_t* output;
+	size_t output_size;
 	std::fstream file;
 	int32_t errors;
 
 };
 
-TEST_F(ConformanceSuite, ConvertAll)
+TEST_F(ConformanceSuite, ReadTheEnd)
 {
-	// output has to be stitched together due to null character in data
-
-	size_t decoded_size = 0;
-
-	size_t total_size = 80624;
-	unicode_t* decoded = new unicode_t[total_size + 1];
-
-	const char* src = input;
-	size_t src_size = input_size;
-	unicode_t* dst = decoded;
-	size_t dst_size = total_size;
-
-	decoded_size = utf8toutf32(src, src_size, nullptr, 0, &errors);
-	ASSERT_EQ(16436, decoded_size);
-	ASSERT_EQ(0, errors);
-
-	utf8toutf32(src, src_size, dst, dst_size, &errors);
-	ASSERT_EQ(0, errors);
-
-	src += 4118;
-	src_size -= 4118;
-	dst += (decoded_size / sizeof(unicode_t));
-	dst_size -= decoded_size;
-
-	decoded_size = utf8toutf32(src, src_size, nullptr, 0, &errors);
-	ASSERT_EQ(64188, decoded_size);
-	ASSERT_EQ(0, errors);
-
-	utf8toutf32(src, src_size, dst, dst_size, &errors);
-	ASSERT_EQ(0, errors);
-
-	unicode_t* the_end = dst + 15967;
+	unicode_t* the_end = output + 20076;
 	EXPECT_EQ('T', the_end[0]);
 	EXPECT_EQ('H', the_end[1]);
 	EXPECT_EQ('E', the_end[2]);
@@ -129,7 +96,4 @@ TEST_F(ConformanceSuite, ConvertAll)
 	EXPECT_EQ('E', the_end[4]);
 	EXPECT_EQ('N', the_end[5]);
 	EXPECT_EQ('D', the_end[6]);
-
-	int i = 0;
-	//decoded[decoded_size] = 0;
 }
