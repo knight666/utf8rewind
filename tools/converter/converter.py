638aaa561d3d85336932e30ccb7d499093092531
Anne <knight666@gmail.com>
2014-10-28 16:21:09 +0100

    BinaryBlob: Class for converting Unicode data into a hashed string.

--- tools/converter/converter.py
+++ tools/converter/converter.py
@@ -1,5 +1,42 @@
 import argparse
+import re
 import libs.unicode
+
+MAX_LEGAL_UNICODE = 0x10FFFF
+REPLACEMENT_CHARACTER = 0xFFFD
+
+def codepointToUtf8(codepoint):
+	encoded_length = 0
+	result = ""
+	
+	if codepoint < 0x80:
+		encoded_length = 1
+	elif codepoint < 0x800:
+		encoded_length = 2
+	elif codepoint < 0x10000:
+		encoded_length = 3
+	elif codepoint <= MAX_LEGAL_UNICODE:
+		encoded_length = 4
+	else:
+		codepoint = REPLACEMENT_CHARACTER
+		encoded_length = 3
+	
+	if encoded_length == 1:
+		result = '\\x' + format(codepoint, '02x')
+	elif encoded_length == 2:
+		result += '\\x' + format((codepoint >>   6)         | 0xC0, '02x')
+		result += '\\x' + format((codepoint         & 0x3F) | 0x80, '02x')
+	elif encoded_length == 3:
+		result += '\\x' + format((codepoint  >> 12)         | 0xE0, '02x')
+		result += '\\x' + format(((codepoint >>  6) & 0x3F) | 0x80, '02x')
+		result += '\\x' + format((codepoint         & 0x3F) | 0x80, '02x')
+	elif encoded_length == 4:
+		result += '\\x' + format((codepoint  >> 18)         | 0xF0, '02x')
+		result += '\\x' + format((codepoint  >> 12)         | 0x80, '02x')
+		result += '\\x' + format(((codepoint >>  6) & 0x3F) | 0x80, '02x')
+		result += '\\x' + format((codepoint         & 0x3F) | 0x80, '02x')
+	
+	return result
 
 class Header:
 	def __init__(self, filename):
@@ -29,6 +66,40 @@ class Printer(libs.unicode.UnicodeVisitor):
 		result += " }"
 		print " - - " + result
 
+class BinaryBlob(libs.unicode.UnicodeVisitor):
+	def __init__(self):
+		self.blob = "\\0"
+		self.total = 0
+		self.offset = 1
+		self.hashed = dict()
+	
+	def visitEntry(self, entry):
+		for match in entry.matches:
+			if match <> None:
+				for group in match.groups():
+					if group <> None:
+						codepoint = int(group, 16)
+						converted = codepointToUtf8(codepoint) + "\\0"
+						if converted not in self.hashed:
+							character_matches = re.findall('\\\\x?[^\\\\]+', converted)
+							if character_matches:
+								offset = len(character_matches)
+							else:
+								offset = 0
+							
+							self.hashed[converted] = self.offset
+							self.offset += offset
+							self.blob += converted
+						else:
+							offset = self.hashed[converted]
+						print "codepoint " + hex(codepoint) + " offset " + str(self.hashed[converted])
+						self.total += 1
+	
+	def write(self):
+		print "total " + str(self.total)
+		print "hashed " + str(len(self.hashed))
+		print self.blob
+
 if __name__ == '__main__':
 	parser = argparse.ArgumentParser(description='Converts Unicode data files.')
 	parser.add_argument(
@@ -42,5 +113,6 @@ if __name__ == '__main__':
 	normalization.limiter = args.limiter
 	normalization.parse('data/NormalizationTest.txt')
 	
-	printer = Printer()
+	printer = BinaryBlob()
 	normalization.accept(printer)
+	printer.write()
