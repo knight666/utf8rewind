928f42821ddfec9b92d2699f5b80c625f3cd9398
Anne <knight666@gmail.com>
2014-10-29 08:20:28 +0100

    Moved codepointToUtf8 function to a separate module.

--- tools/converter/converter.py
+++ tools/converter/converter.py
@@ -3,42 +3,7 @@ import fileinput
 import re
 import sys
 import libs.unicode
-
-MAX_LEGAL_UNICODE = 0x10FFFF
-REPLACEMENT_CHARACTER = 0xFFFD
-
-def codepointToUtf8(codepoint):
-	encoded_length = 0
-	result = ""
-	
-	if codepoint < 0x80:
-		encoded_length = 1
-	elif codepoint < 0x800:
-		encoded_length = 2
-	elif codepoint < 0x10000:
-		encoded_length = 3
-	elif codepoint <= MAX_LEGAL_UNICODE:
-		encoded_length = 4
-	else:
-		codepoint = REPLACEMENT_CHARACTER
-		encoded_length = 3
-	
-	if encoded_length == 1:
-		result = '\\x' + format(codepoint, '02x')
-	elif encoded_length == 2:
-		result += '\\x' + format((codepoint >>   6)         | 0xC0, '02x')
-		result += '\\x' + format((codepoint         & 0x3F) | 0x80, '02x')
-	elif encoded_length == 3:
-		result += '\\x' + format((codepoint  >> 12)         | 0xE0, '02x')
-		result += '\\x' + format(((codepoint >>  6) & 0x3F) | 0x80, '02x')
-		result += '\\x' + format((codepoint         & 0x3F) | 0x80, '02x')
-	elif encoded_length == 4:
-		result += '\\x' + format((codepoint  >> 18)         | 0xF0, '02x')
-		result += '\\x' + format((codepoint  >> 12)         | 0x80, '02x')
-		result += '\\x' + format(((codepoint >>  6) & 0x3F) | 0x80, '02x')
-		result += '\\x' + format((codepoint         & 0x3F) | 0x80, '02x')
-	
-	return result
+import libs.utf8
 
 class Header:
 	def __init__(self, filename):
@@ -195,7 +160,7 @@ class BinaryBlob(libs.unicode.UnicodeVisitor):
 		for group in match:
 			if group <> None:
 				codepoint = int(group, 16)
-				converted = codepointToUtf8(codepoint)
+				converted = libs.utf8.codepointToUtf8(codepoint)
 				result += converted
 		result += "\\0"
 		
