1c1961277afc68caf6668010b4fe72ec7ee5426f
Anne <knight666@gmail.com>
2014-10-31 15:23:27 +0100

    Added source file for normalization functions.

--- source/normalization.c
+++ source/normalization.c
@@ -23,19 +23,9 @@
 	OTHER DEALINGS IN THE SOFTWARE.
 */
 
-#ifndef _UTF8NORMALIZATION_H_
-#define _UTF8NORMALIZATION_H_
+#include "normalization.h"
 
-#include "utf8rewind.h"
-
-typedef struct {
-	unicode_t codepoint;
-	ptrdiff_t offsetC;
-	ptrdiff_t offsetD;
-	ptrdiff_t offsetKC;
-	ptrdiff_t offsetKD;
-} CompositionEntry;
-
-CompositionEntry* findcomposition(unicode_t codepoint);
-
-#endif
+CompositionEntry* findcomposition(unicode_t codepoint)
+{
+	return 0;
+}
