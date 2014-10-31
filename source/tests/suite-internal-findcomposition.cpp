c9c7fe6eb7d68bf8a83063d68821170efb0bf609
Anne <knight666+github@gmail.com>
2014-10-31 20:54:44 +0100

    suite-internal-findcomposition: Added basic tests for binary search.

--- source/tests/suite-internal-findcomposition.cpp
+++ source/tests/suite-internal-findcomposition.cpp
@@ -4,10 +4,52 @@ extern "C" {
 	#include "../normalization.h"
 }
 
+TEST(FindComposition, FoundFirst)
+{
+	int32_t r = 0;
+	const CompositionEntry* e = findcomposition(0x000000A0, &r);
+
+	ASSERT_NE(nullptr, e);
+	EXPECT_EQ(FindResult_Found, r);
+	EXPECT_EQ(0x000000A0, e->codepoint);
+	EXPECT_EQ(0, e->offsetC);
+	EXPECT_EQ(0, e->offsetD);
+	EXPECT_EQ(0x1, e->offsetKC);
+	EXPECT_EQ(0x1, e->offsetKD);
+}
+
+TEST(FindComposition, FoundLast)
+{
+	int32_t r = 0;
+	const CompositionEntry* e = findcomposition(0x0002FA1D, &r);
+
+	ASSERT_NE(nullptr, e);
+	EXPECT_EQ(FindResult_Found, r);
+	EXPECT_EQ(0x0002FA1D, e->codepoint);
+	EXPECT_EQ(0x3007813, e->offsetC);
+	EXPECT_EQ(0x3007813, e->offsetD);
+	EXPECT_EQ(0x3007813, e->offsetKC);
+	EXPECT_EQ(0x3007813, e->offsetKD);
+}
+
+TEST(FindComposition, FoundPivot)
+{
+	int32_t r = 0;
+	const CompositionEntry* e = findcomposition(0x0000C3D6, &r);
+
+	ASSERT_NE(nullptr, e);
+	EXPECT_EQ(FindResult_Found, r);
+	EXPECT_EQ(0x0000C3D6, e->codepoint);
+	EXPECT_EQ(0, e->offsetC);
+	EXPECT_EQ(0x0200192A, e->offsetD);
+	EXPECT_EQ(0, e->offsetKC);
+	EXPECT_EQ(0x0200192A, e->offsetKD);
+}
+
 TEST(FindComposition, NotFound)
 {
 	int32_t r = 0;
-	CompositionEntry* e = findcomposition(0x00000918d, &r);
+	const CompositionEntry* e = findcomposition(0x00000918d, &r);
 
 	EXPECT_EQ(nullptr, e);
 	EXPECT_EQ(FindResult_Missing, r);
@@ -16,7 +58,7 @@ TEST(FindComposition, NotFound)
 TEST(FindComposition, NotFoundLowerBounds)
 {
 	int32_t r = 0;
-	CompositionEntry* e = findcomposition(0x00000001, &r);
+	const CompositionEntry* e = findcomposition(0x00000001, &r);
 
 	EXPECT_EQ(nullptr, e);
 	EXPECT_EQ(FindResult_OutOfBounds, r);
@@ -25,7 +67,7 @@ TEST(FindComposition, NotFoundLowerBounds)
 TEST(FindComposition, NotFoundUpperBounds)
 {
 	int32_t r = 0;
-	CompositionEntry* e = findcomposition(0x0002FA1E, &r);
+	const CompositionEntry* e = findcomposition(0x0002FA1E, &r);
 
 	EXPECT_EQ(nullptr, e);
 	EXPECT_EQ(FindResult_OutOfBounds, r);
@@ -34,7 +76,7 @@ TEST(FindComposition, NotFoundUpperBounds)
 TEST(FindComposition, InvalidCodepoint)
 {
 	int32_t r = 0;
-	CompositionEntry* e = findcomposition(0x00110000, &r);
+	const CompositionEntry* e = findcomposition(0x00110000, &r);
 
 	EXPECT_EQ(nullptr, e);
 	EXPECT_EQ(FindResult_InvalidCodepoint, r);
@@ -42,7 +84,7 @@ TEST(FindComposition, InvalidCodepoint)
 
 TEST(FindComposition, NoResult)
 {
-	CompositionEntry* e = findcomposition(0x00000DDD, nullptr);
+	const CompositionEntry* e = findcomposition(0x00000DDD, nullptr);
 
 	EXPECT_EQ(nullptr, e);
 }
