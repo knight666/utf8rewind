b31e8f4ba4315c5fbbfc48d9d5e7fa0b5e705b3f
Purrminator <knight666@gmail.com>
2014-11-02 20:05:20 +0100

    resolvedecomposition: Added tests for finding first and last offset.

--- source/tests/suite-internal-resolvedecomposition.cpp
+++ source/tests/suite-internal-resolvedecomposition.cpp
@@ -7,16 +7,43 @@ extern "C" {
 TEST(ResolveDecomposition, Found)
 {
 	int32_t r = 0;
-	const char* d = resolvedecomposition(0xf, &r);
+	const char* d = resolvedecomposition(0x0000000F, &r);
 
 	EXPECT_STREQ("\x33\x00\x20\xcc\x81", d);
+	EXPECT_EQ(FindResult_Found, r);
+}
+
+TEST(ResolveDecomposition, FoundFirst)
+{
+	int32_t r = 0;
+	const char* d = resolvedecomposition(0x00000001, &r);
+
+	EXPECT_STREQ("\x20", d);
+	EXPECT_EQ(FindResult_Found, r);
+}
+
+TEST(ResolveDecomposition, FoundLast)
+{
+	int32_t r = 0;
+	const char* d = resolvedecomposition(0x00007FF5, &r);
+
+	EXPECT_STREQ("\xE1\x84\x83\xE1\x85\xAE\xE1\x86\xAF", d);
+	EXPECT_EQ(FindResult_Found, r);
+}
+
+TEST(ResolveDecomposition, FoundWrongOffset)
+{
+	int32_t r = 0;
+	const char* d = resolvedecomposition(0x0000000E, &r);
+
+	EXPECT_STREQ("", d);
 	EXPECT_EQ(FindResult_Found, r);
 }
 
 TEST(ResolveDecomposition, OffsetZero)
 {
 	int32_t r = 0;
-	const char* d = resolvedecomposition(0, &r);
+	const char* d = resolvedecomposition(0x00000000, &r);
 
 	EXPECT_EQ(nullptr, d);
 	EXPECT_EQ(FindResult_Invalid, r);
