90fa848668f8d7709bf2e1e5c519150cd646b80d
Anne <knight666@gmail.com>
2014-11-03 08:15:38 +0100

    suite-internal-resolvedecomposition: Added tests for other pages.

--- source/tests/suite-internal-resolvedecomposition.cpp
+++ source/tests/suite-internal-resolvedecomposition.cpp
@@ -40,33 +40,6 @@ TEST(ResolveDecomposition, FoundWrongOffset)
 	EXPECT_EQ(FindResult_Found, r);
 }
 
-TEST(ResolveDecomposition, NextPageFirst)
-{
-	int32_t r = 0;
-	const char* d = resolvedecomposition(0x01000001, &r);
-
-	EXPECT_STREQ("\xe1\x84\x83\xe1\x85\xae\xe1\x86\xb0", d);
-	EXPECT_EQ(FindResult_Found, r);
-}
-
-TEST(ResolveDecomposition, NextPageLast)
-{
-	int32_t r = 0;
-	const char* d = resolvedecomposition(0x01007FED, &r);
-
-	EXPECT_STREQ("\xe1\x84\x89\xe1\x85\xa6\xe1\x86\xb9", d);
-	EXPECT_EQ(FindResult_Found, r);
-}
-
-TEST(ResolveDecomposition, NextPageInvalidIndex)
-{
-	int32_t r = 0;
-	const char* d = resolvedecomposition(0x21000001, &r);
-
-	EXPECT_STREQ(0, d);
-	EXPECT_EQ(FindResult_Invalid, r);
-}
-
 TEST(ResolveDecomposition, OffsetZero)
 {
 	int32_t r = 0;
@@ -83,6 +56,69 @@ TEST(ResolveDecomposition, OffsetOutOfBounds)
 
 	EXPECT_EQ(nullptr, d);
 	EXPECT_EQ(FindResult_OutOfBounds, r);
+}
+
+TEST(ResolveDecomposition, NextPageFoundFirst)
+{
+	int32_t r = 0;
+	const char* d = resolvedecomposition(0x01000001, &r);
+
+	EXPECT_STREQ("\xe1\x84\x83\xe1\x85\xae\xe1\x86\xb0", d);
+	EXPECT_EQ(FindResult_Found, r);
+}
+
+TEST(ResolveDecomposition, NextPageFoundLast)
+{
+	int32_t r = 0;
+	const char* d = resolvedecomposition(0x01007FED, &r);
+
+	EXPECT_STREQ("\xe1\x84\x89\xe1\x85\xa6\xe1\x86\xb9", d);
+	EXPECT_EQ(FindResult_Found, r);
+}
+
+TEST(ResolveDecomposition, NetPageFoundWrongOffset)
+{
+	int32_t r = 0;
+	const char* d = resolvedecomposition(0x02000014, &r);
+
+	EXPECT_STREQ("", d);
+	EXPECT_EQ(FindResult_Found, r);
+}
+
+TEST(ResolveDecomposition, NextPageOffsetZero)
+{
+	int32_t r = 0;
+	const char* d = resolvedecomposition(0x02000000, &r);
+
+	EXPECT_EQ(nullptr, d);
+	EXPECT_EQ(FindResult_Invalid, r);
+}
+
+TEST(ResolveDecomposition, NextPageOffsetOutOfBounds)
+{
+	int32_t r = 0;
+	const char* d = resolvedecomposition(0x0118A722, &r);
+
+	EXPECT_EQ(nullptr, d);
+	EXPECT_EQ(FindResult_OutOfBounds, r);
+}
+
+TEST(ResolveDecomposition, LastPage)
+{
+	int32_t r = 0;
+	const char* d = resolvedecomposition(0x03000001, &r);
+
+	EXPECT_STREQ("\xe1\x84\x8e\xe1\x85\xb4\xe1\x86\xa9", d);
+	EXPECT_EQ(FindResult_Found, r);
+}
+
+TEST(ResolveDecomposition, InvalidPage)
+{
+	int32_t r = 0;
+	const char* d = resolvedecomposition(0x21000001, &r);
+
+	EXPECT_STREQ(0, d);
+	EXPECT_EQ(FindResult_Invalid, r);
 }
 
 TEST(ResolveDecomposition, NoResult)
