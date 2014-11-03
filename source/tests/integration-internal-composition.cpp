539e1a512cc1c876dc94e019cbd1c39e0d03d271
Anne <knight666@gmail.com>
2014-11-03 17:41:17 +0100

    integration-internal-composition: Added tests for canonical composites.

--- source/tests/integration-internal-composition.cpp
+++ source/tests/integration-internal-composition.cpp
@@ -207,3 +207,133 @@ TEST(Composition, SingletonOhm)
 		delete u;
 	}
 }
+
+TEST(Composition, CanonicalCompositeAWithRing)
+{
+	int32_t errors = 0;
+
+	const CompositionEntry* c = findcomposition(0xC5, &errors);
+	ASSERT_NE(nullptr, c);
+	EXPECT_EQ(0, errors);
+
+	// NFC
+
+	EXPECT_EQ(0, c->offsetC);
+
+	// NFD
+
+	{
+		const char* d = resolvedecomposition(c->offsetD, &errors);
+		ASSERT_NE(nullptr, d);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_STREQ("\x41\xCC\x8A", d);
+
+		unicode_t* u = nullptr;
+		size_t ul = utf8toutf32(d, strlen(d), nullptr, 0, &errors);
+		EXPECT_EQ(0, errors);
+
+		u = new unicode_t[ul / sizeof(unicode_t)];
+		utf8toutf32(d, strlen(d), u, ul, &errors);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_EQ(2 * sizeof(unicode_t), ul);
+		EXPECT_EQ(0x00000041, u[0]);
+		EXPECT_EQ(0x0000030A, u[1]);
+
+		delete u;
+	}
+
+	// NFKC
+
+	EXPECT_EQ(0, c->offsetC);
+
+	// NFKD
+
+	{
+		const char* d = resolvedecomposition(c->offsetKD, &errors);
+		ASSERT_NE(nullptr, d);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_STREQ("\x41\xCC\x8A", d);
+
+		unicode_t* u = nullptr;
+		size_t ul = utf8toutf32(d, strlen(d), nullptr, 0, &errors);
+		EXPECT_EQ(0, errors);
+
+		u = new unicode_t[ul / sizeof(unicode_t)];
+		utf8toutf32(d, strlen(d), u, ul, &errors);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_EQ(2 * sizeof(unicode_t), ul);
+		EXPECT_EQ(0x00000041, u[0]);
+		EXPECT_EQ(0x0000030A, u[1]);
+
+		delete u;
+	}
+}
+
+TEST(Composition, CanonicalCompositeOWithCircumflex)
+{
+	int32_t errors = 0;
+
+	const CompositionEntry* c = findcomposition(0xF4, &errors);
+	ASSERT_NE(nullptr, c);
+	EXPECT_EQ(0, errors);
+
+	// NFC
+
+	EXPECT_EQ(0, c->offsetC);
+
+	// NFD
+
+	{
+		const char* d = resolvedecomposition(c->offsetD, &errors);
+		ASSERT_NE(nullptr, d);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_STREQ("\x6F\xCC\x82", d);
+
+		unicode_t* u = nullptr;
+		size_t ul = utf8toutf32(d, strlen(d), nullptr, 0, &errors);
+		EXPECT_EQ(0, errors);
+
+		u = new unicode_t[ul / sizeof(unicode_t)];
+		utf8toutf32(d, strlen(d), u, ul, &errors);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_EQ(2 * sizeof(unicode_t), ul);
+		EXPECT_EQ(0x0000006F, u[0]);
+		EXPECT_EQ(0x00000302, u[1]);
+
+		delete u;
+	}
+
+	// NFKC
+
+	EXPECT_EQ(0, c->offsetC);
+
+	// NFKD
+
+	{
+		const char* d = resolvedecomposition(c->offsetKD, &errors);
+		ASSERT_NE(nullptr, d);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_STREQ("\x6F\xCC\x82", d);
+
+		unicode_t* u = nullptr;
+		size_t ul = utf8toutf32(d, strlen(d), nullptr, 0, &errors);
+		EXPECT_EQ(0, errors);
+
+		u = new unicode_t[ul / sizeof(unicode_t)];
+		utf8toutf32(d, strlen(d), u, ul, &errors);
+		EXPECT_EQ(0, errors);
+
+		EXPECT_EQ(2 * sizeof(unicode_t), ul);
+		EXPECT_EQ(0x0000006F, u[0]);
+		EXPECT_EQ(0x00000302, u[1]);
+
+		delete u;
+	}
+}
