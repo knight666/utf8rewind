cec34e596c47afcb601112b3c3bd9744e0a88a8a
Anne <knight666+github@gmail.com>
2014-11-04 17:24:30 +0100

    Added UnicodeMapping struct for codepoint mapping.

--- source/normalization.h
+++ source/normalization.h
@@ -28,6 +28,144 @@
 
 #include "utf8rewind.h"
 
+enum CodepointLabel
+{
+	CodepointLabel_Reserved,
+	CodepointLabel_NonCharacter,
+	CodepointLabel_Control,
+	CodepointLabel_PrivateUse,
+	CodepointLabel_Surrogate
+};
+
+enum GeneralCategory
+{
+	GeneralCategory_UppercaseLetter,
+	GeneralCategory_LowercaseLetter,
+	GeneralCategory_TitlecaseLetter,
+	GeneralCategory_ModifierLetter,
+	GeneralCategory_OtherLetter,
+	GeneralCategory_NonspacingMark,
+	GeneralCategory_SpacingMark,
+	GeneralCategory_EnclosingMark,
+	GeneralCategory_DecimalNumber,
+	GeneralCategory_LetterNumber,
+	GeneralCategory_OtherNumber,
+	GeneralCategory_ConnectorPunctuation,
+	GeneralCategory_OpenPunctuation,
+	GeneralCategory_ClosePunctuation,
+	GeneralCategory_InitialPunctuation,
+	GeneralCategory_OtherPunctuation,
+	GeneralCategory_MathSymbol,
+	GeneralCategory_CurrencySymbol,
+	GeneralCategory_ModifierSymbol,
+	GeneralCategory_OtherSymbol,
+	GeneralCategory_SpaceSeparator,
+	GeneralCategory_LineSeparator,
+	GeneralCategory_ParagraphSeparator,
+	GeneralCategory_Control,
+	GeneralCategory_Format,
+	GeneralCategory_Surrogate,
+	GeneralCategory_PrivateUse,
+	GeneralCategory_Unassigned
+};
+
+enum CanonicalCombiningClass
+{
+	CanonicalCombiningClass_NotReordered,
+	CanonicalCombiningClass_Overlay,
+	CanonicalCombiningClass_Nukta,
+	CanonicalCombiningClass_KanaVoicing,
+	CanonicalCombiningClass_Virama,
+	CanonicalCombiningClass_FixedPositionStart,
+	CanonicalCombiningClass_FixedPositionEnd,
+	CanonicalCombiningClass_AttachedBelowLeft,
+	CanonicalCombiningClass_AttachedBelow,
+	CanonicalCombiningClass_AttachedTopRight,
+	CanonicalCombiningClass_AttachedLeft,
+	CanonicalCombiningClass_AttachedRight,
+	CanonicalCombiningClass_AttachedTopLeft,
+	CanonicalCombiningClass_AttachedAbove,
+	CanonicalCombiningClass_AttachedAboveRight,
+	CanonicalCombiningClass_BelowLeft,
+	CanonicalCombiningClass_Below,
+	CanonicalCombiningClass_BelowRight,
+	CanonicalCombiningClass_Left,
+	CanonicalCombiningClass_Right,
+	CanonicalCombiningClass_AboveLeft,
+	CanonicalCombiningClass_Above,
+	CanonicalCombiningClass_AboveRight,
+	CanonicalCombiningClass_DoubleBelow,
+	CanonicalCombiningClass_DoubleAbove,
+	CanonicalCombiningClass_IotaSubscript,
+};
+
+enum BidiClass
+{
+	BidiClass_LeftToRight,
+	BidiClass_LeftToRightEmbedding,
+	BidiClass_LeftToRightOverride,
+	BidiClass_RightToLeft,
+	BidiClass_ArabicLetter,
+	BidiClass_RightToLeftEmbedding,
+	BidiClass_RightToLeftOverride,
+	BidiClass_PopDirectionalFormat,
+	BidiClass_EuropeanNumber,
+	BidiClass_EuropeanSeparator,
+	BidiClass_EuropeanTerminator,
+	BidiClass_ArabicNumber,
+	BidiClass_CommonSeparator,
+	BidiClass_NonspacingMark,
+	BidiClass_BoundaryNeutral,
+	BidiClass_ParagraphSeparator,
+	BidiClass_SegmentSeparator,
+	BidiClass_WhiteSpace,
+	BidiClass_OtherNeutral,
+};
+
+enum DecompositionType
+{
+	DecompositionType_Canonical,
+	DecompositionType_Font,
+	DecompositionType_NoBreak,
+	DecompositionType_InitialArabic,
+	DecompositionType_MedialArabic,
+	DecompositionType_FinalArabic,
+	DecompositionType_IsolatedArabic,
+	DecompositionType_Circle,
+	DecompositionType_Superscript,
+	DecompositionType_Subscript,
+	DecompositionType_Vertical,
+	DecompositionType_Wide,
+	DecompositionType_Narrow,
+	DecompositionType_Small,
+	DecompositionType_SquaredCJK,
+	DecompositionType_Fraction,
+	DecompositionType_Unspecified,
+};
+
+enum NumericType
+{
+	NumericType_None,
+	NumericType_Decimal,
+	NumericType_Digit,
+	NumericType_Numeric,
+};
+
+typedef struct {
+	unicode_t codepoint;
+	int8_t generalCategory;
+	int8_t canonicalCombiningClass;
+	int8_t bidiClass;
+	int8_t decompositionType;
+	ptrdiff_t decompositionMapping;
+	size_t numericType;
+	size_t numericValue;
+	int8_t bidiMirrored;
+	unicode_t simpleUppercaseMapping;
+	unicode_t simpleLowercaseMapping;
+	unicode_t simpleTitlecaseMapping;
+} UnicodeMapping;
+
 typedef struct {
 	unicode_t codepoint;
 	ptrdiff_t offsetC;
