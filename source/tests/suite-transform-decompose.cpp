6d19f787ebd306245c172ce88a02c29367c1f601
Anne <knight666+github@gmail.com>
2014-11-17 08:23:48 +0100

    suite-transform-decompose: Added tests for not enough space at start and end.

--- source/tests/suite-transform-decompose.cpp
+++ source/tests/suite-transform-decompose.cpp
@@ -25,3 +25,27 @@ TEST(TransformDecompose, DecomposeNotEnoughSpace)
 	EXPECT_EQ(UTF8_ERR_NOT_ENOUGH_SPACE, errors);
 	EXPECT_STREQ("AmC\xCC\xA7z", b);
 }
+
+TEST(TransformDecompose, DecomposeNotEnoughSpaceAtEnd)
+{
+	const char* c = "Brill\xC3\x95";
+	const size_t s = 7;
+	char b[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(5, utf8transform(c, strlen(c), b, s - 1, UTF8_TRANSFORM_DECOMPOSED, &errors));
+	EXPECT_EQ(UTF8_ERR_NOT_ENOUGH_SPACE, errors);
+	EXPECT_STREQ("Brill", b);
+}
+
+TEST(TransformDecompose, DecomposeNotEnoughSpaceAtStart)
+{
+	const char* c = "\xE1\xB8\xAE";
+	const size_t s = 4;
+	char b[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(0, utf8transform(c, strlen(c), b, s - 1, UTF8_TRANSFORM_DECOMPOSED, &errors));
+	EXPECT_EQ(UTF8_ERR_NOT_ENOUGH_SPACE, errors);
+	EXPECT_STREQ("", b);
+}
