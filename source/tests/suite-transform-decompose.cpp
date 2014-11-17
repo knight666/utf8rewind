a1b985eec6f925209b97e6bc0f3ec63145cea647
Anne <knight666@gmail.com>
2014-11-17 08:31:46 +0100

    suite-transform-decompose: Added tests for just enough space.

--- source/tests/suite-transform-decompose.cpp
+++ source/tests/suite-transform-decompose.cpp
@@ -14,6 +14,42 @@ TEST(TransformDecompose, Decompose)
 	EXPECT_STREQ("Bjo\xCC\x88rn Zonderland", b);
 }
 
+TEST(TransformDecompose, DecomposeJustEnoughSpace)
+{
+	const char* c = "Ar\xE1\xB9\x9Eogance";
+	const size_t s = 12;
+	char b[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(11, utf8transform(c, strlen(c), b, s - 1, UTF8_TRANSFORM_DECOMPOSED, &errors));
+	EXPECT_EQ(0, errors);
+	EXPECT_STREQ("ArR\xCC\xB1ogance", b);
+}
+
+TEST(TransformDecompose, DecomposeJustEnoughSpaceAtEnd)
+{
+	const char* c = "Pounc\xE1\xB8\x94";
+	const size_t s = 11;
+	char b[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(10, utf8transform(c, strlen(c), b, s - 1, UTF8_TRANSFORM_DECOMPOSED, &errors));
+	EXPECT_EQ(0, errors);
+	EXPECT_STREQ("PouncE\xCC\x84\xCC\x80", b);
+}
+
+TEST(TransformDecompose, DecomposeJustEnoughSpaceAtStart)
+{
+	const char* c = "\xE1\xB8\x9C";
+	const size_t s = 6;
+	char b[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(5, utf8transform(c, strlen(c), b, s - 1, UTF8_TRANSFORM_DECOMPOSED, &errors));
+	EXPECT_EQ(0, errors);
+	EXPECT_STREQ("E\xCC\xA7\xCC\x86", b);
+}
+
 TEST(TransformDecompose, DecomposeNotEnoughSpace)
 {
 	const char* c = "Am\xC3\x87zing";
