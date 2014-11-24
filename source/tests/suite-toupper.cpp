1c95fedd126876e9f6dfdb286c3bb7a85aeb169a
Anne <knight666+github@gmail.com>
2014-11-24 08:11:49 +0100

    suite-toupper: Added tests for ASCII.

--- source/tests/suite-toupper.cpp
+++ source/tests/suite-toupper.cpp
@@ -6,9 +6,46 @@ TEST(ToUpper, Ascii)
 {
 	char* c = "Hello World!";
 	const size_t s = 256;
-	char d[s] = { 0 };
+	char b[s] = { 0 };
 	int32_t errors = 0;
 
-	EXPECT_EQ(12, utf8toupper(c, strlen(c), d, s, &errors));
-	EXPECT_STREQ("HELLO WORLD!", d);
+	EXPECT_EQ(12, utf8toupper(c, strlen(c), b, s, &errors));
+	EXPECT_STREQ("HELLO WORLD!", b);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(ToUpper, AsciiAllLowercase)
+{
+	char* c = "abcdefghijklmnopqrstuvwxyz";
+	const size_t s = 256;
+	char b[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(26, utf8toupper(c, strlen(c), b, s, &errors));
+	EXPECT_STREQ("ABCDEFGHIJKLMNOPQRSTUVWXYZ", b);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(ToUpper, AsciiAllUppercase)
+{
+	char* c = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	const size_t s = 256;
+	char b[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(26, utf8toupper(c, strlen(c), b, s, &errors));
+	EXPECT_STREQ("ABCDEFGHIJKLMNOPQRSTUVWXYZ", b);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(ToUpper, AsciiAllOther)
+{
+	char* c = " !\"#$%&'()*+,-./0123456789:;<=>?@[\\]^_`{|}`";
+	const size_t s = 256;
+	char b[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(43, utf8toupper(c, strlen(c), b, s, &errors));
+	EXPECT_STREQ(" !\"#$%&'()*+,-./0123456789:;<=>?@[\\]^_`{|}`", b);
+	EXPECT_EQ(0, errors);
 }
