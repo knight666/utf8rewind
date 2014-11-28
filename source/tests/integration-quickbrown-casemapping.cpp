88ad5c79452770e07246480b1128d7946755a1a0
Quinten <knight666+github@gmail.com>
2014-11-28 16:31:28 +0100

    quickbrown-casemapping: Added tests for German.

--- source/tests/integration-quickbrown-casemapping.cpp
+++ source/tests/integration-quickbrown-casemapping.cpp
@@ -14,8 +14,8 @@ protected:
 	{
 		errors = 0;
 
-		fileRegular.open("testdata/quickbrown.txt", std::ios_base::in);
-		ASSERT_TRUE(fileRegular.is_open());
+		filei.open("testdata/quickbrown.txt", std::ios_base::in);
+		ASSERT_TRUE(filei.is_open());
 
 		fileUppercase.open("testdata/quickbrown_uppercase.txt", std::ios_base::in);
 		ASSERT_TRUE(fileUppercase.is_open());
@@ -28,21 +28,21 @@ protected:
 	{
 		fileUppercase.close();
 		fileLowercase.close();
-		fileRegular.close();
+		filei.close();
 	}
 
 	std::string ReadRegular(size_t position, size_t length)
 	{
 		std::string result;
 
-		fileRegular.seekg(position, std::ios::beg);
-		if (fileRegular.eof())
+		filei.seekg(position, std::ios::beg);
+		if (filei.eof())
 		{
 			return result;
 		}
 
 		result.resize(length + 1);
-		fileRegular.read(&result[0], length);
+		filei.read(&result[0], length);
 
 		return result;
 	}
@@ -79,7 +79,41 @@ protected:
 		return result;
 	}
 
-	std::fstream fileRegular;
+	std::string ConvertToUppercase(const std::string& value)
+	{
+		std::string converted;
+
+		size_t l = utf8toupper(value.c_str(), value.size() - 1, nullptr, 0, &errors);
+		if (l == 0 ||
+			errors != 0)
+		{
+			return converted;
+		}
+
+		converted.resize(l + 1);
+		utf8toupper(value.c_str(), value.size() - 1, &converted[0], l, &errors);
+
+		return converted;
+	}
+
+	std::string ConvertToLowercase(const std::string& value)
+	{
+		std::string converted;
+
+		size_t l = utf8tolower(value.c_str(), value.size() - 1, nullptr, 0, &errors);
+		if (l == 0 ||
+			errors != 0)
+		{
+			return converted;
+		}
+
+		converted.resize(l + 1);
+		utf8tolower(value.c_str(), value.size() - 1, &converted[0], l, &errors);
+
+		return converted;
+	}
+
+	std::fstream filei;
 	std::fstream fileUppercase;
 	std::fstream fileLowercase;
 	int32_t errors;
@@ -88,36 +122,96 @@ protected:
 
 TEST_F(QuickbrownCaseMapping, DanishUppercase)
 {
-	std::string regular = ReadRegular(271, 95);
-	EXPECT_STREQ("  Quizdeltagerne spiste jordb\xc3\xa6r med fl\xc3\xb8" "de, mens cirkusklovnen\x0a  Wolther spillede p\xc3\xa5 xylofon.", regular.c_str());
+	std::string i = ReadRegular(271, 95);
+	EXPECT_STREQ("  Quizdeltagerne spiste jordb\xc3\xa6r med fl\xc3\xb8" "de, mens cirkusklovnen\x0a  Wolther spillede p\xc3\xa5 xylofon.", i.c_str());
 
-	size_t l = utf8toupper(regular.c_str(), regular.size() - 1, nullptr, 0, &errors);
-	EXPECT_EQ(95, l);
+	std::string au = ConvertToUppercase(i);
 	ASSERT_EQ(0, errors);
 
-	std::string converted;
-	converted.resize(l + 1);
-	utf8toupper(regular.c_str(), regular.size() - 1, &converted[0], l, &errors);
-
-	std::string expected = ReadUppercase(271, 95);
-
-	EXPECT_STREQ(expected.c_str(), converted.c_str());
+	std::string eu = ReadUppercase(271, 95);
+	EXPECT_STREQ(eu.c_str(), au.c_str());
 }
 
 TEST_F(QuickbrownCaseMapping, DanishLowercase)
 {
-	std::string regular = ReadRegular(271, 95);
-	EXPECT_STREQ("  Quizdeltagerne spiste jordb\xc3\xa6r med fl\xc3\xb8" "de, mens cirkusklovnen\x0a  Wolther spillede p\xc3\xa5 xylofon.", regular.c_str());
+	std::string i = ReadRegular(271, 95);
+	EXPECT_STREQ("  Quizdeltagerne spiste jordb\xc3\xa6r med fl\xc3\xb8" "de, mens cirkusklovnen\x0a  Wolther spillede p\xc3\xa5 xylofon.", i.c_str());
 
-	size_t l = utf8tolower(regular.c_str(), regular.size() - 1, nullptr, 0, &errors);
-	EXPECT_EQ(95, l);
+	std::string al = ConvertToLowercase(i);
 	ASSERT_EQ(0, errors);
 
-	std::string converted;
-	converted.resize(l + 1);
-	utf8tolower(regular.c_str(), regular.size() - 1, &converted[0], l, &errors);
+	std::string el = ReadLowercase(271, 95);
+	EXPECT_STREQ(el.c_str(), al.c_str());
+}
 
-	std::string expected = ReadLowercase(271, 95);
+TEST_F(QuickbrownCaseMapping, GermanFirstUppercase)
+{
+	std::string i = ReadRegular(503, 64);
+	EXPECT_STREQ("  Falsches \xC3\x9C" "ben von Xylophonmusik qu\xC3\xA4lt jeden gr\xC3\xB6\xC3\x9F" "eren Zwerg", i.c_str());
 
-	EXPECT_STREQ(expected.c_str(), converted.c_str());
+	std::string au = ConvertToUppercase(i);
+	ASSERT_EQ(0, errors);
+
+	std::string eu = ReadUppercase(503, 64);
+	EXPECT_STREQ(eu.c_str(), au.c_str());
+}
+
+TEST_F(QuickbrownCaseMapping, GermanFirstLowercase)
+{
+	std::string i = ReadRegular(503, 64);
+	EXPECT_STREQ("  Falsches \xC3\x9C" "ben von Xylophonmusik qu\xC3\xA4lt jeden gr\xC3\xB6\xC3\x9F" "eren Zwerg", i.c_str());
+
+	std::string al = ConvertToLowercase(i);
+	ASSERT_EQ(0, errors);
+
+	std::string el = ReadLowercase(503, 64);
+	EXPECT_STREQ(el.c_str(), al.c_str());
+}
+
+TEST_F(QuickbrownCaseMapping, GermanSecondUppercase)
+{
+	std::string i = ReadRegular(642, 59);
+	EXPECT_STREQ("  Zw\xC3\xB6lf Boxk\xC3\xA4mpfer jagten Eva quer \xC3\xBC" "ber den Sylter Deich", i.c_str());
+
+	std::string au = ConvertToUppercase(i);
+	ASSERT_EQ(0, errors);
+
+	std::string eu = ReadUppercase(642, 59);
+	EXPECT_STREQ(eu.c_str(), au.c_str());
+}
+
+TEST_F(QuickbrownCaseMapping, GermanSecondLowercase)
+{
+	std::string i = ReadRegular(642, 59);
+	EXPECT_STREQ("  Zw\xC3\xB6lf Boxk\xC3\xA4mpfer jagten Eva quer \xC3\xBC" "ber den Sylter Deich", i.c_str());
+
+	std::string al = ConvertToLowercase(i);
+	ASSERT_EQ(0, errors);
+
+	std::string el = ReadLowercase(642, 59);
+	EXPECT_STREQ(el.c_str(), al.c_str());
+}
+
+TEST_F(QuickbrownCaseMapping, GermanThirdUppercase)
+{
+	std::string i = ReadRegular(767, 30);
+	EXPECT_STREQ("  Heiz\xC3\xB6lr\xC3\xBC" "cksto\xC3\x9F" "abd\xC3\xA4mpfung", i.c_str());
+
+	std::string au = ConvertToUppercase(i);
+	ASSERT_EQ(0, errors);
+
+	std::string eu = ReadUppercase(767, 30);
+	EXPECT_STREQ(eu.c_str(), au.c_str());
+}
+
+TEST_F(QuickbrownCaseMapping, GermanThirdLowercase)
+{
+	std::string i = ReadRegular(767, 30);
+	EXPECT_STREQ("  Heiz\xC3\xB6lr\xC3\xBC" "cksto\xC3\x9F" "abd\xC3\xA4mpfung", i.c_str());
+
+	std::string al = ConvertToLowercase(i);
+	ASSERT_EQ(0, errors);
+
+	std::string el = ReadLowercase(767, 30);
+	EXPECT_STREQ(el.c_str(), al.c_str());
 }
