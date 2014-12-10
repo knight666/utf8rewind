a29c10938e812f340c6f9da13ac503492c0f0ea4
Anne <knight666@gmail.com>
2014-12-10 09:35:05 +0100

    helpers-casemapping: Removed functionality that is no longer required.

--- source/tests/helpers-casemapping.hpp
+++ source/tests/helpers-casemapping.hpp
@@ -28,23 +28,6 @@
 	EXPECT_PRED_FORMAT2(::helpers::CompareUtf8Lowercase, e, a); \
 }
 
-#define CHECK_CASE_MAPPING(_codepoint) { \
-	helpers::CaseMappingEntry e; \
-	e.codepoint = _codepoint; \
-	e.lowercase = helpers::lowercase(_codepoint); \
-	e.uppercase = helpers::uppercase(_codepoint); \
-	helpers::CaseMappingEntry* a = helpers::CaseMappingDatabase::Get().Find(_codepoint); \
-	if (a != nullptr) { \
-		EXPECT_PRED_FORMAT2(CompareCaseMappingEntry, e, *a); \
-	} else { \
-		helpers::CaseMappingEntry af; \
-		af.codepoint = _codepoint; \
-		af.lowercase = helpers::lowercase(_codepoint); \
-		af.uppercase = helpers::uppercase(_codepoint); \
-		EXPECT_PRED_FORMAT2(CompareCaseMappingEntry, e, af); \
-	} \
-}
-
 namespace helpers {
 
 	std::string uppercase(unicode_t codepoint);
@@ -66,30 +49,6 @@ namespace helpers {
 		std::string name;
 	};
 
-	class CaseMappingDatabase
-	{
-
-	public:
-
-		static CaseMappingDatabase& Get();
-
-		bool IsValid() const { return m_Database.size() > 0; }
-
-		CaseMappingEntry* Find(unicode_t codepoint) const;
-
-	private:
-
-		CaseMappingDatabase();
-
-		CaseMappingEntry* ParseLine(const char* line) const;
-
-		std::vector<unicode_t> ReadCodepoints(const char* line, size_t& offset) const;
-
-		unicode_t StringToCodepoint(const char* inputStart, const char* inputEnd) const;
-
-		std::map<unicode_t, CaseMappingEntry*> m_Database;
-	};
-
 	::testing::AssertionResult CompareUtf8Uppercase(
 		const char* expressionExpected, const char* expressionActual,
 		const CaseMappingEntry& entryExpected, const CaseMappingEntry& entryActual);
@@ -98,8 +57,4 @@ namespace helpers {
 		const char* expressionLeft, const char* expressionRight,
 		const CaseMappingEntry& entryExpected, const CaseMappingEntry& entryActual);
 
-	::testing::AssertionResult CompareCaseMappingEntry(
-		const char* expressionLeft, const char* expressionRight,
-		const helpers::CaseMappingEntry& entryLeft, const helpers::CaseMappingEntry& entryRight);
-
-}
+};
