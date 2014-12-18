85265ca2a1c34311915609e5848271e99db704fa
Anne <knight666+github@gmail.com>
2014-12-18 17:29:04 +0100

    querycomposition: Removed query parameter.

--- source/tests/suite-internal-querycomposition.cpp
+++ source/tests/suite-internal-querycomposition.cpp
@@ -6,24 +6,15 @@ extern "C" {
 
 TEST(QueryComposition, NoResult)
 {
-	unicode_t r = querycomposition(0x00000002F, 0x00001988, DecompositionQuery_Composed, nullptr);
+	unicode_t r = querycomposition(0x00000002F, 0x00001988, nullptr);
 
 	EXPECT_EQ(0, r);
-}
-
-TEST(QueryComposition, InvalidQuery)
-{
-	int32_t e = 0;
-	unicode_t r = querycomposition(0x00A82D8D, 0x00008818, 13, &e);
-
-	EXPECT_EQ(0, r);
-	EXPECT_EQ(FindResult_Invalid, e);
 }
 
 TEST(QueryComposition, Found)
 {
 	int32_t e = 0;
-	unicode_t r = querycomposition(0x00001F7C, 0x00000345, DecompositionQuery_Composed, &e);
+	unicode_t r = querycomposition(0x00001F7C, 0x00000345, &e);
 
 	EXPECT_EQ(0x00001FF2, r);
 	EXPECT_EQ(FindResult_Found, e);
@@ -32,7 +23,7 @@ TEST(QueryComposition, Found)
 TEST(QueryComposition, FoundFirst)
 {
 	int32_t e = 0;
-	unicode_t r = querycomposition(0x0000003C, 0x00000338, DecompositionQuery_Composed, &e);
+	unicode_t r = querycomposition(0x0000003C, 0x00000338, &e);
 
 	EXPECT_EQ(0x0000226E, r);
 	EXPECT_EQ(FindResult_Found, e);
@@ -41,7 +32,7 @@ TEST(QueryComposition, FoundFirst)
 TEST(QueryComposition, FoundLast)
 {
 	int32_t e = 0;
-	unicode_t r = querycomposition(0x0001D1BC, 0x0001D16F, DecompositionQuery_Composed, &e);
+	unicode_t r = querycomposition(0x0001D1BC, 0x0001D16F, &e);
 
 	EXPECT_EQ(0x0001D1C0, r);
 	EXPECT_EQ(FindResult_Found, e);
@@ -50,7 +41,7 @@ TEST(QueryComposition, FoundLast)
 TEST(QueryComposition, FoundPivot)
 {
 	int32_t e = 0;
-	unicode_t r = querycomposition(0x00000399, 0x00000314, DecompositionQuery_Composed, &e);
+	unicode_t r = querycomposition(0x00000399, 0x00000314, &e);
 
 	EXPECT_EQ(0x00001F39, r);
 	EXPECT_EQ(FindResult_Found, e);
@@ -59,7 +50,7 @@ TEST(QueryComposition, FoundPivot)
 TEST(QueryComposition, FoundPivotUp)
 {
 	int32_t e = 0;
-	unicode_t r = querycomposition(0x00000069, 0x00000306, DecompositionQuery_Composed, &e);
+	unicode_t r = querycomposition(0x00000069, 0x00000306, &e);
 
 	EXPECT_EQ(0x0000012D, r);
 	EXPECT_EQ(FindResult_Found, e);
@@ -68,7 +59,7 @@ TEST(QueryComposition, FoundPivotUp)
 TEST(QueryComposition, FoundPivotDown)
 {
 	int32_t e = 0;
-	unicode_t r = querycomposition(0x00001F00, 0x00000345, DecompositionQuery_Composed, &e);
+	unicode_t r = querycomposition(0x00001F00, 0x00000345, &e);
 
 	EXPECT_EQ(0x00001F80, r);
 	EXPECT_EQ(FindResult_Found, e);
@@ -77,7 +68,7 @@ TEST(QueryComposition, FoundPivotDown)
 TEST(QueryComposition, FoundPivotDownDown)
 {
 	int32_t e = 0;
-	unicode_t r = querycomposition(0x00001FFE, 0x00000301, DecompositionQuery_Composed, &e);
+	unicode_t r = querycomposition(0x00001FFE, 0x00000301, &e);
 
 	EXPECT_EQ(0x00001FDE, r);
 	EXPECT_EQ(FindResult_Found, e);
@@ -86,7 +77,7 @@ TEST(QueryComposition, FoundPivotDownDown)
 TEST(QueryComposition, FoundMaxDepth)
 {
 	int32_t e = 0;
-	unicode_t r = querycomposition(0x00002282, 0x00000338, DecompositionQuery_Composed, &e);
+	unicode_t r = querycomposition(0x00002282, 0x00000338, &e);
 
 	EXPECT_EQ(0x00002284, r);
 	EXPECT_EQ(FindResult_Found, e);
@@ -95,7 +86,7 @@ TEST(QueryComposition, FoundMaxDepth)
 TEST(QueryComposition, Missing)
 {
 	int32_t e = 0;
-	unicode_t r = querycomposition(0x00001F28, 0x00001D16, DecompositionQuery_Composed, &e);
+	unicode_t r = querycomposition(0x00001F28, 0x00001D16, &e);
 
 	EXPECT_EQ(0, r);
 	EXPECT_EQ(FindResult_Missing, e);
@@ -104,7 +95,7 @@ TEST(QueryComposition, Missing)
 TEST(QueryComposition, MissingOutOfLowerBounds)
 {
 	int32_t e = 0;
-	unicode_t r = querycomposition(0x00000001, 0x0000011D, DecompositionQuery_Composed, &e);
+	unicode_t r = querycomposition(0x00000001, 0x0000011D, &e);
 
 	EXPECT_EQ(0, r);
 	EXPECT_EQ(FindResult_OutOfBounds, e);
@@ -113,7 +104,7 @@ TEST(QueryComposition, MissingOutOfLowerBounds)
 TEST(QueryComposition, MissingOutOfUpperBounds)
 {
 	int32_t e = 0;
-	unicode_t r = querycomposition(0xABABABAB, 0xDADADADA, DecompositionQuery_Composed, &e);
+	unicode_t r = querycomposition(0xABABABAB, 0xDADADADA, &e);
 
 	EXPECT_EQ(0, r);
 	EXPECT_EQ(FindResult_OutOfBounds, e);
