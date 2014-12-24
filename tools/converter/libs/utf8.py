22b12c6fdb01c10d59a645fc6379aeb570e39ee8
Anne <knight666+github@gmail.com>
2014-12-24 09:02:41 +0100

    utf8: Refactored conversion to UTF-8 to use less characters.

--- tools/converter/libs/utf8.py
+++ tools/converter/libs/utf8.py
@@ -20,19 +20,35 @@ def codepointToUtf8(codepoint):
 		encoded_length = 3
 	
 	if encoded_length == 1:
-		result = '\\x' + format(codepoint, '02x')
+		if codepoint < 0x20:
+			conversion = {
+				0x00: "",
+				0x07: "\\a",
+				0x08: "\\b",
+				0x09: "\\t",
+				0x0A: "\\n",
+				0x0B: "\\v",
+				0x0C: "\\f",
+				0x0D: "\\r",
+			}
+			if codepoint in conversion:
+				result = conversion[codepoint]
+			else:
+				result = '\\x' + format(codepoint, '02X')
+		else:
+			result = "%c" % codepoint
 	elif encoded_length == 2:
-		result += '\\x' + format((codepoint >>   6)         | 0xC0, '02x')
-		result += '\\x' + format((codepoint         & 0x3F) | 0x80, '02x')
+		result += '\\x' + format((codepoint >>   6)         | 0xC0, '02X')
+		result += '\\x' + format((codepoint         & 0x3F) | 0x80, '02X')
 	elif encoded_length == 3:
-		result += '\\x' + format((codepoint  >> 12)         | 0xE0, '02x')
-		result += '\\x' + format(((codepoint >>  6) & 0x3F) | 0x80, '02x')
-		result += '\\x' + format((codepoint         & 0x3F) | 0x80, '02x')
+		result += '\\x' + format((codepoint  >> 12)         | 0xE0, '02X')
+		result += '\\x' + format(((codepoint >>  6) & 0x3F) | 0x80, '02X')
+		result += '\\x' + format((codepoint         & 0x3F) | 0x80, '02X')
 	elif encoded_length == 4:
-		result += '\\x' + format((codepoint  >> 18)         | 0xF0, '02x')
-		result += '\\x' + format(((codepoint >> 12) & 0x3F) | 0x80, '02x')
-		result += '\\x' + format(((codepoint >>  6) & 0x3F) | 0x80, '02x')
-		result += '\\x' + format((codepoint         & 0x3F) | 0x80, '02x')
+		result += '\\x' + format((codepoint  >> 18)         | 0xF0, '02X')
+		result += '\\x' + format(((codepoint >> 12) & 0x3F) | 0x80, '02X')
+		result += '\\x' + format(((codepoint >>  6) & 0x3F) | 0x80, '02X')
+		result += '\\x' + format((codepoint         & 0x3F) | 0x80, '02X')
 	
 	return result
 
