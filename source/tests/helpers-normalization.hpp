ba5f38223f319eba06ca49e9bae946ac66b4c295
Quinten <knight666+github@gmail.com>
2014-12-24 16:26:41 +0100

    helpers-normalization: Refactored comparison for entries. integration: Refactored test output.

--- source/tests/helpers-normalization.hpp
+++ source/tests/helpers-normalization.hpp
@@ -4,28 +4,19 @@
 
 #include "utf8rewind.h"
 
-#define CHECK_NORMALIZE(_codepoint, _decomposed, _composed, _name) { \
+#define CHECK_NORMALIZE(_codepoint, _decomposed, _composed, _decomposedCompatibility, _composedCompatibility, _name) { \
 	::helpers::NormalizationEntry e; \
 	e.codepoint = _codepoint; \
 	e.name = _name; \
 	e.decomposed = _decomposed; \
 	e.composed = _composed; \
+	e.decomposedCompatibility = _decomposedCompatibility; \
+	e.composedCompatibility = _composedCompatibility; \
 	::helpers::NormalizationEntry a; \
 	a.decomposed = helpers::nfd(_codepoint); \
 	a.composed = helpers::nfc(helpers::nfd(_codepoint)); \
-	EXPECT_PRED_FORMAT2(::helpers::CompareNormalization, e, a); \
-}
-
-#define CHECK_NORMALIZE_COMPATIBILITY(_codepoint, _decomposed, _composed, _name) { \
-	::helpers::NormalizationEntry e; \
-	e.codepoint = _codepoint; \
-	e.name = _name; \
-	e.compatibility = true; \
-	e.decomposed = _decomposed; \
-	e.composed = _composed; \
-	::helpers::NormalizationEntry a; \
-	a.decomposed = helpers::nfkd(_codepoint); \
-	a.composed = helpers::nfkc(helpers::nfkd(_codepoint)); \
+	a.decomposedCompatibility = helpers::nfkd(_codepoint); \
+	a.composedCompatibility = helpers::nfkc(helpers::nfkd(_codepoint)); \
 	EXPECT_PRED_FORMAT2(::helpers::CompareNormalization, e, a); \
 }
 
@@ -47,15 +38,15 @@ namespace helpers {
 	{
 		NormalizationEntry()
 			: codepoint(0)
-			, compatibility(false)
 		{
 		}
 
 		unicode_t codepoint;
 		std::string name;
-		bool compatibility;
 		std::string decomposed;
 		std::string composed;
+		std::string decomposedCompatibility;
+		std::string composedCompatibility;
 	};
 
 	::testing::AssertionResult CompareNormalization(
