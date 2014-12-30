75a8eccea269e7534b1cd88af0b2692c0cca6c58
Anne <knight666+github@gmail.com>
2014-12-30 09:12:31 +0100

    utf8transform: Fixed issues when writing strings. suite-transform-compose: Added tests for invalid characters.

--- source/tests/suite-transform-compose.cpp
+++ source/tests/suite-transform-compose.cpp
@@ -11,9 +11,45 @@ TEST(TransformCompose, Found)
 	char b[s] = { 0 };
 	int32_t errors = 0;
 
-	EXPECT_EQ(3, utf8transform(c, strlen(c), b, s, UTF8_TRANSFORM_COMPOSED, &errors));
+	EXPECT_EQ(3, utf8transform(c, strlen(c), b, s - 1, UTF8_TRANSFORM_COMPOSED, &errors));
 	EXPECT_EQ(0, errors);
 	EXPECT_UTF8EQ("\xE1\xB8\x8A", b);
+}
+
+TEST(TransformCompose, InvalidCharacter)
+{
+	const char* c = "\xCC";
+	const size_t s = 512;
+	char b[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(3, utf8transform(c, strlen(c), b, s - 1, UTF8_TRANSFORM_COMPOSED, &errors));
+	EXPECT_EQ(0, errors);
+	EXPECT_UTF8EQ("\xEF\xBF\xBD", b);
+}
+
+TEST(TransformCompose, InvalidCharacterNotEnoughSpace)
+{
+	const char* c = "\xCC";
+	const size_t s = 3;
+	char b[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(0, utf8transform(c, strlen(c), b, s - 1, UTF8_TRANSFORM_COMPOSED, &errors));
+	EXPECT_EQ(UTF8_ERR_NOT_ENOUGH_SPACE, errors);
+	EXPECT_UTF8EQ("", b);
+}
+
+TEST(TransformCompose, InvalidCharacterAtEnd)
+{
+	const char* c = "jordbI\xCC\x80\x98";
+	const size_t s = 512;
+	char b[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(10, utf8transform(c, strlen(c), b, s - 1, UTF8_TRANSFORM_COMPOSED, &errors));
+	EXPECT_EQ(0, errors);
+	EXPECT_UTF8EQ("jordb\xC3\x8C\xEF\xBF\xBD", b);
 }
 
 TEST(TransformCompose, LeftBasicLatin)
@@ -23,7 +59,7 @@ TEST(TransformCompose, LeftBasicLatin)
 	char b[s] = { 0 };
 	int32_t errors = 0;
 
-	EXPECT_EQ(4, utf8transform(c, strlen(c), b, s, UTF8_TRANSFORM_COMPOSED, &errors));
+	EXPECT_EQ(4, utf8transform(c, strlen(c), b, s - 1, UTF8_TRANSFORM_COMPOSED, &errors));
 	EXPECT_EQ(0, errors);
 	EXPECT_UTF8EQ("z\xE0\xA9\x82", b);
 }
@@ -35,7 +71,7 @@ TEST(TransformCompose, RightBasicLatin)
 	char b[s] = { 0 };
 	int32_t errors = 0;
 
-	EXPECT_EQ(3, utf8transform(c, strlen(c), b, s, UTF8_TRANSFORM_COMPOSED, &errors));
+	EXPECT_EQ(3, utf8transform(c, strlen(c), b, s - 1, UTF8_TRANSFORM_COMPOSED, &errors));
 	EXPECT_EQ(0, errors);
 	EXPECT_UTF8EQ("\xC6\x99t", b);
 }
@@ -47,7 +83,7 @@ TEST(TransformCompose, BothBasicLatin)
 	char b[s] = { 0 };
 	int32_t errors = 0;
 
-	EXPECT_EQ(2, utf8transform(c, strlen(c), b, s, UTF8_TRANSFORM_COMPOSED, &errors));
+	EXPECT_EQ(2, utf8transform(c, strlen(c), b, s - 1, UTF8_TRANSFORM_COMPOSED, &errors));
 	EXPECT_EQ(0, errors);
 	EXPECT_UTF8EQ("AV", b);
 }
