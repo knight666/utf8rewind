46a42e0f0eb389872c005a64ca28a12635b51edf
Anne <knight666+github@gmail.com>
2014-12-31 17:51:54 +0100

    suite-tolower: Refactored tests to avoid redundancy.

--- source/tests/suite-tolower.cpp
+++ source/tests/suite-tolower.cpp
@@ -2,6 +2,8 @@
 
 #include "utf8rewind.h"
 
+#include "helpers-strings.hpp"
+
 TEST(ToLower, BasicLatin)
 {
 	const char* c = "Hello World!";
@@ -9,44 +11,44 @@ TEST(ToLower, BasicLatin)
 	char b[s] = { 0 };
 	int32_t errors = 0;
 
-	EXPECT_EQ(12, utf8tolower(c, strlen(c), b, s, &errors));
-	EXPECT_STREQ("hello world!", b);
+	EXPECT_EQ(12, utf8tolower(c, strlen(c), b, s - 1, &errors));
+	EXPECT_UTF8EQ("hello world!", b);
 	EXPECT_EQ(0, errors);
 }
 
-TEST(ToLower, BasicLatinAllUppercase)
+TEST(ToLower, BasicLatinUppercase)
 {
-	const char* c = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	const char* c = "G";
 	const size_t s = 256;
 	char b[s] = { 0 };
 	int32_t errors = 0;
 
-	EXPECT_EQ(26, utf8tolower(c, strlen(c), b, s, &errors));
-	EXPECT_STREQ("abcdefghijklmnopqrstuvwxyz", b);
+	EXPECT_EQ(1, utf8tolower(c, strlen(c), b, s - 1, &errors));
+	EXPECT_UTF8EQ("g", b);
 	EXPECT_EQ(0, errors);
 }
 
-TEST(ToLower, BasicLatinAllLowercase)
+TEST(ToLower, BasicLatinLowercase)
 {
-	const char* c = "abcdefghijklmnopqrstuvwxyz";
+	const char* c = "y";
 	const size_t s = 256;
 	char b[s] = { 0 };
 	int32_t errors = 0;
 
-	EXPECT_EQ(26, utf8tolower(c, strlen(c), b, s, &errors));
-	EXPECT_STREQ("abcdefghijklmnopqrstuvwxyz", b);
+	EXPECT_EQ(1, utf8tolower(c, strlen(c), b, s - 1, &errors));
+	EXPECT_UTF8EQ("y", b);
 	EXPECT_EQ(0, errors);
 }
 
-TEST(ToLower, BasicLatinAllOther)
+TEST(ToLower, BasicLatinUnaffected)
 {
-	const char* c = " !\"#$%&'()*+,-./0123456789:;<=>?@[\\]^_`{|}`";
+	const char* c = "@";
 	const size_t s = 256;
 	char b[s] = { 0 };
 	int32_t errors = 0;
 
-	EXPECT_EQ(43, utf8tolower(c, strlen(c), b, s, &errors));
-	EXPECT_STREQ(" !\"#$%&'()*+,-./0123456789:;<=>?@[\\]^_`{|}`", b);
+	EXPECT_EQ(1, utf8tolower(c, strlen(c), b, s - 1, &errors));
+	EXPECT_UTF8EQ("@", b);
 	EXPECT_EQ(0, errors);
 }
 
@@ -67,7 +69,7 @@ TEST(ToLower, BasicLatinNotEnoughSpace)
 	int32_t errors = 0;
 
 	EXPECT_EQ(8, utf8tolower(c, strlen(c), b, s - 1, &errors));
-	EXPECT_STREQ("very imp", b);
+	EXPECT_UTF8EQ("very imp", b);
 	EXPECT_EQ(UTF8_ERR_NOT_ENOUGH_SPACE, errors);
 }
 
