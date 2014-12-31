608a4d3432fd53bb604a99af19531150078dfe42
Anne <knight666@gmail.com>
2014-12-31 08:09:57 +0100

    suite-internal-quickcheck: Refactored existing tests.

--- source/tests/suite-internal-quickcheck.cpp
+++ source/tests/suite-internal-quickcheck.cpp
@@ -6,248 +6,180 @@ extern "C" {
 
 TEST(QuickCheck, NoResult)
 {
-	uint8_t r = quickcheck(0x81288, QuickCheck_Normalize_Composed);
-
-	EXPECT_EQ(QuickCheckResult_Yes, r);
+	EXPECT_EQ(0, quickcheck(0x81288, QuickCheck_Normalize_Composed));
 }
 
 TEST(QuickCheck, InvalidNormalizationForm)
 {
-	uint8_t r = quickcheck(0xC0, 198);
-
-	EXPECT_EQ(QuickCheckResult_Invalid, r);
+	EXPECT_EQ(QuickCheckResult_Invalid, quickcheck(0x00C0, 198));
 }
 
 // Composed
 
 TEST(QuickCheck, ComposedFoundNo)
 {
-	uint8_t r = quickcheck(0x1FEE, QuickCheck_Normalize_Composed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0x1FEE, QuickCheck_Normalize_Composed));
 }
 
 TEST(QuickCheck, ComposedFoundMaybe)
 {
-	uint8_t r = quickcheck(0x0DCF, QuickCheck_Normalize_Composed);
-
-	EXPECT_EQ(QuickCheckResult_Maybe, r);
+	EXPECT_EQ(QuickCheckResult_Maybe, quickcheck(0x0DCF, QuickCheck_Normalize_Composed));
 }
 
 TEST(QuickCheck, ComposedFoundFirst)
 {
-	uint8_t r = quickcheck(0x0300, QuickCheck_Normalize_Composed);
-
-	EXPECT_EQ(QuickCheckResult_Maybe, r);
+	EXPECT_EQ(QuickCheckResult_Maybe, quickcheck(0x0300, QuickCheck_Normalize_Composed));
 }
 
 TEST(QuickCheck, ComposedFoundLast)
 {
-	uint8_t r = quickcheck(0x2FA1D, QuickCheck_Normalize_Composed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0x2FA1D, QuickCheck_Normalize_Composed));
 }
 
 TEST(QuickCheck, ComposedOutOfLowerBounds)
 {
-	uint8_t r = quickcheck(0x00FF, QuickCheck_Normalize_Composed);
-
-	EXPECT_EQ(QuickCheckResult_Yes, r);
+	EXPECT_EQ(QuickCheckResult_Yes, quickcheck(0x00FF, QuickCheck_Normalize_Composed));
 }
 
 TEST(QuickCheck, ComposedOutOfUpperBounds)
 {
-	uint8_t r = quickcheck(0x30000, QuickCheck_Normalize_Composed);
-
-	EXPECT_EQ(QuickCheckResult_Yes, r);
+	EXPECT_EQ(QuickCheckResult_Yes, quickcheck(0x30000, QuickCheck_Normalize_Composed));
 }
 
 TEST(QuickCheck, ComposedBetweenBlocks)
 {
-	uint8_t r = quickcheck(0x2B81, QuickCheck_Normalize_Composed);
-
-	EXPECT_EQ(QuickCheckResult_Yes, r);
+	EXPECT_EQ(QuickCheckResult_Yes, quickcheck(0x2B81, QuickCheck_Normalize_Composed));
 }
 
 // Decomposed
 
 TEST(QuickCheck, DecomposedFoundNo)
 {
-	uint8_t r = quickcheck(0x0374, QuickCheck_Normalize_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0x0374, QuickCheck_Normalize_Decomposed));
 }
 
 TEST(QuickCheck, DecomposedFoundFirst)
 {
-	uint8_t r = quickcheck(0x00C0, QuickCheck_Normalize_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0x00C0, QuickCheck_Normalize_Decomposed));
 }
 
 TEST(QuickCheck, DecomposedFoundLast)
 {
-	uint8_t r = quickcheck(0x2FA1D, QuickCheck_Normalize_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0x2FA1D, QuickCheck_Normalize_Decomposed));
 }
 
 TEST(QuickCheck, DecomposedOutOfLowerBounds)
 {
-	uint8_t r = quickcheck(0x007F, QuickCheck_Normalize_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_Yes, r);
+	EXPECT_EQ(QuickCheckResult_Yes, quickcheck(0x007F, QuickCheck_Normalize_Decomposed));
 }
 
 TEST(QuickCheck, DecomposedOutOfUpperBounds)
 {
-	uint8_t r = quickcheck(0x30000, QuickCheck_Normalize_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_Yes, r);
+	EXPECT_EQ(QuickCheckResult_Yes, quickcheck(0x30000, QuickCheck_Normalize_Decomposed));
 }
 
 TEST(QuickCheck, DecomposedBetweenBlocks)
 {
-	uint8_t r = quickcheck(0xFB20, QuickCheck_Normalize_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_Yes, r);
+	EXPECT_EQ(QuickCheckResult_Yes, quickcheck(0xFB20, QuickCheck_Normalize_Decomposed));
 }
 
 TEST(QuickCheck, DecomposedHangulSyllable)
 {
-	uint8_t r = quickcheck(0xB1A1, QuickCheck_Normalize_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0xB1A1, QuickCheck_Normalize_Decomposed));
 }
 
 TEST(QuickCheck, DecomposedHangulSyllableStart)
 {
-	uint8_t r = quickcheck(0xAC00, QuickCheck_Normalize_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0xAC00, QuickCheck_Normalize_Decomposed));
 }
 
 TEST(QuickCheck, DecomposedHangulSyllableEnd)
 {
-	uint8_t r = quickcheck(0xD7A3, QuickCheck_Normalize_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0xD7A3, QuickCheck_Normalize_Decomposed));
 }
 
 // Compatibility composed
 
 TEST(QuickCheck, CompatibilityComposedFoundNo)
 {
-	uint8_t r = quickcheck(0x0A5B, QuickCheck_Normalize_Compatibility_Composed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0x0A5B, QuickCheck_Normalize_Compatibility_Composed));
 }
 
 TEST(QuickCheck, CompatibilityComposedFoundMaybe)
 {
-	uint8_t r = quickcheck(0x0B57, QuickCheck_Normalize_Compatibility_Composed);
-
-	EXPECT_EQ(QuickCheckResult_Maybe, r);
+	EXPECT_EQ(QuickCheckResult_Maybe, quickcheck(0x0B57, QuickCheck_Normalize_Compatibility_Composed));
 }
 
 TEST(QuickCheck, CompatibilityComposedFoundFirst)
 {
-	uint8_t r = quickcheck(0x00A0, QuickCheck_Normalize_Compatibility_Composed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0x00A0, QuickCheck_Normalize_Compatibility_Composed));
 }
 
 TEST(QuickCheck, CompatibilityComposedFoundLast)
 {
-	uint8_t r = quickcheck(0x2FA1D, QuickCheck_Normalize_Compatibility_Composed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0x2FA1D, QuickCheck_Normalize_Compatibility_Composed));
 }
 
 TEST(QuickCheck, CompatibilityComposedOutOfLowerBounds)
 {
-	uint8_t r = quickcheck(0x007F, QuickCheck_Normalize_Compatibility_Composed);
-
-	EXPECT_EQ(QuickCheckResult_Yes, r);
+	EXPECT_EQ(QuickCheckResult_Yes, quickcheck(0x007F, QuickCheck_Normalize_Compatibility_Composed));
 }
 
 TEST(QuickCheck, CompatibilityComposedOutOfUpperBounds)
 {
-	uint8_t r = quickcheck(0x30000, QuickCheck_Normalize_Compatibility_Composed);
-
-	EXPECT_EQ(QuickCheckResult_Yes, r);
+	EXPECT_EQ(QuickCheckResult_Yes, quickcheck(0x30000, QuickCheck_Normalize_Compatibility_Composed));
 }
 
 TEST(QuickCheck, CompatibilityComposedBetweenBlocks)
 {
-	uint8_t r = quickcheck(0x1EE5C, QuickCheck_Normalize_Compatibility_Composed);
-
-	EXPECT_EQ(QuickCheckResult_Yes, r);
+	EXPECT_EQ(QuickCheckResult_Yes, quickcheck(0x1EE5C, QuickCheck_Normalize_Compatibility_Composed));
 }
 
 // Compatibility decomposed
 
 TEST(QuickCheck, CompatibilityDecomposedFoundNo)
 {
-	uint8_t r = quickcheck(0x03F9, QuickCheck_Normalize_Compatibility_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0x03F9, QuickCheck_Normalize_Compatibility_Decomposed));
 }
 
 TEST(QuickCheck, CompatibilityDecomposedFoundFirst)
 {
-	uint8_t r = quickcheck(0x00A0, QuickCheck_Normalize_Compatibility_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0x00A0, QuickCheck_Normalize_Compatibility_Decomposed));
 }
 
 TEST(QuickCheck, CompatibilityDecomposedFoundLast)
 {
-	uint8_t r = quickcheck(0x2FA1D, QuickCheck_Normalize_Compatibility_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0x2FA1D, QuickCheck_Normalize_Compatibility_Decomposed));
 }
 
 TEST(QuickCheck, CompatibilityDecomposedOutOfLowerBounds)
 {
-	uint8_t r = quickcheck(0x007F, QuickCheck_Normalize_Compatibility_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_Yes, r);
+	EXPECT_EQ(QuickCheckResult_Yes, quickcheck(0x007F, QuickCheck_Normalize_Compatibility_Decomposed));
 }
 
 TEST(QuickCheck, CompatibilityDecomposedOutOfUpperBounds)
 {
-	uint8_t r = quickcheck(0x30000, QuickCheck_Normalize_Compatibility_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_Yes, r);
+	EXPECT_EQ(QuickCheckResult_Yes, quickcheck(0x30000, QuickCheck_Normalize_Compatibility_Decomposed));
 }
 
 TEST(QuickCheck, CompatibilityDecomposedBetweenBlocks)
 {
-	uint8_t r = quickcheck(0x038D, QuickCheck_Normalize_Compatibility_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_Yes, r);
+	EXPECT_EQ(QuickCheckResult_Yes, quickcheck(0x038D, QuickCheck_Normalize_Compatibility_Decomposed));
 }
 
 TEST(QuickCheck, CompatibilityDecomposedHangulSyllable)
 {
-	uint8_t r = quickcheck(0xCCCA, QuickCheck_Normalize_Compatibility_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0xCCCA, QuickCheck_Normalize_Compatibility_Decomposed));
 }
 
 TEST(QuickCheck, CompatibilityDecomposedHangulSyllableStart)
 {
-	uint8_t r = quickcheck(0xAC00, QuickCheck_Normalize_Compatibility_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0xAC00, QuickCheck_Normalize_Compatibility_Decomposed));
 }
 
 TEST(QuickCheck, CompatibilityDecomposedHangulSyllableEnd)
 {
-	uint8_t r = quickcheck(0xD7A3, QuickCheck_Normalize_Compatibility_Decomposed);
-
-	EXPECT_EQ(QuickCheckResult_No, r);
+	EXPECT_EQ(QuickCheckResult_No, quickcheck(0xD7A3, QuickCheck_Normalize_Compatibility_Decomposed));
 }
 
 // Uppercase
