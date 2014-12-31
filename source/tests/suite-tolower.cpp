5b840e3895a2a49048883740429a2c5e45800edf
Anne <knight666@gmail.com>
2014-12-31 17:57:18 +0100

    suite-tolower: Added tests for two-byte codepoints.

--- source/tests/suite-tolower.cpp
+++ source/tests/suite-tolower.cpp
@@ -73,6 +73,75 @@ TEST(ToLower, BasicLatinNotEnoughSpace)
 	EXPECT_EQ(UTF8_ERR_NOT_ENOUGH_SPACE, errors);
 }
 
+TEST(ToLower, TwoBytes)
+{
+	const char* c = "Rac\xC3\x88r";
+	const size_t s = 256;
+	char b[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(6, utf8tolower(c, strlen(c), b, s - 1, &errors));
+	EXPECT_UTF8EQ("rac\xC3\xA8r", b);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(ToLower, TwoBytesUppercase)
+{
+	const char* c = "\xC3\x82";
+	const size_t s = 256;
+	char b[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(2, utf8tolower(c, strlen(c), b, s - 1, &errors));
+	EXPECT_UTF8EQ("\xC3\xA2", b);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(ToLower, TwoBytesLowercase)
+{
+	const char* c = "\xC3\xA7";
+	const size_t s = 256;
+	char b[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(2, utf8tolower(c, strlen(c), b, s - 1, &errors));
+	EXPECT_UTF8EQ("\xC3\xA7", b);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(ToLower, TwoBytesUnaffected)
+{
+	const char* c = "\xC3\xB7";
+	const size_t s = 256;
+	char b[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(2, utf8tolower(c, strlen(c), b, s - 1, &errors));
+	EXPECT_UTF8EQ("\xC3\xB7", b);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(ToLower, TwoBytesAmountOfBytes)
+{
+	const char* c = "\xC3\x8Dnteresting";
+	int32_t errors = 0;
+
+	EXPECT_EQ(12, utf8tolower(c, strlen(c), nullptr, 0, &errors));
+	EXPECT_EQ(0, errors);
+}
+
+TEST(ToLower, TwoBytesNotEnoughSpace)
+{
+	const char* c = "Atl\xC3\x84ntis";
+	const size_t s = 4;
+	char b[s] = { 0 };
+	int32_t errors = 0;
+
+	EXPECT_EQ(3, utf8tolower(c, strlen(c), b, s - 1, &errors));
+	EXPECT_UTF8EQ("atl", b);
+	EXPECT_EQ(UTF8_ERR_NOT_ENOUGH_SPACE, errors);
+}
+
 TEST(ToLower, InvalidData)
 {
 	int32_t errors = 0;
