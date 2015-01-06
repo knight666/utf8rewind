40231fd64579d842edb358b5e31bf9435e4cdd76
Anne <knight666+github@gmail.com>
2015-01-06 07:54:18 +0100

    Moved seeking functions to a seperate header.

--- source/utf8rewind.c
+++ source/utf8rewind.c
@@ -28,6 +28,7 @@
 #include "internal/casemapping.h"
 #include "internal/codepoint.h"
 #include "internal/composition.h"
+#include "internal/seeking.h"
 #include "normalization.h"
 
 #if defined(__GNUC__) && !defined(COMPILER_ICC)
@@ -447,84 +448,6 @@ size_t utf8towide(const char* input, size_t inputSize, wchar_t* target, size_t t
 #else
 	return SIZE_MAX;
 #endif
-}
-
-const char* seekforward(const char* src, const char* srcEnd, size_t srcLength, off_t offset)
-{
-	size_t i;
-
-	if (srcEnd <= src || offset <= 0 || srcLength == 0)
-	{
-		return src;
-	}
-	if (offset >= (off_t)srcLength)
-	{
-		return srcEnd;
-	}
-
-	do
-	{
-		size_t codepoint_length = codepoint_decoded_length[(uint8_t)*src];
-		if (codepoint_length == 0)
-		{
-			codepoint_length = 1;
-		}
-
-		src++;
-		srcLength--;
-
-		for (i = 1; i < codepoint_length && srcLength > 0; ++i)
-		{
-			if (
-				(*src & 0x80) == 0    || /* Not a continuation byte */
-				(*src & 0xC0) == 0xC0    /* Start of a new sequence */
-			)
-			{
-				break;
-			}
-
-			src++;
-			srcLength--;
-		}
-	}
-	while (--offset > 0 && srcLength > 0);
-
-	return src;
-}
-
-const char* seekrewind(const char* srcStart, const char* src, size_t srcLength, off_t offset)
-{
-	if (srcStart >= src || offset >= 0 || srcLength == 0)
-	{
-		return src;
-	}
-	if (-offset >= (off_t)srcLength)
-	{
-		return srcStart;
-	}
-
-	/* Ignore NUL codepoint */
-	src--;
-
-	while (src != srcStart)
-	{
-		if (
-			(*src & 0x80) == 0 ||    /* ASCII */
-			(*src & 0xC0) != 0x80 || /* Malformed continuation byte */
-			(*src & 0xFE) == 0xFE    /* Illegal byte */
-		)
-		{
-			++offset;
-			if (offset == 0)
-			{
-				break;
-			}
-		}
-
-		src--;
-	}
-
-	return src;
 }
 
 const char* utf8seek(const char* text, const char* textStart, off_t offset, int direction)
