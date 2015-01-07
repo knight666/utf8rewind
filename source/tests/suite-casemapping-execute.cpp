6b7c48a1c45f5e94446e56d24c6cebcb2178f651
Quinten <knight666+github@gmail.com>
2015-01-07 09:23:35 +0100

    suite-casemapping-execute: Refactored existing tests.

--- source/tests/suite-casemapping-execute.cpp
+++ source/tests/suite-casemapping-execute.cpp
@@ -7,30 +7,42 @@ extern "C" {
 
 #include "helpers-strings.hpp"
 
-// Uppercase
+TEST(CaseMappingExecute, NotEnoughSpaceBasicLatin)
+{
+	int32_t e = 0;
+	size_t s = 0;
+	char d[2] = { 0 };
+	char* dp = d;
 
-TEST(CaseMappingExecuteUppercase, DecomposedNotEnoughSpace)
+	EXPECT_EQ(0, casemapping_execute(0x00000073, &dp, &s, UnicodeProperty_Uppercase, &e));
+	EXPECT_UTF8EQ("", d);
+	EXPECT_EQ(d, dp);
+	EXPECT_EQ(0, s);
+	EXPECT_EQ(UTF8_ERR_NOT_ENOUGH_SPACE, e);
+}
+
+TEST(CaseMappingExecute, NotEnoughSpaceUnaffected)
 {
 	int32_t e = 0;
 	size_t s = 1;
 	char d[2] = { 0 };
 	char* dp = d;
 
-	EXPECT_EQ(0, casemapping_execute(0x0000029E, &dp, &s, UnicodeProperty_Uppercase, &e));
+	EXPECT_EQ(0, casemapping_execute(0x0000211E, &dp, &s, UnicodeProperty_Titlecase, &e));
 	EXPECT_UTF8EQ("", d);
 	EXPECT_EQ(d, dp);
 	EXPECT_EQ(1, s);
 	EXPECT_EQ(UTF8_ERR_NOT_ENOUGH_SPACE, e);
 }
 
-TEST(CaseMappingExecuteUppercase, UnaffectedNotEnoughSpace)
+TEST(CaseMappingExecute, NotEnoughSpaceDecomposed)
 {
 	int32_t e = 0;
 	size_t s = 1;
 	char d[2] = { 0 };
 	char* dp = d;
 
-	EXPECT_EQ(0, casemapping_execute(0x0000055E, &dp, &s, UnicodeProperty_Uppercase, &e));
+	EXPECT_EQ(0, casemapping_execute(0x00001F54, &dp, &s, UnicodeProperty_Lowercase, &e));
 	EXPECT_UTF8EQ("", d);
 	EXPECT_EQ(d, dp);
 	EXPECT_EQ(1, s);
@@ -81,91 +93,7 @@ TEST(CaseMappingExecuteUppercase, BasicLatinUnaffected)
 	EXPECT_EQ(0, e);
 }
 
-TEST(CaseMappingExecuteUppercase, TwoBytesUppercase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-
-	EXPECT_EQ(2, casemapping_execute(0x00000546, &dp, &s, UnicodeProperty_Uppercase, &e));
-	EXPECT_UTF8EQ("\xD5\x86", d);
-	EXPECT_EQ(d + 2, dp);
-	EXPECT_EQ(253, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteUppercase, TwoBytesLowercase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-
-	EXPECT_EQ(2, casemapping_execute(0x00000524, &dp, &s, UnicodeProperty_Uppercase, &e));
-	EXPECT_UTF8EQ("\xD4\xA4", d);
-	EXPECT_EQ(d + 2, dp);
-	EXPECT_EQ(253, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteUppercase, TwoBytesUnaffected)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-
-	EXPECT_EQ(2, casemapping_execute(0x00000482, &dp, &s, UnicodeProperty_Uppercase, &e));
-	EXPECT_UTF8EQ("\xD2\x82", d);
-	EXPECT_EQ(d + 2, dp);
-	EXPECT_EQ(253, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteUppercase, ThreeBytesUppercase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-
-	EXPECT_EQ(3, casemapping_execute(0x000010C4, &dp, &s, UnicodeProperty_Uppercase, &e));
-	EXPECT_UTF8EQ("\xE1\x83\x84", d);
-	EXPECT_EQ(d + 3, dp);
-	EXPECT_EQ(252, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteUppercase, ThreeBytesLowercase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-
-	EXPECT_EQ(3, casemapping_execute(0x00001E09, &dp, &s, UnicodeProperty_Uppercase, &e));
-	EXPECT_UTF8EQ("\xE1\xB8\x88", d);
-	EXPECT_EQ(d + 3, dp);
-	EXPECT_EQ(252, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteUppercase, ThreeBytesUnaffected)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-
-	EXPECT_EQ(3, casemapping_execute(0x00001FDD, &dp, &s, UnicodeProperty_Uppercase, &e));
-	EXPECT_UTF8EQ("\xE1\xBF\x9D", d);
-	EXPECT_EQ(d + 3, dp);
-	EXPECT_EQ(252, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteUppercase, FourBytesUppercase)
+TEST(CaseMappingExecuteUppercase, MultiByteUppercase)
 {
 	int32_t e = 0;
 	size_t s = 255;
@@ -179,33 +107,49 @@ TEST(CaseMappingExecuteUppercase, FourBytesUppercase)
 	EXPECT_EQ(0, e);
 }
 
-TEST(CaseMappingExecuteUppercase, FourBytesLowercase)
+TEST(CaseMappingExecuteUppercase, MultiByteLowercase)
 {
 	int32_t e = 0;
 	size_t s = 255;
 	char d[256] = { 0 };
 	char* dp = d;
 
-	EXPECT_EQ(4, casemapping_execute(0x0001043C, &dp, &s, UnicodeProperty_Uppercase, &e));
-	EXPECT_UTF8EQ("\xF0\x90\x90\x94", d);
-	EXPECT_EQ(d + 4, dp);
-	EXPECT_EQ(251, s);
+	EXPECT_EQ(6, casemapping_execute(0x00001FD2, &dp, &s, UnicodeProperty_Uppercase, &e));
+	EXPECT_UTF8EQ("\xCE\x99\xCC\x88\xCC\x80", d);
+	EXPECT_EQ(d + 6, dp);
+	EXPECT_EQ(249, s);
 	EXPECT_EQ(0, e);
 }
 
-TEST(CaseMappingExecuteUppercase, FourBytesUnaffected)
+TEST(CaseMappingExecuteUppercase, MultiByteTitlecase)
 {
 	int32_t e = 0;
 	size_t s = 255;
 	char d[256] = { 0 };
 	char* dp = d;
 
-	EXPECT_EQ(4, casemapping_execute(0x000118FF, &dp, &s, UnicodeProperty_Uppercase, &e));
-	EXPECT_UTF8EQ("\xF0\x91\xA3\xBF", d);
-	EXPECT_EQ(d + 4, dp);
-	EXPECT_EQ(251, s);
+	EXPECT_EQ(2, casemapping_execute(0x000001C8, &dp, &s, UnicodeProperty_Uppercase, &e));
+	EXPECT_UTF8EQ("\xC7\x87", d);
+	EXPECT_EQ(d + 2, dp);
+	EXPECT_EQ(253, s);
 	EXPECT_EQ(0, e);
 }
+
+TEST(CaseMappingExecuteUppercase, MultiByteUnaffected)
+{
+	int32_t e = 0;
+	size_t s = 255;
+	char d[256] = { 0 };
+	char* dp = d;
+
+	EXPECT_EQ(2, casemapping_execute(0x00000482, &dp, &s, UnicodeProperty_Uppercase, &e));
+	EXPECT_UTF8EQ("\xD2\x82", d);
+	EXPECT_EQ(d + 2, dp);
+	EXPECT_EQ(253, s);
+	EXPECT_EQ(0, e);
+}
+
+// Lowercase
 
 TEST(CaseMappingExecuteLowercase, BasicLatinUppercase)
 {
@@ -249,7 +193,7 @@ TEST(CaseMappingExecuteLowercase, BasicLatinUnaffected)
 	EXPECT_EQ(0, e);
 }
 
-TEST(CaseMappingExecuteLowercase, TwoBytesUppercase)
+TEST(CaseMappingExecuteLowercase, MultiByteUppercase)
 {
 	int32_t e = 0;
 	size_t s = 255;
@@ -263,49 +207,7 @@ TEST(CaseMappingExecuteLowercase, TwoBytesUppercase)
 	EXPECT_EQ(0, e);
 }
 
-TEST(CaseMappingExecuteLowercase, TwoBytesLowercase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-
-	EXPECT_EQ(2, casemapping_execute(0x000000EA, &dp, &s, UnicodeProperty_Lowercase, &e));
-	EXPECT_UTF8EQ("\xC3\xAA", d);
-	EXPECT_EQ(d + 2, dp);
-	EXPECT_EQ(253, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteLowercase, TwoBytesUnaffected)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-
-	EXPECT_EQ(2, casemapping_execute(0x000000D7, &dp, &s, UnicodeProperty_Lowercase, &e));
-	EXPECT_UTF8EQ("\xC3\x97", d);
-	EXPECT_EQ(d + 2, dp);
-	EXPECT_EQ(253, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteLowercase, ThreeBytesUppercase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-
-	EXPECT_EQ(3, casemapping_execute(0x00001E94, &dp, &s, UnicodeProperty_Lowercase, &e));
-	EXPECT_UTF8EQ("\xE1\xBA\x95", d);
-	EXPECT_EQ(d + 3, dp);
-	EXPECT_EQ(252, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteLowercase, ThreeBytesLowercase)
+TEST(CaseMappingExecuteLowercase, MultiByteLowercase)
 {
 	int32_t e = 0;
 	size_t s = 255;
@@ -319,49 +221,21 @@ TEST(CaseMappingExecuteLowercase, ThreeBytesLowercase)
 	EXPECT_EQ(0, e);
 }
 
-TEST(CaseMappingExecuteLowercase, ThreeBytesUnaffected)
+TEST(CaseMappingExecuteLowercase, MultiByteTitlecase)
 {
 	int32_t e = 0;
 	size_t s = 255;
 	char d[256] = { 0 };
 	char* dp = d;
 
-	EXPECT_EQ(3, casemapping_execute(0x00001FDE, &dp, &s, UnicodeProperty_Lowercase, &e));
-	EXPECT_UTF8EQ("\xE1\xBF\x9E", d);
+	EXPECT_EQ(3, casemapping_execute(0x00001FFC, &dp, &s, UnicodeProperty_Lowercase, &e));
+	EXPECT_UTF8EQ("\xE1\xBF\xB3", d);
 	EXPECT_EQ(d + 3, dp);
 	EXPECT_EQ(252, s);
 	EXPECT_EQ(0, e);
 }
 
-TEST(CaseMappingExecuteLowercase, FourBytesUppercase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-
-	EXPECT_EQ(4, casemapping_execute(0x00010422, &dp, &s, UnicodeProperty_Lowercase, &e));
-	EXPECT_UTF8EQ("\xF0\x90\x91\x8A", d);
-	EXPECT_EQ(d + 4, dp);
-	EXPECT_EQ(251, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteLowercase, FourBytesLowercase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-
-	EXPECT_EQ(4, casemapping_execute(0x0001044F, &dp, &s, UnicodeProperty_Lowercase, &e));
-	EXPECT_UTF8EQ("\xF0\x90\x91\x8F", d);
-	EXPECT_EQ(d + 4, dp);
-	EXPECT_EQ(251, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteLowercase, FourBytesUnaffected)
+TEST(CaseMappingExecuteLowercase, MultiByteUnaffected)
 {
 	int32_t e = 0;
 	size_t s = 255;
