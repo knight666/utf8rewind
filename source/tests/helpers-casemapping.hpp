5efa19bf0585345e698b7bede3c2b9e261adfca5
Quinten <quintenlansu@gmail.com>
2015-01-07 08:57:07 +0100

    helpers-casemapping: Added titlecase to check macro. integration: Titlecase is added to tests.

--- source/tests/helpers-casemapping.hpp
+++ source/tests/helpers-casemapping.hpp
@@ -4,15 +4,17 @@
 
 #include "utf8rewind.h"
 
-#define CHECK_UTF8_CASEMAPPING(_codepoint, _uppercase, _lowercase, _name) { \
+#define CHECK_UTF8_CASEMAPPING(_codepoint, _uppercase, _lowercase, _titlecase, _name) { \
 	::helpers::CaseMappingEntry e; \
 	e.codepoint = _codepoint; \
 	e.uppercase = _uppercase; \
 	e.lowercase = _lowercase; \
+	e.titlecase = _titlecase; \
 	e.name = _name; \
 	::helpers::CaseMappingEntry a; \
 	a.uppercase = ::helpers::uppercase(_codepoint); \
 	a.lowercase = ::helpers::lowercase(_codepoint); \
+	a.titlecase = ::helpers::titlecase(_codepoint); \
 	EXPECT_PRED_FORMAT2(::helpers::CompareCasemapping, e, a); \
 }
 
@@ -37,6 +39,7 @@ namespace helpers {
 		unicode_t codepoint;
 		std::string uppercase;
 		std::string lowercase;
+		std::string titlecase;
 		std::string name;
 	};
 
