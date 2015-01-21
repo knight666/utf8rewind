e5fd425a3dc0ce326756c625e6fb873362aef368
Anne <knight666+github@gmail.com>
2015-01-21 12:06:17 -0500

    suite-stream-reorder: Refactored test to use correct data.

--- source/tests/suite-stream-reorder.cpp
+++ source/tests/suite-stream-reorder.cpp
@@ -7,14 +7,25 @@ extern "C" {
 
 #include "helpers-strings.hpp"
 
+StreamState CreateStream(const char* text)
+{
+	StreamState stream = { 0 };
+
+	std::vector<unicode_t> converted = helpers::utf32(text);
+
+	for (std::vector<unicode_t>::iterator it = converted.begin(); it != converted.end(); ++it)
+	{
+		stream.codepoint[stream.current] = *it;
+		stream.canonical_combining_class[stream.current] = database_queryproperty(*it, UnicodeProperty_CanonicalCombiningClass);
+		stream.current++;
+	}
+
+	return stream;
+}
+
 TEST(StreamReorder, Unchanged)
 {
-	const char* i = "Bike";
-	size_t il = strlen(i);
-
-	StreamState stream;
-	EXPECT_EQ(1, stream_initialize(&stream, i, il, UnicodeProperty_Normalization_Compatibility_Compose));
-	EXPECT_EQ(1, stream_execute(&stream));
+	StreamState stream = CreateStream("Bike");
 
 	EXPECT_EQ(1, stream_reorder(&stream));
 	EXPECT_UTF8EQ("Bike", helpers::utf8(stream.codepoint, stream.current * sizeof(unicode_t)).c_str());
