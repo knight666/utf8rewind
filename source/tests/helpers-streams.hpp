33b6cd1b39feca8f13252472d51c5be6bc4f6fc4
Quinten <knight666+github@gmail.com>
2015-01-22 10:13:30 +0100

    Removed CHECK_STREAM_END macro. Renamed CHECK_STREAM to CHECK_STREAM_ENTRY.

--- source/tests/helpers-streams.hpp
+++ source/tests/helpers-streams.hpp
@@ -9,25 +9,13 @@ extern "C" {
 	#include "../internal/streaming.h"
 }
 
-#define CHECK_STREAM(_stream, _index, _codepoint, _qc, _ccc) { \
+#define CHECK_STREAM_ENTRY(_stream, _index, _codepoint, _qc, _ccc) { \
 	::helpers::StreamEntry e; \
 	e.codepoint = _codepoint; \
 	e.quick_check = QuickCheckResult_ ## _qc; \
 	e.canonical_combining_class = _ccc; \
 	::helpers::StreamEntry a; \
 	a.index = _index; \
-	a.codepoint = _stream.codepoint[_index]; \
-	a.quick_check = _stream.quick_check[_index]; \
-	a.canonical_combining_class = _stream.canonical_combining_class[_index]; \
-	EXPECT_PRED_FORMAT2(::helpers::CompareStream, e, a); \
-}
-
-#define CHECK_STREAM_END(_stream, _index) { \
-	::helpers::StreamEntry e; \
-	e.codepoint = 0; \
-	e.quick_check = QuickCheckResult_Yes; \
-	e.canonical_combining_class = 0; \
-	::helpers::StreamEntry a; \
 	a.codepoint = _stream.codepoint[_index]; \
 	a.quick_check = _stream.quick_check[_index]; \
 	a.canonical_combining_class = _stream.canonical_combining_class[_index]; \
