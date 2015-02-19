71d141fa5c5041226bdb2e50aeed56e33feef461
Quinten <quintenlansu@gmail.com>
2015-02-19 16:45:17 +0100

    suite-casemapping-execute: Added tests for multiple multi-byte codepoints.

--- source/tests/suite-casemapping-execute.cpp
+++ source/tests/suite-casemapping-execute.cpp
@@ -511,306 +511,212 @@ TEST(CaseMappingExecute, GeneralCategoryCaseMappedSingleNotEnoughSpace)
 	EXPECT_UTF8EQ("", o);
 }
 
-TEST(CaseMappingExecuteUppercase, BasicLatinLowercase)
+TEST(CaseMappingExecute, GeneralCategoryCaseMappedMultipleLowercase)
 {
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x00000065, UnicodeProperty_GeneralCategory);
+	// 01A4 017D 015E
+	// 01A5 017E 015F
 
-	EXPECT_EQ(1, casemapping_execute(0x00000065, &dp, &s, gc, UnicodeProperty_Uppercase, &e));
-	EXPECT_UTF8EQ("E", d);
-	EXPECT_EQ(d + 1, dp);
-	EXPECT_EQ(254, s);
-	EXPECT_EQ(0, e);
+	CaseMappingState state;
+	const char* i = "\xC6\xA4\xC5\xBD\xC5\x9E";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+
+	EXPECT_TRUE(casemapping_initialize(&state, i, is, o, os, UnicodeProperty_Lowercase));
+
+	EXPECT_EQ(2, casemapping_execute2(&state));
+	EXPECT_EQ(i + 2, state.src);
+	EXPECT_EQ(is - 2, state.src_size);
+	EXPECT_EQ(o + 2, state.dst);
+	EXPECT_EQ(os - 2, state.dst_size);
+
+	EXPECT_EQ(2, casemapping_execute2(&state));
+	EXPECT_EQ(i + 4, state.src);
+	EXPECT_EQ(is - 4, state.src_size);
+	EXPECT_EQ(o + 4, state.dst);
+	EXPECT_EQ(os - 4, state.dst_size);
+
+	EXPECT_EQ(2, casemapping_execute2(&state));
+	EXPECT_EQ(i + 6, state.src);
+	EXPECT_EQ(is - 6, state.src_size);
+	EXPECT_EQ(o + 6, state.dst);
+	EXPECT_EQ(os - 6, state.dst_size);
+
+	EXPECT_EQ(0, casemapping_execute2(&state));
+
+	EXPECT_UTF8EQ("\xC6\xA5\xC5\xBE\xC5\x9F", o);
 }
 
-TEST(CaseMappingExecuteUppercase, BasicLatinUnaffected)
+TEST(CaseMappingExecute, GeneralCategoryCaseMappedMultipleUppercase)
 {
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x0000005D, UnicodeProperty_GeneralCategory);
+	// 0149 00DF
+	// 02BC 004E 0053 0053
 
-	EXPECT_EQ(1, casemapping_execute(0x0000005D, &dp, &s, gc, UnicodeProperty_Uppercase, &e));
-	EXPECT_UTF8EQ("]", d);
-	EXPECT_EQ(d + 1, dp);
-	EXPECT_EQ(254, s);
-	EXPECT_EQ(0, e);
+	CaseMappingState state;
+	const char* i = "\xC5\x89\xC3\x9F";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+
+	EXPECT_TRUE(casemapping_initialize(&state, i, is, o, os, UnicodeProperty_Uppercase));
+
+	EXPECT_EQ(3, casemapping_execute2(&state));
+	EXPECT_EQ(i + 2, state.src);
+	EXPECT_EQ(is - 2, state.src_size);
+	EXPECT_EQ(o + 3, state.dst);
+	EXPECT_EQ(os - 3, state.dst_size);
+
+	EXPECT_EQ(2, casemapping_execute2(&state));
+	EXPECT_EQ(i + 4, state.src);
+	EXPECT_EQ(is - 4, state.src_size);
+	EXPECT_EQ(o + 5, state.dst);
+	EXPECT_EQ(os - 5, state.dst_size);
+
+	EXPECT_EQ(0, casemapping_execute2(&state));
+
+	EXPECT_UTF8EQ("\xCA\xBCNSS", o);
 }
 
-TEST(CaseMappingExecuteUppercase, MultiByteUppercase)
+TEST(CaseMappingExecute, GeneralCategoryCaseMappedMultipleTitlecase)
 {
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x00010406, UnicodeProperty_GeneralCategory);
+	// 01C7 01DC 01D0
+	// 01C8 01DB 01CF
 
-	EXPECT_EQ(4, casemapping_execute(0x00010406, &dp, &s, gc, UnicodeProperty_Uppercase, &e));
-	EXPECT_UTF8EQ("\xF0\x90\x90\x86", d);
-	EXPECT_EQ(d + 4, dp);
-	EXPECT_EQ(251, s);
-	EXPECT_EQ(0, e);
+	CaseMappingState state;
+	const char* i = "\xC7\x87\xC7\x9C\xC7\x90";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+
+	EXPECT_TRUE(casemapping_initialize(&state, i, is, o, os, UnicodeProperty_Titlecase));
+
+	EXPECT_EQ(2, casemapping_execute2(&state));
+	EXPECT_EQ(i + 2, state.src);
+	EXPECT_EQ(is - 2, state.src_size);
+	EXPECT_EQ(o + 2, state.dst);
+	EXPECT_EQ(os - 2, state.dst_size);
+
+	EXPECT_EQ(2, casemapping_execute2(&state));
+	EXPECT_EQ(i + 4, state.src);
+	EXPECT_EQ(is - 4, state.src_size);
+	EXPECT_EQ(o + 4, state.dst);
+	EXPECT_EQ(os - 4, state.dst_size);
+
+	EXPECT_EQ(2, casemapping_execute2(&state));
+	EXPECT_EQ(i + 6, state.src);
+	EXPECT_EQ(is - 6, state.src_size);
+	EXPECT_EQ(o + 6, state.dst);
+	EXPECT_EQ(os - 6, state.dst_size);
+
+	EXPECT_EQ(0, casemapping_execute2(&state));
+
+	EXPECT_UTF8EQ("\xC7\x88\xC7\x9B\xC7\x8F", o);
 }
 
-TEST(CaseMappingExecuteUppercase, MultiByteLowercase)
+TEST(CaseMappingExecute, GeneralCategoryCaseMappedMultipleUnaffected)
 {
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x00001FD2, UnicodeProperty_GeneralCategory);
+	// 1F5D8 1AA3 3010 1F64B
+	// 1F5D8 1AA3 3010 1F64B
 
-	EXPECT_EQ(6, casemapping_execute(0x00001FD2, &dp, &s, gc, UnicodeProperty_Uppercase, &e));
-	EXPECT_UTF8EQ("\xCE\x99\xCC\x88\xCC\x80", d);
-	EXPECT_EQ(d + 6, dp);
-	EXPECT_EQ(249, s);
-	EXPECT_EQ(0, e);
+	CaseMappingState state;
+	const char* i = "\xF0\x9F\x97\x98\xE1\xAA\xA3\xE3\x80\x90\xF0\x9F\x99\x8B";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+
+	EXPECT_TRUE(casemapping_initialize(&state, i, is, o, os, UnicodeProperty_Uppercase));
+
+	EXPECT_EQ(4, casemapping_execute2(&state));
+	EXPECT_EQ(i + 4, state.src);
+	EXPECT_EQ(is - 4, state.src_size);
+	EXPECT_EQ(o + 4, state.dst);
+	EXPECT_EQ(os - 4, state.dst_size);
+
+	EXPECT_EQ(3, casemapping_execute2(&state));
+	EXPECT_EQ(i + 7, state.src);
+	EXPECT_EQ(is - 7, state.src_size);
+	EXPECT_EQ(o + 7, state.dst);
+	EXPECT_EQ(os - 7, state.dst_size);
+
+	EXPECT_EQ(3, casemapping_execute2(&state));
+	EXPECT_EQ(i + 10, state.src);
+	EXPECT_EQ(is - 10, state.src_size);
+	EXPECT_EQ(o + 10, state.dst);
+	EXPECT_EQ(os - 10, state.dst_size);
+
+	EXPECT_EQ(4, casemapping_execute2(&state));
+	EXPECT_EQ(i + 14, state.src);
+	EXPECT_EQ(is - 14, state.src_size);
+	EXPECT_EQ(o + 14, state.dst);
+	EXPECT_EQ(os - 14, state.dst_size);
+
+	EXPECT_EQ(0, casemapping_execute2(&state));
+
+	EXPECT_UTF8EQ("\xF0\x9F\x97\x98\xE1\xAA\xA3\xE3\x80\x90\xF0\x9F\x99\x8B", o);
 }
 
-TEST(CaseMappingExecuteUppercase, MultiByteTitlecase)
+TEST(CaseMappingExecute, GeneralCategoryCaseMappedMultipleAmountOfBytes)
 {
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x000001C8, UnicodeProperty_GeneralCategory);
+	// 0130 0390 041A
+	// 0069 0307 0390 043A
 
-	EXPECT_EQ(2, casemapping_execute(0x000001C8, &dp, &s, gc, UnicodeProperty_Uppercase, &e));
-	EXPECT_UTF8EQ("\xC7\x87", d);
-	EXPECT_EQ(d + 2, dp);
-	EXPECT_EQ(253, s);
-	EXPECT_EQ(0, e);
+	CaseMappingState state;
+	const char* i = "\xC4\xB0\xCE\x90\xD0\x9A";
+	size_t is = strlen(i);
+
+	EXPECT_TRUE(casemapping_initialize(&state, i, is, nullptr, 0, UnicodeProperty_Lowercase));
+
+	EXPECT_EQ(3, casemapping_execute2(&state));
+	EXPECT_EQ(i + 2, state.src);
+	EXPECT_EQ(is - 2, state.src_size);
+	EXPECT_EQ(nullptr, state.dst);
+	EXPECT_EQ(0, state.dst_size);
+
+	EXPECT_EQ(2, casemapping_execute2(&state));
+	EXPECT_EQ(i + 4, state.src);
+	EXPECT_EQ(is - 4, state.src_size);
+	EXPECT_EQ(nullptr, state.dst);
+	EXPECT_EQ(0, state.dst_size);
+
+	EXPECT_EQ(2, casemapping_execute2(&state));
+	EXPECT_EQ(i + 6, state.src);
+	EXPECT_EQ(is - 6, state.src_size);
+	EXPECT_EQ(nullptr, state.dst);
+	EXPECT_EQ(0, state.dst_size);
+
+	EXPECT_EQ(0, casemapping_execute2(&state));
 }
 
-TEST(CaseMappingExecuteUppercase, MultiByteUnaffected)
+TEST(CaseMappingExecute, GeneralCategoryCaseMappedMultipleNotEnoughSpace)
 {
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x00000482, UnicodeProperty_GeneralCategory);
+	// 03E4 03B0 0390
+	// 03E4 03A5 0308 0301 0399 0308 0301
 
-	EXPECT_EQ(2, casemapping_execute(0x00000482, &dp, &s, gc, UnicodeProperty_Uppercase, &e));
-	EXPECT_UTF8EQ("\xD2\x82", d);
-	EXPECT_EQ(d + 2, dp);
-	EXPECT_EQ(253, s);
-	EXPECT_EQ(0, e);
-}
+	CaseMappingState state;
+	const char* i = "\xCF\xA4\xCE\xB0\xCE\x90";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 9;
 
-// Lowercase
+	EXPECT_TRUE(casemapping_initialize(&state, i, is, o, os, UnicodeProperty_Uppercase));
 
-TEST(CaseMappingExecuteLowercase, BasicLatinUppercase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x0000006D, UnicodeProperty_GeneralCategory);
+	EXPECT_EQ(2, casemapping_execute2(&state));
+	EXPECT_EQ(i + 2, state.src);
+	EXPECT_EQ(is - 2, state.src_size);
+	EXPECT_EQ(o + 2, state.dst);
+	EXPECT_EQ(os - 2, state.dst_size);
 
-	EXPECT_EQ(1, casemapping_execute(0x0000006D, &dp, &s, gc, UnicodeProperty_Lowercase, &e));
-	EXPECT_UTF8EQ("m", d);
-	EXPECT_EQ(d + 1, dp);
-	EXPECT_EQ(254, s);
-	EXPECT_EQ(0, e);
-}
+	EXPECT_EQ(6, casemapping_execute2(&state));
+	EXPECT_EQ(i + 4, state.src);
+	EXPECT_EQ(is - 4, state.src_size);
+	EXPECT_EQ(o + 8, state.dst);
+	EXPECT_EQ(os - 8, state.dst_size);
 
-TEST(CaseMappingExecuteLowercase, BasicLatinLowercase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x00000054, UnicodeProperty_GeneralCategory);
+	EXPECT_EQ(0, casemapping_execute2(&state));
+	EXPECT_EQ(i + 4, state.src);
+	EXPECT_EQ(is - 6, state.src_size);
+	EXPECT_EQ(o + 8, state.dst);
+	EXPECT_EQ(os - 8, state.dst_size);
 
-	EXPECT_EQ(1, casemapping_execute(0x00000054, &dp, &s, gc, UnicodeProperty_Lowercase, &e));
-	EXPECT_UTF8EQ("t", d);
-	EXPECT_EQ(d + 1, dp);
-	EXPECT_EQ(254, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteLowercase, BasicLatinUnaffected)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x0000007D, UnicodeProperty_GeneralCategory);
-
-	EXPECT_EQ(1, casemapping_execute(0x0000007D, &dp, &s, gc, UnicodeProperty_Lowercase, &e));
-	EXPECT_UTF8EQ("}", d);
-	EXPECT_EQ(d + 1, dp);
-	EXPECT_EQ(254, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteLowercase, MultiByteUppercase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x000000D8, UnicodeProperty_GeneralCategory);
-
-	EXPECT_EQ(2, casemapping_execute(0x000000D8, &dp, &s, gc, UnicodeProperty_Lowercase, &e));
-	EXPECT_UTF8EQ("\xC3\xB8", d);
-	EXPECT_EQ(d + 2, dp);
-	EXPECT_EQ(253, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteLowercase, MultiByteLowercase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x00001E09, UnicodeProperty_GeneralCategory);
-
-	EXPECT_EQ(3, casemapping_execute(0x00001E09, &dp, &s, gc, UnicodeProperty_Lowercase, &e));
-	EXPECT_UTF8EQ("\xE1\xB8\x89", d);
-	EXPECT_EQ(d + 3, dp);
-	EXPECT_EQ(252, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteLowercase, MultiByteTitlecase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x00001FFC, UnicodeProperty_GeneralCategory);
-
-	EXPECT_EQ(3, casemapping_execute(0x00001FFC, &dp, &s, gc, UnicodeProperty_Lowercase, &e));
-	EXPECT_UTF8EQ("\xE1\xBF\xB3", d);
-	EXPECT_EQ(d + 3, dp);
-	EXPECT_EQ(252, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteLowercase, MultiByteUnaffected)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x000118E1, UnicodeProperty_GeneralCategory);
-
-	EXPECT_EQ(4, casemapping_execute(0x000118E1, &dp, &s, gc, UnicodeProperty_Lowercase, &e));
-	EXPECT_UTF8EQ("\xF0\x91\xA3\xA1", d);
-	EXPECT_EQ(d + 4, dp);
-	EXPECT_EQ(251, s);
-	EXPECT_EQ(0, e);
-}
-
-// Titlecase
-
-TEST(CaseMappingExecuteTitlecase, BasicLatinUppercase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x00000056, UnicodeProperty_GeneralCategory);
-
-	EXPECT_EQ(1, casemapping_execute(0x00000056, &dp, &s, gc, UnicodeProperty_Titlecase, &e));
-	EXPECT_UTF8EQ("V", d);
-	EXPECT_EQ(d + 1, dp);
-	EXPECT_EQ(254, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteTitlecase, BasicLatinLowercase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x0000006F, UnicodeProperty_GeneralCategory);
-
-	EXPECT_EQ(1, casemapping_execute(0x0000006F, &dp, &s, gc, UnicodeProperty_Titlecase, &e));
-	EXPECT_UTF8EQ("O", d);
-	EXPECT_EQ(d + 1, dp);
-	EXPECT_EQ(254, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteTitlecase, BasicLatinUnaffected)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x00000036, UnicodeProperty_GeneralCategory);
-
-	EXPECT_EQ(1, casemapping_execute(0x00000036, &dp, &s, gc, UnicodeProperty_Titlecase, &e));
-	EXPECT_UTF8EQ("6", d);
-	EXPECT_EQ(d + 1, dp);
-	EXPECT_EQ(254, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteTitlecase, MultiByteUppercase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x000001F2, UnicodeProperty_GeneralCategory);
-
-	EXPECT_EQ(2, casemapping_execute(0x000001F2, &dp, &s, gc, UnicodeProperty_Titlecase, &e));
-	EXPECT_UTF8EQ("\xC7\xB2", d);
-	EXPECT_EQ(d + 2, dp);
-	EXPECT_EQ(253, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteTitlecase, MultiByteLowercase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x00000587, UnicodeProperty_GeneralCategory);
-
-	EXPECT_EQ(4, casemapping_execute(0x00000587, &dp, &s, gc, UnicodeProperty_Titlecase, &e));
-	EXPECT_UTF8EQ("\xD4\xB5\xD6\x82", d);
-	EXPECT_EQ(d + 4, dp);
-	EXPECT_EQ(251, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteTitlecase, MultiByteTitlecase)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x000001C8, UnicodeProperty_GeneralCategory);
-
-	EXPECT_EQ(2, casemapping_execute(0x000001C8, &dp, &s, gc, UnicodeProperty_Titlecase, &e));
-	EXPECT_UTF8EQ("\xC7\x88", d);
-	EXPECT_EQ(d + 2, dp);
-	EXPECT_EQ(253, s);
-	EXPECT_EQ(0, e);
-}
-
-TEST(CaseMappingExecuteTitlecase, MultiByteUnaffected)
-{
-	int32_t e = 0;
-	size_t s = 255;
-	char d[256] = { 0 };
-	char* dp = d;
-	uint8_t gc = database_queryproperty(0x000000F7, UnicodeProperty_GeneralCategory);
-
-	EXPECT_EQ(2, casemapping_execute(0x000000F7, &dp, &s, gc, UnicodeProperty_Titlecase, &e));
-	EXPECT_UTF8EQ("\xC3\xB7", d);
-	EXPECT_EQ(d + 2, dp);
-	EXPECT_EQ(253, s);
-	EXPECT_EQ(0, e);
+	EXPECT_UTF8EQ("\xCF\xA4\xCE\xA5\xCC\x88\xCC\x81", o);
 }
