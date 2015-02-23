97bd0565a8cff65297997a5e92e279ad249c0b80
Anne <knight666@gmail.com>
2015-02-23 08:14:04 +0100

    suite-utf8-normalize-decomposition: Added descriptive comments to tests.

--- source/tests/suite-utf8-normalize-decomposition.cpp
+++ source/tests/suite-utf8-normalize-decomposition.cpp
@@ -10,6 +10,12 @@ extern "C" {
 
 TEST(NormalizeDecomposition, BasicLatinSingle)
 {
+	/*
+		U+007A
+		     Y
+		     0
+	*/
+
 	const char* i = "z";
 	size_t is = strlen(i);
 	char o[256] = { 0 };
@@ -23,6 +29,12 @@ TEST(NormalizeDecomposition, BasicLatinSingle)
 
 TEST(NormalizeDecomposition, BasicLatinMultiple)
 {
+	/*
+		U+0048 U+006F U+006D U+0065
+		     Y      Y      Y      Y
+		     0      0      0      0
+	*/
+
 	const char* i = "Home";
 	size_t is = strlen(i);
 	char o[256] = { 0 };
@@ -36,6 +48,12 @@ TEST(NormalizeDecomposition, BasicLatinMultiple)
 
 TEST(NormalizeDecomposition, BasicLatinAmountOfBytes)
 {
+	/*
+		U+0024 U+0020 U+0034 U+002E U+0032 U+0035 U+0020 U+0070 U+0065 U+0072 U+0020 U+0070 U+006F U+0075 U+006E U+0064
+		     Y      Y      Y      Y      Y      Y      Y      Y      Y      Y      Y      Y      Y      Y      Y      Y
+		     0      0      0      0      0      0      0      0      0      0      0      0      0      0      0      0
+	*/
+
 	const char* i = "$ 4.25 per pound";
 	size_t is = strlen(i);
 	int32_t errors = 0;
@@ -46,6 +64,12 @@ TEST(NormalizeDecomposition, BasicLatinAmountOfBytes)
 
 TEST(NormalizeDecomposition, BasicLatinNotEnoughSpace)
 {
+	/*
+		U+0052 U+0061 U+0069 U+006E U+0079 U+0020 U+0064 U+0061 U+0079
+		     Y      Y      Y      Y      Y      Y      Y      Y      Y
+		     0      0      0      0      0      0      0      0      0
+	*/
+
 	const char* i = "Rainy day";
 	size_t is = strlen(i);
 	char o[256] = { 0 };
@@ -59,6 +83,12 @@ TEST(NormalizeDecomposition, BasicLatinNotEnoughSpace)
 
 TEST(NormalizeDecomposition, MultiByteUnaffectedSingle)
 {
+	/*
+		U+00B5
+		     Y
+		     0
+	*/
+
 	const char* i = "\xC2\xB5";
 	size_t is = strlen(i);
 	char o[256] = { 0 };
@@ -72,6 +102,12 @@ TEST(NormalizeDecomposition, MultiByteUnaffectedSingle)
 
 TEST(NormalizeDecomposition, MultiByteUnaffectedMultiple)
 {
+	/*
+		U+1AA8 U+1A80 U+1A87
+		     Y      Y      Y
+		     0      0      0
+	*/
+
 	const char* i = "\xE1\xAA\xA8\xE1\xAA\x80\xE1\xAA\x87";
 	size_t is = strlen(i);
 	char o[256] = { 0 };
@@ -85,9 +121,11 @@ TEST(NormalizeDecomposition, MultiByteUnaffectedMultiple)
 
 TEST(NormalizeDecomposition, MultiByteUnaffectedAmountOfBytes)
 {
-	// ROMAN NUMERAL FIVE THOUSAND
-	// DOUBLE-STRUCK ITALIC CAPITAL D
-	// SUBSCRIPT SIX
+	/*
+		U+2181 U+2145 U+2086
+		     Y      Y      Y
+		     0      0      0
+	*/
 
 	const char* i = "\xE2\x86\x81\xE2\x85\x85\xE2\x82\x86";
 	size_t is = strlen(i);
@@ -118,6 +156,12 @@ TEST(NormalizeDecomposition, MultiByteUnaffectedNotEnoughSpace)
 
 TEST(NormalizeDecomposition, MultiByteDecomposeSingle)
 {
+	/*
+		U+0958
+		     N
+		     0
+	*/
+
 	const char* i = "\xE0\xA5\x98";
 	size_t is = strlen(i);
 	char o[256] = { 0 };
@@ -131,6 +175,12 @@ TEST(NormalizeDecomposition, MultiByteDecomposeSingle)
 
 TEST(NormalizeDecomposition, MultiByteDecomposeSequenceSingleOrdered)
 {
+	/*
+		U+0108 U+0301
+		     N      Y
+		     0    230
+	*/
+
 	const char* i = "\xC4\x88\xCC\x81";
 	size_t is = strlen(i);
 	char o[256] = { 0 };
@@ -144,6 +194,12 @@ TEST(NormalizeDecomposition, MultiByteDecomposeSequenceSingleOrdered)
 
 TEST(NormalizeDecomposition, MultiByteDecomposeSequenceSingleUnordered)
 {
+	/*
+		U+0041 U+0304 U+031D
+		     Y      Y      Y
+		     0    230    220
+	*/
+
 	const char* i = "A\xCC\x84\xCC\x9D";
 	size_t is = strlen(i);
 	char o[256] = { 0 };
@@ -157,6 +213,12 @@ TEST(NormalizeDecomposition, MultiByteDecomposeSequenceSingleUnordered)
 
 TEST(NormalizeDecomposition, MultiByteDecomposeSequenceMultipleOrdered)
 {
+	/*
+		U+0202 U+0315 U+038E U+0301
+		     N      Y      N      Y
+		     0    232      0    230
+	*/
+
 	const char* i = "\xC8\x82\xCC\x95\xCE\x8E\xCC\x81";
 	size_t is = strlen(i);
 	char o[256] = { 0 };
@@ -170,6 +232,12 @@ TEST(NormalizeDecomposition, MultiByteDecomposeSequenceMultipleOrdered)
 
 TEST(NormalizeDecomposition, MultiByteDecomposeSequenceMultipleUnordered)
 {
+	/*
+		U+00CA U+0347 U+00C3 U+035C U+0348 U+00ED U+031B
+		     N      Y      N      Y      Y      N      Y
+		     0    220      0    233    220      0    216
+	*/
+
 	const char* i = "\xC3\x8A\xCD\x87\xC3\x83\xCD\x9C\xCD\x88\xC3\xAD\xCC\x9B";
 	size_t is = strlen(i);
 	char o[256] = { 0 };
@@ -184,6 +252,12 @@ TEST(NormalizeDecomposition, MultiByteDecomposeSequenceMultipleUnordered)
 
 TEST(NormalizeDecomposition, HangulUnaffectedSingle)
 {
+	/*
+		U+11BD
+		     Y
+		     0
+	*/
+
 	const char* i = "\xE1\x86\xBD";
 	size_t is = strlen(i);
 	char o[256] = { 0 };
@@ -197,6 +271,12 @@ TEST(NormalizeDecomposition, HangulUnaffectedSingle)
 
 TEST(NormalizeDecomposition, HangulDecomposeSingleTwoCodepoints)
 {
+	/*
+		U+AC70
+		     N
+		     0
+	*/
+
 	const char* i = "\xEA\xB1\xB0";
 	size_t is = strlen(i);
 	char o[256] = { 0 };
@@ -210,6 +290,12 @@ TEST(NormalizeDecomposition, HangulDecomposeSingleTwoCodepoints)
 
 TEST(NormalizeDecomposition, HangulDecomposeSingleThreeCodepoints)
 {
+	/*
+		U+AD83
+		     N
+		     0
+	*/
+
 	const char* i = "\xEA\xB6\x83";
 	size_t is = strlen(i);
 	char o[256] = { 0 };
@@ -223,6 +309,12 @@ TEST(NormalizeDecomposition, HangulDecomposeSingleThreeCodepoints)
 
 TEST(NormalizeDecomposition, HangulDecomposeSequenceMultiple)
 {
+	/*
+		U+ACE0 U+1100 U+1169 U+11B0 U+ACFC
+		     N      Y      Y      Y      N
+		     0      0      0      0      0
+	*/
+
 	const char* i = "\xEA\xB3\xA0\xE1\x84\x80\xE1\x85\xA9\xE1\x86\xB0\xEA\xB3\xBC";
 	size_t is = strlen(i);
 	char o[256] = { 0 };
@@ -237,6 +329,12 @@ TEST(NormalizeDecomposition, HangulDecomposeSequenceMultiple)
 
 TEST(NormalizeDecomposition, InvalidCodepointSingle)
 {
+	/*
+		U+FFFD
+		     Y
+		     0
+	*/
+
 	const char* i = "\xF4";
 	size_t is = strlen(i);
 	char o[256] = { 0 };
@@ -250,6 +348,12 @@ TEST(NormalizeDecomposition, InvalidCodepointSingle)
 
 TEST(NormalizeDecomposition, InvalidCodepointMultiple)
 {
+	/*
+		U+FFFD U+FFFD U+FFFD
+		     Y      Y      Y
+		     0      0      0
+	*/
+
 	const char* i = "\xEA\xF4\xC8";
 	size_t is = strlen(i);
 	char o[256] = { 0 };
@@ -318,6 +422,8 @@ TEST(NormalizeDecomposition, NotEnoughSpace)
 	char o[5] = { 0 };
 	size_t os = 4;
 	int32_t errors = 0;
+
+	std::string seq = helpers::sequence(i, UnicodeProperty_Normalization_Decompose);
 
 	EXPECT_EQ(3, utf8normalize(i, is, o, os, UTF8_NORMALIZE_DECOMPOSE, &errors));
 	EXPECT_UTF8EQ("a\xD6\xAE", o);
