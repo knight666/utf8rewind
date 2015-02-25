0472539316c410efd7b42fad62378c40ef545b32
Anne <knight666+github@gmail.com>
2015-02-25 08:28:27 +0100

    suite-utf8-normalize-composition: Added tests for multi-byte unaffected codepoints.

--- source/tests/suite-utf8-normalize-compose.cpp
+++ source/tests/suite-utf8-normalize-compose.cpp
@@ -99,3 +99,95 @@ TEST(NormalizeCompose, BasicLatinNotEnoughSpace)
 	EXPECT_UTF8EQ("App", o);
 	EXPECT_EQ(UTF8_ERR_NOT_ENOUGH_SPACE, errors);
 }
+
+TEST(NormalizeCompose, MultiByteUnaffectedSingle)
+{
+	/*
+		U+20D7
+		     Y
+		     0
+	*/
+
+	const char* i = "\xE2\x83\x97";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(3, utf8normalize(i, is, o, os, UTF8_NORMALIZE_COMPOSE, &errors));
+	EXPECT_UTF8EQ("\xE2\x83\x97", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeCompose, MultiByteUnaffectedMultiple)
+{
+	/*
+		U+206F U+1E9F U+202B
+		     Y      Y      Y
+		     0      0      0
+	*/
+
+	const char* i = "\xE2\x81\xAF\xE1\xBA\x9F\xE2\x80\xAB";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(9, utf8normalize(i, is, o, os, UTF8_NORMALIZE_COMPOSE, &errors));
+	EXPECT_UTF8EQ("\xE2\x81\xAF\xE1\xBA\x9F\xE2\x80\xAB", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeCompose, MultiByteUnaffectedCompatibility)
+{
+	/*
+		U+20A0 U+20AC U+20B0 U+20AF
+		     Y      Y      Y      Y
+		     0      0      0      0
+	*/
+
+	const char* i = "\xE2\x82\xA0\xE2\x82\xAC\xE2\x82\xB0\xE2\x82\xAF";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 255;
+	int32_t errors = 0;
+
+	EXPECT_EQ(12, utf8normalize(i, is, o, os, UTF8_NORMALIZE_COMPOSE, &errors));
+	EXPECT_UTF8EQ("\xE2\x82\xA0\xE2\x82\xAC\xE2\x82\xB0\xE2\x82\xAF", o);
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeCompose, MultiByteUnaffectedAmountOfBytes)
+{
+	/*
+		U+211C U+211F U+2122
+		     Y      Y      Y
+		     0      0      0
+	*/
+
+	const char* i = "\xE2\x84\x9C\xE2\x84\x9F\xE2\x84\xA2";
+	size_t is = strlen(i);
+	int32_t errors = 0;
+
+	EXPECT_EQ(9, utf8normalize(i, is, nullptr, 0, UTF8_NORMALIZE_COMPOSE, &errors));
+	EXPECT_EQ(0, errors);
+}
+
+TEST(NormalizeCompose, MultiByteUnaffectedNotEnoughSpace)
+{
+	/*
+		U+221A U+2215 U+2181 U+21D4
+		     Y      Y      Y      Y
+		     0      0      0      0
+	*/
+
+	const char* i = "\xE2\x88\x9A\xE2\x88\x95\xE2\x86\x81\xE2\x87\x94";
+	size_t is = strlen(i);
+	char o[256] = { 0 };
+	size_t os = 6;
+	int32_t errors = 0;
+
+	EXPECT_EQ(6, utf8normalize(i, is, o, os, UTF8_NORMALIZE_COMPOSE, &errors));
+	EXPECT_UTF8EQ("\xE2\x88\x9A\xE2\x88\x95", o);
+	EXPECT_EQ(UTF8_ERR_NOT_ENOUGH_SPACE, errors);
+}
