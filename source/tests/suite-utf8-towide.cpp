858f2b993d530bbaf306f43ffa5cbf6ed2546984
Anne <knight666+github@gmail.com>
2015-02-27 08:20:20 +0100

    Renamed suite-towdie to suite-utf8-towide.

--- source/tests/suite-utf8-towide.cpp
+++ source/tests/suite-utf8-towide.cpp
@@ -2,7 +2,7 @@
 
 #include "utf8rewind.h"
 
-TEST(ToWide, String)
+TEST(Utf8ToWide, String)
 {
 	const char* c = "\xE0\xA4\x9C\xE0\xA4\xA1\xE0\xA4\xA4";
 	const size_t s = 256;
@@ -14,7 +14,7 @@ TEST(ToWide, String)
 	EXPECT_STREQ(L"\x91C\x921\x924", b);
 }
 
-TEST(ToWide, Ascii)
+TEST(Utf8ToWide, Ascii)
 {
 	const char* c = "7";
 	const size_t s = 256;
@@ -26,7 +26,7 @@ TEST(ToWide, Ascii)
 	EXPECT_STREQ(L"7", b);
 }
 
-TEST(ToWide, AsciiNotEnoughData)
+TEST(Utf8ToWide, AsciiNotEnoughData)
 {
 	const char* c = "%";
 	const size_t s = 256;
@@ -38,7 +38,7 @@ TEST(ToWide, AsciiNotEnoughData)
 	EXPECT_STREQ(L"", b);
 }
 
-TEST(ToWide, AsciiNotEnoughSpace)
+TEST(Utf8ToWide, AsciiNotEnoughSpace)
 {
 	const char* c = "D";
 	const size_t s = 0;
@@ -50,7 +50,7 @@ TEST(ToWide, AsciiNotEnoughSpace)
 	EXPECT_STREQ(L"", b);
 }
 
-TEST(ToWide, TwoBytes)
+TEST(Utf8ToWide, TwoBytes)
 {
 	const char* c = "\xCB\x8C";
 	const size_t s = 256;
@@ -62,7 +62,7 @@ TEST(ToWide, TwoBytes)
 	EXPECT_STREQ(L"\x02CC", b);
 }
 
-TEST(ToWide, TwoBytesNotEnoughData)
+TEST(Utf8ToWide, TwoBytesNotEnoughData)
 {
 	const char* c = "\xDC";
 	const size_t s = 256;
@@ -74,7 +74,7 @@ TEST(ToWide, TwoBytesNotEnoughData)
 	EXPECT_STREQ(L"\xFFFD", b);
 }
 
-TEST(ToWide, TwoBytesNotEnoughSpace)
+TEST(Utf8ToWide, TwoBytesNotEnoughSpace)
 {
 	const char* c = "\xDC\x80";
 	const size_t s = 1;
@@ -86,7 +86,7 @@ TEST(ToWide, TwoBytesNotEnoughSpace)
 	EXPECT_STREQ("", b);
 }
 
-TEST(ToWide, ThreeBytes)
+TEST(Utf8ToWide, ThreeBytes)
 {
 	const char* c = "\xEA\xAC\x81";
 	const size_t s = 256;
@@ -98,7 +98,7 @@ TEST(ToWide, ThreeBytes)
 	EXPECT_STREQ(L"\xAB01", b);
 }
 
-TEST(ToWide, ThreeBytesNotEnoughData)
+TEST(Utf8ToWide, ThreeBytesNotEnoughData)
 {
 	const char* c = "\xEA\xAC";
 	const size_t s = 256;
@@ -110,7 +110,7 @@ TEST(ToWide, ThreeBytesNotEnoughData)
 	EXPECT_STREQ(L"\xFFFD", b);
 }
 
-TEST(ToWide, AboveBasicMultilingualPlane)
+TEST(Utf8ToWide, AboveBasicMultilingualPlane)
 {
 	const char* c = "\xF0\x90\xB0\x91";
 	const size_t s = 256;
@@ -127,7 +127,7 @@ TEST(ToWide, AboveBasicMultilingualPlane)
 	EXPECT_EQ(0, errors);
 }
 
-TEST(ToWide, AboveBasicMultilingualPlaneFirst)
+TEST(Utf8ToWide, AboveBasicMultilingualPlaneFirst)
 {
 	const char* c = "\xF0\x90\x80\x80";
 	const size_t s = 256;
@@ -144,7 +144,7 @@ TEST(ToWide, AboveBasicMultilingualPlaneFirst)
 	EXPECT_EQ(0, errors);
 }
 
-TEST(ToWide, AboveBasicMultilingualPlaneLast)
+TEST(Utf8ToWide, AboveBasicMultilingualPlaneLast)
 {
 	const char* c = "\xF4\x8F\xBF\xBF";
 	const size_t s = 256;
@@ -161,7 +161,7 @@ TEST(ToWide, AboveBasicMultilingualPlaneLast)
 	EXPECT_EQ(0, errors);
 }
 
-TEST(ToWide, AboveBasicMultilingualPlaneNotEnoughData)
+TEST(Utf8ToWide, AboveBasicMultilingualPlaneNotEnoughData)
 {
 	const char* c = "\xF0\x90\x8C";
 	const size_t s = 256;
@@ -173,7 +173,7 @@ TEST(ToWide, AboveBasicMultilingualPlaneNotEnoughData)
 	EXPECT_STREQ(L"\xFFFD", b);
 }
 
-TEST(ToWide, AboveBasicMultilingualPlaneNotEnoughSpace)
+TEST(Utf8ToWide, AboveBasicMultilingualPlaneNotEnoughSpace)
 {
 	const char* c = "\xF0\x90\x82\xB0";
 	const size_t s = 1;
@@ -185,7 +185,7 @@ TEST(ToWide, AboveBasicMultilingualPlaneNotEnoughSpace)
 	EXPECT_STREQ("", b);
 }
 
-TEST(ToWide, NoData)
+TEST(Utf8ToWide, NoData)
 {
 	const char* c = nullptr;
 	const size_t s = 256;
