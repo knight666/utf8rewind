f62e2ddc929cdf178adf6aa0ec9d506674efea39
Quinten <knight666+github@gmail.com>
2015-03-03 10:49:15 +0100

    utf8toupper: Added documentation.

--- documentation/html/navtree.js
+++ documentation/html/navtree.js
@@ -1,66 +1,3 @@
-var NAVTREE =
-[
-  [ "utf8rewind", "index.html", [
-    [ "Examples", "examples.html", [
-      [ "Introduction", "index.html#introduction", null ],
-      [ "Why UTF-8?", "index.html#why-utf8", null ],
-      [ "Why not UTF-16?", "index.html#why-not-utf16", null ],
-      [ "Licensing", "index.html#license", null ],
-      [ "Building the project", "index.html#building", [
-        [ "Building on Windows with Visual Studio", "index.html#building-windows", null ],
-        [ "Building on Linux with GCC", "index.html#building-linux", null ],
-        [ "Building on Mac OS X using Xcode", "index.html#building-mac", null ],
-        [ "Using the source directly", "index.html#using-source", null ],
-        [ "Running the tests", "index.html#running-tests", null ]
-      ] ],
-      [ "Helping out", "index.html#helping-out", null ],
-      [ "Contact", "index.html#contact", null ],
-      [ "Changes to existing code", "examples.html#example-changes", null ],
-      [ "Dealing with user input", "examples.html#example-user-input", null ],
-      [ "Displaying Unicode text", "examples.html#example-display", null ]
-    ] ],
-    [ "Modules", "modules.html", "modules" ],
-    [ "Namespaces", null, [
-      [ "Namespace List", "namespaces.html", "namespaces" ],
-      [ "Namespace Members", "namespacemembers.html", [
-        [ "All", "namespacemembers.html", null ],
-        [ "Functions", "namespacemembers_func.html", null ]
-      ] ]
-    ] ],
-    [ "Classes", null, [
-      [ "Class List", "annotated.html", "annotated" ],
-      [ "Class Index", "classes.html", null ],
-      [ "Class Members", "functions.html", [
-        [ "All", "functions.html", null ],
-        [ "Functions", "functions_func.html", null ],
-        [ "Variables", "functions_vars.html", null ]
-      ] ]
-    ] ],
-    [ "Files", null, [
-      [ "File List", "files.html", "files" ],
-      [ "File Members", "globals.html", [
-        [ "All", "globals.html", null ],
-        [ "Functions", "globals_func.html", null ],
-        [ "Variables", "globals_vars.html", null ],
-        [ "Typedefs", "globals_type.html", null ],
-        [ "Enumerations", "globals_enum.html", null ],
-        [ "Enumerator", "globals_eval.html", null ],
-        [ "Macros", "globals_defs.html", null ]
-      ] ]
-    ] ],
-    [ "Library interface", "utf8rewind_8h.html", null ],
-    [ "Examples", "examples.html", null ]
-  ] ]
-];
-
-var NAVTREEINDEX =
-[
-"annotated.html",
-"struct_composition_record.html#a225cc42087502955b9066f660d13dfed"
-];
-
-var SYNCONMSG = 'click to disable panel synchronisation';
-var SYNCOFFMSG = 'click to enable panel synchronisation';
 var navTreeSubIndices = new Array();
 
 function getData(varName)
@@ -168,7 +105,7 @@ function createIndent(o,domNode,node,level)
     node.expandToggle.onclick = function() {
       if (node.expanded) {
         $(node.getChildrenUL()).slideUp("fast");
-        node.plus_img.src = node.relpath+"ftv2pnode.png";
+        node.plus_img.src = node.relpath+"arrowright.png";
         node.expanded = false;
       } else {
         expandNode(o, node, false, false);
@@ -176,7 +113,7 @@ function createIndent(o,domNode,node,level)
     }
     node.expandToggle.appendChild(imgNode);
     domNode.appendChild(node.expandToggle);
-    imgNode.src = node.relpath+"ftv2pnode.png";
+    imgNode.src = node.relpath+"arrowright.png";
   } else {
     var span = document.createElement("span");
     span.style.display = 'inline-block';
@@ -332,9 +269,9 @@ function expandNode(o, node, imm, showRoot)
         $(node.getChildrenUL()).slideDown("fast");
       }
       if (node.isLast) {
-        node.plus_img.src = node.relpath+"ftv2mlastnode.png";
+        node.plus_img.src = node.relpath+"arrowdown.png";
       } else {
-        node.plus_img.src = node.relpath+"ftv2mnode.png";
+        node.plus_img.src = node.relpath+"arrowdown.png";
       }
       node.expanded = true;
     }
@@ -404,11 +341,7 @@ function showNode(o, node, index, hash)
         getNode(o, node);
       }
       $(node.getChildrenUL()).css({'display':'block'});
-      if (node.isLast) {
-        node.plus_img.src = node.relpath+"ftv2mlastnode.png";
-      } else {
-        node.plus_img.src = node.relpath+"ftv2mnode.png";
-      }
+      node.plus_img.src = node.relpath+"arrowdown.png";
       node.expanded = true;
       var n = node.children[o.breadcrumbs[index]];
       if (index+1<o.breadcrumbs.length) {
@@ -546,7 +479,7 @@ function initNavTree(toroot,relpath)
   o.node.expanded = false;
   o.node.isLast = true;
   o.node.plus_img = document.createElement("img");
-  o.node.plus_img.src = relpath+"ftv2pnode.png";
+  o.node.plus_img.src = relpath+"arrowright.png";
   o.node.plus_img.width = 16;
   o.node.plus_img.height = 22;
 
