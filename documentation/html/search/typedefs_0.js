2b8667bb85f6f28cdf634020e2cd259917829b81
Anne <knight666@gmail.com>
2015-03-03 08:28:17 +0100

    Cleaned up documentation.

--- documentation/html/search/typedefs_0.js
+++ documentation/html/search/typedefs_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['unicode_5ft',['unicode_t',['../utf8rewind_8h.html#aea65d3ed643f803027827d47ccd3d981',1,'utf8rewind.h']]],
-  ['utf16_5ft',['utf16_t',['../utf8rewind_8h.html#a56676fc77a7ba52afe7ee977902a88ea',1,'utf8rewind.h']]]
+  ['unicode_5ft',['unicode_t',['../utf8rewind_8h.html#ae128fe5bd9e57be736e64d3638229988',1,'utf8rewind.h']]],
+  ['utf16_5ft',['utf16_t',['../utf8rewind_8h.html#a1c56c34e8f9e0fbcf4949782f110df8f',1,'utf8rewind.h']]]
 ];
