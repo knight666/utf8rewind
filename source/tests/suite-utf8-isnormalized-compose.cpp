17791ff96952135278ea72261224067187a95821
Quinten <knight666+github@gmail.com>
2015-03-03 16:25:20 +0100

    suite-utf8-isnormalized-compose: Added tests for multi-byte MAYBE codepoints.

--- source/tests/suite-utf8-isnormalized-compose.cpp
+++ source/tests/suite-utf8-isnormalized-compose.cpp
@@ -78,6 +78,20 @@ TEST(Utf8IsNormalizedCompose, MultiByteYesMultiple)
 	EXPECT_EQ(UTF8_NORMALIZATION_RESULT_YES, utf8isnormalized(i, is, UTF8_NORMALIZE_COMPOSE));
 }
 
+TEST(Utf8IsNormalizedCompose, MultiByteYesUnordered)
+{
+	/*
+		U+1A18 U+0F39 U+0EB9
+		     Y      Y      Y
+		   220    216    118
+	*/
+
+	const char* i = "\xE1\xA8\x98\xE0\xBC\xB9\xE0\xBA\xB9";
+	size_t is = strlen(i);
+
+	EXPECT_EQ(UTF8_NORMALIZATION_RESULT_NO, utf8isnormalized(i, is, UTF8_NORMALIZE_COMPOSE));
+}
+
 TEST(Utf8IsNormalizedCompose, MultiByteYesCompatibility)
 {
 	/*
@@ -90,4 +104,88 @@ TEST(Utf8IsNormalizedCompose, MultiByteYesCompatibility)
 	size_t is = strlen(i);
 
 	EXPECT_EQ(UTF8_NORMALIZATION_RESULT_YES, utf8isnormalized(i, is, UTF8_NORMALIZE_COMPOSE | UTF8_NORMALIZE_COMPATIBILITY));
+}
+
+TEST(Utf8IsNormalizedCompose, MultiByteMaybeSingle)
+{
+	/*
+		U+0323
+		     M
+		   220
+	*/
+
+	const char* i = "\xCC\xA3";
+	size_t is = strlen(i);
+
+	EXPECT_EQ(UTF8_NORMALIZATION_RESULT_MAYBE, utf8isnormalized(i, is, UTF8_NORMALIZE_COMPOSE));
+}
+
+TEST(Utf8IsNormalizedCompose, MultiByteMaybeMultiple)
+{
+	/*
+		U+0B56 U+0CD6 U+031B U+0301
+		     M      M      M      M
+		     0      0    216    230
+	*/
+
+	const char* i = "\xE0\xAD\x96\xE0\xB3\x96\xCC\x9B\xCC\x81";
+	size_t is = strlen(i);
+
+	EXPECT_EQ(UTF8_NORMALIZATION_RESULT_MAYBE, utf8isnormalized(i, is, UTF8_NORMALIZE_COMPOSE));
+}
+
+TEST(Utf8IsNormalizedCompose, MultiByteMaybeSequenceStart)
+{
+	/*
+		U+0342 U+0024 U+01D4
+		     M      Y      Y
+		   230      0      0
+	*/
+
+	const char* i = "\xCD\x82$\xC7\x94";
+	size_t is = strlen(i);
+
+	EXPECT_EQ(UTF8_NORMALIZATION_RESULT_MAYBE, utf8isnormalized(i, is, UTF8_NORMALIZE_COMPOSE));
+}
+
+TEST(Utf8IsNormalizedCompose, MultiByteMaybeSequenceEnd)
+{
+	/*
+		U+0157 U+0301
+		     Y      M
+		     0    230
+	*/
+
+	const char* i = "\xC5\x97\xCC\x81";
+	size_t is = strlen(i);
+
+	EXPECT_EQ(UTF8_NORMALIZATION_RESULT_MAYBE, utf8isnormalized(i, is, UTF8_NORMALIZE_COMPOSE));
+}
+
+TEST(Utf8IsNormalizedCompose, MultiByteMaybeUnordered)
+{
+	/*
+		U+0301 U+031B U+0328
+		     M      M      M
+		   230    216    202
+	*/
+
+	const char* i = "\xCC\x81\xCC\x9B\xCC\xA8";
+	size_t is = strlen(i);
+
+	EXPECT_EQ(UTF8_NORMALIZATION_RESULT_NO, utf8isnormalized(i, is, UTF8_NORMALIZE_COMPOSE));
+}
+
+TEST(Utf8IsNormalizedCompose, MultiByteMaybeCompatibility)
+{
+	/*
+		U+093C U+0C56 U+0328
+		     M      M      M
+		     7     91    202
+	*/
+
+	const char* i = "\xE0\xA4\xBC\xE0\xB1\x96\xCC\xA8";
+	size_t is = strlen(i);
+
+	EXPECT_EQ(UTF8_NORMALIZATION_RESULT_MAYBE, utf8isnormalized(i, is, UTF8_NORMALIZE_COMPOSE | UTF8_NORMALIZE_COMPATIBILITY));
 }
