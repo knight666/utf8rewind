3c6275baada6e76019c2828e7a096bd72494a2d7
Quinten <knight666+github@gmail.com>
2015-03-03 15:30:59 +0100

    suite-utf8-isnormalized-compose: Added tests for multi-byte YES codepoints.

--- source/tests/suite-utf8-isnormalized-compose.cpp
+++ source/tests/suite-utf8-isnormalized-compose.cpp
@@ -49,3 +49,45 @@ TEST(Utf8IsNormalizedCompose, BasicLatinCompatibility)
 
 	EXPECT_EQ(UTF8_NORMALIZATION_RESULT_YES, utf8isnormalized(i, is, UTF8_NORMALIZE_COMPOSE | UTF8_NORMALIZE_COMPATIBILITY));
 }
+
+TEST(Utf8IsNormalizedCompose, MultiByteYesSingle)
+{
+	/*
+		U+2B6C
+		     Y
+		     0
+	*/
+
+	const char* i = "\xE2\xAD\xAC";
+	size_t is = strlen(i);
+
+	EXPECT_EQ(UTF8_NORMALIZATION_RESULT_YES, utf8isnormalized(i, is, UTF8_NORMALIZE_COMPOSE));
+}
+
+TEST(Utf8IsNormalizedCompose, MultiByteYesMultiple)
+{
+	/*
+		U+2A6E U+A99F U+2923 U+23B9
+		     Y      Y      Y      Y
+		     0      0      0      0
+	*/
+
+	const char* i = "\xE2\xA9\xAE\xEA\xA6\x9F\xE2\xA4\xA3\xE2\x8E\xB9";
+	size_t is = strlen(i);
+
+	EXPECT_EQ(UTF8_NORMALIZATION_RESULT_YES, utf8isnormalized(i, is, UTF8_NORMALIZE_COMPOSE));
+}
+
+TEST(Utf8IsNormalizedCompose, MultiByteYesCompatibility)
+{
+	/*
+		U+20E0 U+20B0 U+2108
+		     Y      Y      Y
+		     0      0      0
+	*/
+
+	const char* i = "\xE2\x83\xA0\xE2\x82\xB0\xE2\x84\x88";
+	size_t is = strlen(i);
+
+	EXPECT_EQ(UTF8_NORMALIZATION_RESULT_YES, utf8isnormalized(i, is, UTF8_NORMALIZE_COMPOSE | UTF8_NORMALIZE_COMPATIBILITY));
+}
