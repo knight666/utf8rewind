217724c9623d3006bcf091c97debcf0240c522cf
Quinten <knight666+github@gmail.com>
2015-03-04 11:18:13 +0100

    Cleaned up doxygen file.

--- documentation/html/casemapping_8h.js
+++ documentation/html/casemapping_8h.js
@@ -1,6 +1,6 @@
 var casemapping_8h =
 [
     [ "CaseMappingState", "struct_case_mapping_state.html", "struct_case_mapping_state" ],
-    [ "casemapping_execute", "casemapping_8h.html#aeeeb95d1cec1016de22a570b9f7a1854", null ],
-    [ "casemapping_initialize", "casemapping_8h.html#a01a2a2a122d88330150e3044b953079d", null ]
+    [ "casemapping_initialize", "casemapping_8h.html#a01a2a2a122d88330150e3044b953079d", null ],
+    [ "casemapping_execute", "casemapping_8h.html#aeeeb95d1cec1016de22a570b9f7a1854", null ]
 ];
--- documentation/html/struct_stream_state.js
+++ documentation/html/struct_stream_state.js
@@ -1,13 +1,13 @@
 var struct_stream_state =
 [
-    [ "canonical_combining_class", "struct_stream_state.html#aa514266c689097ee5105e698a5de3666", null ],
-    [ "codepoint", "struct_stream_state.html#ad4b27ff173fe60a29cb0788dd128884e", null ],
-    [ "current", "struct_stream_state.html#a96a5d1ae40676778261a4c85132e8686", null ],
-    [ "filled", "struct_stream_state.html#ac5296567fe7363fd9df1973c618053b8", null ],
-    [ "index", "struct_stream_state.html#afdccb70afa86ba6961eb434d342aa38c", null ],
-    [ "last_length", "struct_stream_state.html#a63cd1a95e9292f48586e742ffc2d6ecc", null ],
-    [ "quick_check", "struct_stream_state.html#ac8bac3e1bbfb13b79c93522c09ec961e", null ],
     [ "src", "struct_stream_state.html#a905503dbb301ea1a8d4514293254f432", null ],
     [ "src_size", "struct_stream_state.html#ad126e43bcd4f692946c87454a224c8ee", null ],
-    [ "stable", "struct_stream_state.html#a917fde7b57f25f6540ddf1556e0cdb8b", null ]
+    [ "index", "struct_stream_state.html#afdccb70afa86ba6961eb434d342aa38c", null ],
+    [ "current", "struct_stream_state.html#a96a5d1ae40676778261a4c85132e8686", null ],
+    [ "filled", "struct_stream_state.html#ac5296567fe7363fd9df1973c618053b8", null ],
+    [ "stable", "struct_stream_state.html#a917fde7b57f25f6540ddf1556e0cdb8b", null ],
+    [ "last_length", "struct_stream_state.html#a63cd1a95e9292f48586e742ffc2d6ecc", null ],
+    [ "codepoint", "struct_stream_state.html#ad4b27ff173fe60a29cb0788dd128884e", null ],
+    [ "quick_check", "struct_stream_state.html#ac8bac3e1bbfb13b79c93522c09ec961e", null ],
+    [ "canonical_combining_class", "struct_stream_state.html#aa514266c689097ee5105e698a5de3666", null ]
 ];
