2bc994365909989d6c07ea58ab59aaa2e70e29d9
Quinten <knight666+github@gmail.com>
2015-03-18 10:43:36 +0100

    Added helper functions for errors.

---
+++ source/tests/helpers-errors.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+/*!
+	\file
+	\brief String helper functions.
+
+	\cond INTERNAL
+*/
+
+#include "tests-base.hpp"
+
+#include "utf8rewind.h"
+
+#define EXPECT_ERROREQ(_expected, _actual) EXPECT_PRED_FORMAT2(::helpers::CompareErrors, _expected, _actual)
+
+namespace helpers {
+
+	std::string error(int32_t error);
+
+	::testing::AssertionResult CompareErrors(
+		const char* expressionExpected, const char* expressionActual,
+		int32_t errorExpected, int32_t errorActual);
+
+};
+
+/*! \endcond */
