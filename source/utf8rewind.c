392ffbebc1c2342bcc2ec105bc9f43bd13c801e0
Anne <knight666@gmail.com>
2015-03-23 09:49:59 +0100

    Updated copyright notice.

--- source/utf8rewind.c
+++ source/utf8rewind.c
@@ -1,5 +1,5 @@
 /*
-	Copyright (C) 2014 Quinten Lansu
+	Copyright (C) 2014-2015 Quinten Lansu
 
 	Permission is hereby granted, free of charge, to any person
 	obtaining a copy of this software and associated documentation
