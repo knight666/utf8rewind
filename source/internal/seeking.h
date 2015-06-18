23747157d1ca19fd3257cda949b81a668803a9e9
Quinten <knight666+github@gmail.com>
2015-06-18 16:37:11 +0200

    seeking_rewind: Clean up implementation.

--- source/internal/seeking.h
+++ source/internal/seeking.h
@@ -37,7 +37,7 @@
 
 const char* seeking_forward(const char* src, const char* srcEnd, size_t srcLength, off_t offset);
 
-const char* seeking_rewind(const char* srcStart, const char* src, size_t srcLength, off_t offset);
+const char* seeking_rewind(const char* inputStart, const char* input, size_t inputLength, off_t offset);
 
 /*! \endcond */
 
