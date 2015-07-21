4f7e5213b109057292799640eeec7036dea3b7a0
Purrminator <knight666@gmail.com>
2015-07-21 22:41:06 +0200

    Rename EXPECT_SEEKEQ2 to EXPECT_SEEKEQ.

--- source/tests/helpers-seeking.cpp
+++ source/tests/helpers-seeking.cpp
@@ -5,43 +5,6 @@
 namespace helpers {
 
 	::testing::AssertionResult CompareSeeking(
-		const char* expressionExpected, const char* expressionActual, const char* expressionInput,
-		const SeekingParameters& paramsExpected, const SeekingParameters& paramsActual, const SeekingParameters& paramsInput)
-	{
-		if (!strcmp(paramsExpected.text, paramsActual.text))
-		{
-			return ::testing::AssertionSuccess();
-		}
-		else
-		{
-			::testing::AssertionResult result = ::testing::AssertionFailure();
-
-			result << paramsActual.expression << std::endl;
-
-			result << "    Actual: " << hex(paramsInput.text) << std::endl;
-
-			size_t marker_actual = paramsActual.text - paramsInput.text;
-			result << "            ";
-			for (size_t i = 0; i < strlen(paramsInput.text); i++)
-			{
-				result << ((i == marker_actual) ? "^^^^" : "----");
-			}
-			result << std::endl;
-
-			result << "  Expected: " << hex(paramsInput.text) << std::endl;
-
-			result << "            ";
-			for (size_t i = 0; i < strlen(paramsInput.text); i++)
-			{
-				result << ((i == paramsExpected.offset) ? "^^^^" : "----");
-			}
-			result << std::endl;
-
-			return result;
-		}
-	}
-
-	::testing::AssertionResult CompareSeeking2(
 		const char* expressionExpected, const char* expressionActual,
 		const SeekingParameters& paramsExpected, const SeekingParameters& paramsActual)
 	{
