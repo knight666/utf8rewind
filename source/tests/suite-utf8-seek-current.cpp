0c1eb67bd5c60daee5187f55b4f599780e100560
Purrminator <knight666@gmail.com>
2015-07-27 23:02:28 +0200

    Refactor seek current tests.

--- source/tests/suite-utf8-seek-current.cpp
+++ source/tests/suite-utf8-seek-current.cpp
@@ -2,24 +2,18 @@
 
 #include "utf8rewind.h"
 
-#include "helpers-strings.hpp"
+#include "helpers-seeking.hpp"
 
 TEST(Utf8SeekCurrent, SwappedParameters)
 {
 	const char* t = "\xF0\x90\x92\x80\xF0\x90\x92\x80\xF0\x90\x92\x80\xF0\x90\x92\x80\xF0\x90\x92\x80";
 
-	const char* r = utf8seek(t, t + strlen(t), 2, SEEK_CUR);
-
-	EXPECT_EQ(t, r);
-	EXPECT_UTF8EQ("\xF0\x90\x92\x80\xF0\x90\x92\x80\xF0\x90\x92\x80\xF0\x90\x92\x80\xF0\x90\x92\x80", r);
+	EXPECT_SEEKEQ(t, 0, 0, strlen(t), 0, SEEK_CUR);
 }
 
 TEST(Utf8SeekCurrent, ZeroOffset)
 {
 	const char* t = "Banana";
 
-	const char* r = utf8seek(t + 2, t, 0, SEEK_CUR);
-
-	EXPECT_EQ(t + 2, r);
-	EXPECT_UTF8EQ("nana", r);
+	EXPECT_SEEKEQ(t, 0, 0, 0, 0, SEEK_CUR);
 }
