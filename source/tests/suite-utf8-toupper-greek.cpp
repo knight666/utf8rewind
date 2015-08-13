f6c28e0c43798aac2a21f243bca05a50409cb636
Quinten <quintenlansu@gmail.com>
2015-08-13 16:42:07 +0200

    Refactor Greek uppercase tests.

--- source/tests/suite-utf8-toupper-greek.cpp
+++ source/tests/suite-utf8-toupper-greek.cpp
@@ -12,7 +12,9 @@ protected:
 
 	void SetUp()
 	{
-		SET_LOCALE_GREEK();
+		// Greek grammar rules are not exclusive to Greek locale
+
+		SET_LOCALE_ENGLISH();
 	}
 
 	void TearDown()
@@ -22,44 +24,134 @@ protected:
 
 };
 
-TEST_F(Utf8ToUpperGreek, GreekSingleUppercase)
+TEST_F(Utf8ToUpperGreek, SingleCapitalLetterSigma)
 {
-	// U+039C GREEK CAPITAL LETTER MU
+	// 03A3
 
-	const char* c = "\xCE\x9C";
+	const char* c = "\xCE\xA3";
 	const size_t s = 256;
 	char b[s] = { 0 };
 	int32_t errors = UTF8_ERR_NONE;
 
 	EXPECT_EQ(2, utf8toupper(c, strlen(c), b, s - 1, &errors));
-	EXPECT_UTF8EQ("\xCE\x9C", b);
+	EXPECT_UTF8EQ("\xCE\xA3", b);
 	EXPECT_ERROREQ(UTF8_ERR_NONE, errors);
 }
 
-TEST_F(Utf8ToUpperGreek, GreekSingleLowercase)
+TEST_F(Utf8ToUpperGreek, SingleSmallLetterSigma)
 {
-	// U+03CA GREEK SMALL LETTER IOTA WITH DIALYTIKA
+	// 03C3
 
-	const char* c = "\xCF\x8A";
+	const char* c = "\xCF\x83";
 	const size_t s = 256;
 	char b[s] = { 0 };
 	int32_t errors = UTF8_ERR_NONE;
 
 	EXPECT_EQ(2, utf8toupper(c, strlen(c), b, s - 1, &errors));
-	EXPECT_UTF8EQ("\xCE\xAA", b);
+	EXPECT_UTF8EQ("\xCE\xA3", b);
 	EXPECT_ERROREQ(UTF8_ERR_NONE, errors);
 }
 
-TEST_F(Utf8ToUpperGreek, GreekSingleUnaffected)
+TEST_F(Utf8ToUpperGreek, SingleSmallLetterFinalSigma)
 {
-	// U+0387 GREEK ANO TELEIA
+	// 03C2
 
-	const char* c = "\xCE\x87";
+	const char* c = "\xCF\x82";
 	const size_t s = 256;
 	char b[s] = { 0 };
 	int32_t errors = UTF8_ERR_NONE;
 
 	EXPECT_EQ(2, utf8toupper(c, strlen(c), b, s - 1, &errors));
-	EXPECT_UTF8EQ("\xCE\x87", b);
+	EXPECT_UTF8EQ("\xCE\xA3", b);
+	EXPECT_ERROREQ(UTF8_ERR_NONE, errors);
+}
+
+TEST_F(Utf8ToUpperGreek, WordCapitalLetterSigma)
+{
+	// 0396 03A0 03A4 03A3
+	// 0396 03A0 03A4 03A3
+
+	const char* c = "\xCE\x96\xCE\xA0\xCE\xA4\xCE\xA3";
+	const size_t s = 256;
+	char b[s] = { 0 };
+	int32_t errors = UTF8_ERR_NONE;
+
+	EXPECT_EQ(8, utf8toupper(c, strlen(c), b, s - 1, &errors));
+	EXPECT_UTF8EQ("\xCE\x96\xCE\xA0\xCE\xA4\xCE\xA3", b);
+	EXPECT_ERROREQ(UTF8_ERR_NONE, errors);
+}
+
+TEST_F(Utf8ToUpperGreek, WordSmallLetterSigma)
+{
+	// 03C5 03BD 03B6 03B6 03C3
+	// 03A5 039D 0396 0396 03A3
+
+	const char* c = "\xCF\x85\xCE\xBD\xCE\xB6\xCE\xB6\xCF\x83";
+	const size_t s = 256;
+	char b[s] = { 0 };
+	int32_t errors = UTF8_ERR_NONE;
+
+	EXPECT_EQ(10, utf8toupper(c, strlen(c), b, s - 1, &errors));
+	EXPECT_UTF8EQ("\xCE\xA5\xCE\x9D\xCE\x96\xCE\x96\xCE\xA3", b);
+	EXPECT_ERROREQ(UTF8_ERR_NONE, errors);
+}
+
+TEST_F(Utf8ToUpperGreek, WordSmallLetterFinalSigma)
+{
+	// 03B6 03B3 03C2
+	// 0396 0393 03A3
+
+	const char* c = "\xCE\xB6\xCE\xB3\xCF\x82";
+	const size_t s = 256;
+	char b[s] = { 0 };
+	int32_t errors = UTF8_ERR_NONE;
+
+	EXPECT_EQ(6, utf8toupper(c, strlen(c), b, s - 1, &errors));
+	EXPECT_UTF8EQ("\xCE\x96\xCE\x93\xCE\xA3", b);
+	EXPECT_ERROREQ(UTF8_ERR_NONE, errors);
+}
+
+TEST_F(Utf8ToUpperGreek, SentenceCapitalLetterSigma)
+{
+	// 0399 0399 03A3 0020 0394 03A0 03AB
+	// 0399 0399 03A3 0020 0394 03A0 03AB
+
+	const char* c = "\xCE\x99\xCE\x99\xCE\xA3 \xCE\x94\xCE\xA0\xCE\xAB";
+	const size_t s = 256;
+	char b[s] = { 0 };
+	int32_t errors = UTF8_ERR_NONE;
+
+	EXPECT_EQ(13, utf8toupper(c, strlen(c), b, s - 1, &errors));
+	EXPECT_UTF8EQ("\xCE\x99\xCE\x99\xCE\xA3 \xCE\x94\xCE\xA0\xCE\xAB", b);
+	EXPECT_ERROREQ(UTF8_ERR_NONE, errors);
+}
+
+TEST_F(Utf8ToUpperGreek, SentenceSmallLetterSigma)
+{
+	// 0397 03A3 03A7 03BF 03C3 0020 03B8 03C3 03C6
+	// 0397 03A3 03A7 039F 03A3 0020 0398 03A3 03A6
+
+	const char* c = "\xCE\x97\xCE\xA3\xCE\xA7\xCE\xBF\xCF\x83 \xCE\xB8\xCF\x83\xCF\x86";
+	const size_t s = 256;
+	char b[s] = { 0 };
+	int32_t errors = UTF8_ERR_NONE;
+
+	EXPECT_EQ(17, utf8toupper(c, strlen(c), b, s - 1, &errors));
+	EXPECT_UTF8EQ("\xCE\x97\xCE\xA3\xCE\xA7\xCE\x9F\xCE\xA3 \xCE\x98\xCE\xA3\xCE\xA6", b);
+	EXPECT_ERROREQ(UTF8_ERR_NONE, errors);
+}
+
+TEST_F(Utf8ToUpperGreek, SentenceSmallLetterFinalSigma)
+{
+	// 03C3 03C2 0020 03BA 03B7 03CB 03CB
+	// 03A3 03A3 0020 039A 0397 03AB 03AB
+
+	const char* c = "\xCF\x83\xCF\x82 \xCE\xBA\xCE\xB7\xCF\x8B\xCF\x8B";
+	const size_t s = 256;
+	char b[s] = { 0 };
+	int32_t errors = UTF8_ERR_NONE;
+
+	EXPECT_EQ(13, utf8toupper(c, strlen(c), b, s - 1, &errors));
+	EXPECT_UTF8EQ("\xCE\xA3\xCE\xA3 \xCE\x9A\xCE\x97\xCE\xAB\xCE\xAB", b);
 	EXPECT_ERROREQ(UTF8_ERR_NONE, errors);
 }
