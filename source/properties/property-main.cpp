55c5fbd4d4682a484423a9c2f60665a7f819a0f2
Quinten <quintenlansu@gmail.com>
2015-08-17 09:50:24 +0200

    Add property suite for converting UTF-32.

--- source/properties/property-main.cpp
+++ source/properties/property-main.cpp
@@ -1,6 +1,7 @@
 #include "property-base.hpp"
 
 #include "property-convert-utf16.hpp"
+#include "property-convert-utf32.hpp"
 
 extern "C" {
 	#include "../internal/codepoint.h"
@@ -9,6 +10,7 @@ extern "C" {
 int main(int argc, char** argv)
 {
 	quickcheck::check<PropertyConvertUtf16>("Convert UTF-16 to UTF-8 and back.", 10000);
+	quickcheck::check<PropertyConvertUtf32>("Convert UTF-32 to UTF-8 and back.", 10000);
 
 	return 0;
 }
