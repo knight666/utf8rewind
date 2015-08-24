c6d846bf278087f06c12ee5cdaa55da8e10d4eb6
Quinten <quintenlansu@gmail.com>
2015-08-24 09:14:12 +0200

    Add test for reserved strings.

--- source/tests/integration-naughty-strings.cpp
+++ source/tests/integration-naughty-strings.cpp
@@ -44,4 +44,16 @@ protected:
 TEST_F(NaughtyStrings, ReservedStrings)
 {
 	EXPECT_STREQ(L"undefined", helpers::wide(ReadSection(68, 9)).c_str());
+	EXPECT_STREQ(L"undef", helpers::wide(ReadSection(78, 5)).c_str());
+	EXPECT_STREQ(L"null", helpers::wide(ReadSection(84, 4)).c_str());
+	EXPECT_STREQ(L"NULL", helpers::wide(ReadSection(89, 4)).c_str());
+	EXPECT_STREQ(L"nil", helpers::wide(ReadSection(94, 3)).c_str());
+	EXPECT_STREQ(L"NIL", helpers::wide(ReadSection(98, 3)).c_str());
+	EXPECT_STREQ(L"true", helpers::wide(ReadSection(102, 4)).c_str());
+	EXPECT_STREQ(L"false", helpers::wide(ReadSection(107, 5)).c_str());
+	EXPECT_STREQ(L"True", helpers::wide(ReadSection(113, 4)).c_str());
+	EXPECT_STREQ(L"False", helpers::wide(ReadSection(118, 5)).c_str());
+	EXPECT_STREQ(L"None", helpers::wide(ReadSection(124, 4)).c_str());
+	EXPECT_STREQ(L"\\", helpers::wide(ReadSection(129, 1)).c_str());
+	EXPECT_STREQ(L"\\\\", helpers::wide(ReadSection(131, 2)).c_str());
 }
