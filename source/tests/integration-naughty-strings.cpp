ecfad5e26b6a709cf4cf7689ed9f8d4bd485aa7f
Quinten <knight666+github@gmail.com>
2015-08-24 09:39:59 +0200

    Add test for numeric strings.

--- source/tests/integration-naughty-strings.cpp
+++ source/tests/integration-naughty-strings.cpp
@@ -57,3 +57,53 @@ TEST_F(NaughtyStrings, ReservedStrings)
 	EXPECT_STREQ(L"\\", helpers::wide(ReadSection(129, 1)).c_str());
 	EXPECT_STREQ(L"\\\\", helpers::wide(ReadSection(131, 2)).c_str());
 }
+
+TEST_F(NaughtyStrings, NumericStrings)
+{
+	EXPECT_STREQ(L"0", helpers::wide(ReadSection(202, 1)).c_str());
+	EXPECT_STREQ(L"1", helpers::wide(ReadSection(204, 1)).c_str());
+	EXPECT_STREQ(L"1.00", helpers::wide(ReadSection(206, 4)).c_str());
+	EXPECT_STREQ(L"$1.00", helpers::wide(ReadSection(211, 5)).c_str());
+	EXPECT_STREQ(L"1/2", helpers::wide(ReadSection(217, 3)).c_str());
+	EXPECT_STREQ(L"1E2", helpers::wide(ReadSection(221, 3)).c_str());
+	EXPECT_STREQ(L"1E02", helpers::wide(ReadSection(225, 4)).c_str());
+	EXPECT_STREQ(L"1E+02", helpers::wide(ReadSection(230, 5)).c_str());
+	EXPECT_STREQ(L"-1", helpers::wide(ReadSection(236, 2)).c_str());
+	EXPECT_STREQ(L"-1.00", helpers::wide(ReadSection(239, 5)).c_str());
+	EXPECT_STREQ(L"-$1.00", helpers::wide(ReadSection(245, 6)).c_str());
+	EXPECT_STREQ(L"-1/2", helpers::wide(ReadSection(252, 4)).c_str());
+	EXPECT_STREQ(L"-1E2", helpers::wide(ReadSection(257, 4)).c_str());
+	EXPECT_STREQ(L"-1E02", helpers::wide(ReadSection(262, 5)).c_str());
+	EXPECT_STREQ(L"-1E+02", helpers::wide(ReadSection(268, 6)).c_str());
+	EXPECT_STREQ(L"1/0", helpers::wide(ReadSection(275, 3)).c_str());
+	EXPECT_STREQ(L"0/0", helpers::wide(ReadSection(279, 3)).c_str());
+	EXPECT_STREQ(L"-2147483648/-1", helpers::wide(ReadSection(283, 14)).c_str());
+	EXPECT_STREQ(L"-9223372036854775808/-1", helpers::wide(ReadSection(298, 23)).c_str());
+	EXPECT_STREQ(L"0.00", helpers::wide(ReadSection(322, 4)).c_str());
+	EXPECT_STREQ(L"0..0", helpers::wide(ReadSection(327, 4)).c_str());
+	EXPECT_STREQ(L".", helpers::wide(ReadSection(332, 1)).c_str());
+	EXPECT_STREQ(L"0.0.0", helpers::wide(ReadSection(334, 5)).c_str());
+	EXPECT_STREQ(L"0,00", helpers::wide(ReadSection(340, 4)).c_str());
+	EXPECT_STREQ(L"0,,0", helpers::wide(ReadSection(345, 4)).c_str());
+	EXPECT_STREQ(L",", helpers::wide(ReadSection(350, 1)).c_str());
+	EXPECT_STREQ(L"0,0", helpers::wide(ReadSection(352, 3)).c_str());
+	EXPECT_STREQ(L"0,0,0", helpers::wide(ReadSection(352, 5)).c_str());
+	EXPECT_STREQ(L"0.0/0", helpers::wide(ReadSection(358, 5)).c_str());
+	EXPECT_STREQ(L"1.0/0.0", helpers::wide(ReadSection(364, 7)).c_str());
+	EXPECT_STREQ(L"0.0/0.0", helpers::wide(ReadSection(372, 7)).c_str());
+	EXPECT_STREQ(L"1,0/0,0", helpers::wide(ReadSection(380, 7)).c_str());
+	EXPECT_STREQ(L"0,0/0,0", helpers::wide(ReadSection(388, 7)).c_str());
+	EXPECT_STREQ(L"--1", helpers::wide(ReadSection(396, 3)).c_str());
+	EXPECT_STREQ(L"-", helpers::wide(ReadSection(400, 1)).c_str());
+	EXPECT_STREQ(L"-.", helpers::wide(ReadSection(402, 2)).c_str());
+	EXPECT_STREQ(L"-,", helpers::wide(ReadSection(405, 2)).c_str());
+	EXPECT_STREQ(L"999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999", helpers::wide(ReadSection(408, 96)).c_str());
+	EXPECT_STREQ(L"NaN", helpers::wide(ReadSection(505, 3)).c_str());
+	EXPECT_STREQ(L"Infinity", helpers::wide(ReadSection(509, 8)).c_str());
+	EXPECT_STREQ(L"-Infinity", helpers::wide(ReadSection(518, 9)).c_str());
+	EXPECT_STREQ(L"0x0", helpers::wide(ReadSection(528, 3)).c_str());
+	EXPECT_STREQ(L"0xffffffff", helpers::wide(ReadSection(532, 10)).c_str());
+	EXPECT_STREQ(L"0xffffffffffffffff", helpers::wide(ReadSection(543, 18)).c_str());
+	EXPECT_STREQ(L"0xabad1dea", helpers::wide(ReadSection(562, 10)).c_str());
+	EXPECT_STREQ(L"123456789012345678901234567890123456789", helpers::wide(ReadSection(573, 39)).c_str());
+}
