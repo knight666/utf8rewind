ef9845a677c51880269d2586b9917dad176902a8
Quinten <knight666+github@gmail.com>
2015-09-11 16:45:33 +0200

    Add performance suites based on a Greek dictionary.

---
+++ source/performance/performance-greek-seeking.cpp
@@ -0,0 +1,84 @@
+#include "performance-base.hpp"
+
+class GreekSeeking
+	: public performance::Suite
+{
+
+public:
+
+	virtual void setup() override
+	{
+		m_file.open("testdata/dictionaries/Greek.dic", std::ios_base::in);
+
+		std::stringstream ss;
+		ss << m_file.rdbuf();
+
+		m_contents = ss.str();
+
+		m_file.close();
+	}
+
+	std::fstream m_file;
+	std::string m_contents;
+
+};
+
+PERF_TEST_F(GreekSeeking, Begin)
+{
+	const char* s = m_contents.c_str();
+
+	utf8seek(s, s, (off_t)utf8len(s) - 1, SEEK_SET);
+}
+
+PERF_TEST_F(GreekSeeking, CurrentForwards)
+{
+	const char* s = m_contents.c_str();
+
+	utf8seek(s, s, (off_t)utf8len(s) - 1, SEEK_CUR);
+}
+
+PERF_TEST_F(GreekSeeking, CurrentBackwards)
+{
+	const char* s = m_contents.c_str();
+	const char* e = s + m_contents.length();
+
+	utf8seek(e, s, -(off_t)utf8len(s) + 1, SEEK_CUR);
+}
+
+PERF_TEST_F(GreekSeeking, End)
+{
+	const char* s = m_contents.c_str();
+	const char* e = s + m_contents.length();
+
+	utf8seek(e, s, (off_t)utf8len(s) - 1, SEEK_END);
+}
+
+PERF_TEST_F(GreekSeeking, IncrementalForwards)
+{
+	const char* s = m_contents.c_str();
+	const char* e = s + m_contents.length();
+	const char* c = s;
+	const char* n = c;
+
+	do
+	{
+		c = n;
+		n = utf8seek(c, s, 1, SEEK_CUR);
+	}
+	while (n != c && n != e);
+}
+
+PERF_TEST_F(GreekSeeking, IncrementalBackwards)
+{
+	const char* s = m_contents.c_str();
+	const char* e = s + m_contents.length();
+	const char* c = e;
+	const char* n = c;
+
+	do
+	{
+		c = n;
+		n = utf8seek(c, s, -1, SEEK_CUR);
+	}
+	while (n != c && n != s);
+}
