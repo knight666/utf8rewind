86785a6bbb2eac0183eec9ba755742312eddc787
Quinten Lansu <knight666+github@gmail.com>
2015-12-22 08:07:39 +0100

    Refactor tests for utf8iscategory.

--- source/tests/suite-utf8-iscategory.cpp
+++ source/tests/suite-utf8-iscategory.cpp
@@ -2,7 +2,7 @@
 
 #include "../helpers/helpers-strings.hpp"
 
-TEST(Utf8IsCategory, BasicLatinSingleLetterUppercase)
+TEST(Utf8IsCategory, BasicLatinSingle)
 {
 	const char* i = "L";
 	size_t is = strlen(i);
@@ -10,36 +10,36 @@ TEST(Utf8IsCategory, BasicLatinSingleLetterUppercase)
 	EXPECT_EQ(1, utf8iscategory(i, is, UTF8_CATEGORY_LETTER_UPPERCASE));
 }
 
-TEST(Utf8IsCategory, BasicLatinSingleLetterUppercaseMissing)
+TEST(Utf8IsCategory, BasicLatinSingleMissing)
 {
-	const char* i = "^";
+	const char* i = ")";
 	size_t is = strlen(i);
 
-	EXPECT_EQ(0, utf8iscategory(i, is, UTF8_CATEGORY_LETTER_UPPERCASE));
+	EXPECT_EQ(0, utf8iscategory(i, is, UTF8_CATEGORY_LETTER_LOWERCASE));
 }
 
-TEST(Utf8IsCategory, BasicLatinMultipleLetterUppercase)
+TEST(Utf8IsCategory, BasicLatinMultiple)
 {
-	const char* i = "KIP";
+	const char* i = "123";
 	size_t is = strlen(i);
 
-	EXPECT_EQ(3, utf8iscategory(i, is, UTF8_CATEGORY_LETTER_UPPERCASE));
+	EXPECT_EQ(3, utf8iscategory(i, is, UTF8_CATEGORY_NUMBER_DECIMAL));
 }
 
-TEST(Utf8IsCategory, BasicLatinMultipleLetterUppercaseMissingAtStart)
+TEST(Utf8IsCategory, BasicLatinMultipleMissingAtStart)
 {
-	const char* i = "1048";
+	const char* i = "(good)";
 	size_t is = strlen(i);
 
-	EXPECT_EQ(0, utf8iscategory(i, is, UTF8_CATEGORY_LETTER_UPPERCASE));
+	EXPECT_EQ(0, utf8iscategory(i, is, UTF8_CATEGORY_LETTER_LOWERCASE));
 }
 
-TEST(Utf8IsCategory, BasicLatinMultipleLetterUppercaseMissingBeforeEnd)
+TEST(Utf8IsCategory, BasicLatinMultipleMissingBeforeEnd)
 {
-	const char* i = "DIn1";
+	const char* i = "    %";
 	size_t is = strlen(i);
 
-	EXPECT_EQ(2, utf8iscategory(i, is, UTF8_CATEGORY_LETTER_UPPERCASE));
+	EXPECT_EQ(4, utf8iscategory(i, is, UTF8_CATEGORY_MARK_SPACING));
 }
 
 TEST(Utf8IsCategory, InvalidData)
