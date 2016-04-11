eee617cd43304785dad0dbe39ede17e724f0db68
Quinten Lansu <knight666@gmail.com>
2016-04-11 08:32:11 +0200

    Add tests for invalid code points.

--- source/tests/suite-utf8-cmp.cpp
+++ source/tests/suite-utf8-cmp.cpp
@@ -7,19 +7,39 @@ TEST(Utf8CompareCaseFolded, BasicLatinSingle)
 	EXPECT_EQ(0, utf8cmp("Y", "Y", 1, UTF8_COMPARE_CASE_FOLDED));
 }
 
+TEST(Utf8CompareCaseFolded, BasicLatinSingleInvalid)
+{
+	EXPECT_EQ(0, utf8cmp("\xFF", "\xFF", 1, UTF8_COMPARE_CASE_FOLDED));
+}
+
 TEST(Utf8CompareCaseFolded, BasicLatinSingleMismatch)
 {
 	EXPECT_EQ(1, utf8cmp("c", "b", 1, UTF8_COMPARE_CASE_FOLDED));
 }
 
-TEST(Utf8CompareCaseFolded, BasicLatinSingleMismatchCasingLeft)
+TEST(Utf8CompareCaseFolded, BasicLatinSingleMismatchCaseFoldLeft)
 {
 	EXPECT_EQ(0, utf8cmp("Z", "z", 1, UTF8_COMPARE_CASE_FOLDED));
 }
 
-TEST(Utf8CompareCaseFolded, BasicLatinSingleMismatchCasingRight)
+TEST(Utf8CompareCaseFolded, BasicLatinSingleMismatchCaseFoldRight)
 {
 	EXPECT_EQ(0, utf8cmp("d", "D", 1, UTF8_COMPARE_CASE_FOLDED));
+}
+
+TEST(Utf8CompareCaseFolded, BasicLatinSingleMismatchInvalidLeft)
+{
+	EXPECT_EQ(1, utf8cmp("\xFE", "B", 1, UTF8_COMPARE_CASE_FOLDED));
+}
+
+TEST(Utf8CompareCaseFolded, BasicLatinSingleMismatchInvalidRight)
+{
+	EXPECT_EQ(1, utf8cmp("1", "\xFF", 1, UTF8_COMPARE_CASE_FOLDED));
+}
+
+TEST(Utf8CompareCaseFolded, BasicLatinSingleMismatchInvalidBoth)
+{
+	EXPECT_EQ(1, utf8cmp("\xFE", "\xFF", 1, UTF8_COMPARE_CASE_FOLDED));
 }
 
 TEST(Utf8CompareCaseFolded, BasicLatinMultiple)
@@ -42,17 +62,17 @@ TEST(Utf8CompareCaseFolded, BasicLatinMultipleMismatchLast)
 	EXPECT_EQ(5, utf8cmp("magic", "magik", 5, UTF8_COMPARE_CASE_FOLDED));
 }
 
-TEST(Utf8CompareCaseFolded, BasicLatinMultipleMismatchCasing)
+TEST(Utf8CompareCaseFolded, BasicLatinMultipleMismatchCaseFold)
 {
 	EXPECT_EQ(0, utf8cmp("DroughT", "dROUGHt", 7, UTF8_COMPARE_CASE_FOLDED));
 }
 
-TEST(Utf8CompareCaseFolded, BasicLatinMultipleMismatchCasingFirst)
+TEST(Utf8CompareCaseFolded, BasicLatinMultipleMismatchCaseFoldFirst)
 {
 	EXPECT_EQ(0, utf8cmp("Business", "business", 8, UTF8_COMPARE_CASE_FOLDED));
 }
 
-TEST(Utf8CompareCaseFolded, BasicLatinMultipleMismatchCasingLast)
+TEST(Utf8CompareCaseFolded, BasicLatinMultipleMismatchCaseFoldLast)
 {
 	EXPECT_EQ(0, utf8cmp("lasT", "last", 8, UTF8_COMPARE_CASE_FOLDED));
 }
@@ -72,7 +92,17 @@ TEST(Utf8CompareCaseFolded, MultiByteSingleMismatch)
 	EXPECT_EQ(1, utf8cmp("\xEF\xBB\xA2", "\xEF\xB9\x91", 3, UTF8_COMPARE_CASE_FOLDED));
 }
 
-TEST(Utf8CompareCaseFolded, MultiByteSingleMismatchCasing)
+TEST(Utf8CompareCaseFolded, MultiByteSingleMismatchCaseFoldLeft)
 {
 	EXPECT_EQ(0, utf8cmp("\xE1\xBD\x80", "\xE1\xBD\x88", 3, UTF8_COMPARE_CASE_FOLDED));
+}
+
+TEST(Utf8CompareCaseFolded, MultiByteSingleMismatchCaseFoldRight)
+{
+	EXPECT_EQ(0, utf8cmp("\xE1\xBC\xA5", "\xE1\xBC\xAD", 3, UTF8_COMPARE_CASE_FOLDED));
+}
+
+TEST(Utf8CompareCaseFolded, MultiByteMultiple)
+{
+	EXPECT_EQ(0, utf8cmp("\xE1\xB6\xA6\xE2\x98\xB5\xE2\x8B\x9F\xE2\x99\x98", "\xE1\xB6\xA6\xE2\x98\xB5\xE2\x8B\x9F\xE2\x99\x98", 12, UTF8_COMPARE_CASE_FOLDED));
 }
