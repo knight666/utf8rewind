cb373d09fb569b3975f92e7e30238b6e2857c24d
Quinten Lansu <knight666@gmail.com>
2016-04-14 08:00:25 +0200

    Add line underneath mismatching code points.

--- source/helpers/helpers-compare.cpp
+++ source/helpers/helpers-compare.cpp
@@ -18,26 +18,52 @@ namespace helpers {
 
 			result << entryActual.expression << std::endl;
 
+			size_t offset = std::max<size_t>(entryActual.result, 1) - 1;
+
+			const char* left = entryExpected.left.c_str();
+			const char* leftCurrent = left + offset;
+			const char* leftNext = utf8seek(leftCurrent, entryExpected.left.length(), left, 1, SEEK_CUR);
+			size_t leftDifferenceStart = offset;
+			size_t leftDifferenceEnd = leftDifferenceStart + (leftNext - leftCurrent);
+			std::stringstream printLeft;
+			std::stringstream markLeft;
+
+			const char* right = entryExpected.right.c_str();
+			const char* rightCurrent = right + offset;
+			const char* rightNext = utf8seek(rightCurrent, entryExpected.right.length(), right, 1, SEEK_CUR);
+			size_t rightDifferenceStart = offset;
+			size_t rightDifferenceEnd = rightDifferenceStart + (rightNext - rightCurrent);
+			std::stringstream printRight;
+			std::stringstream markRight;
+
+			for (size_t i = 0; i < std::max(entryExpected.left.length(), entryExpected.right.length()); ++i)
+			{
+				if (i < entryExpected.left.length())
+				{
+					uint8_t left = (uint8_t)entryExpected.left[i];
+
+					printLeft << "\\x" << std::uppercase << std::setfill('0') << std::hex << std::setw(2) << ((unicode_t)left & 0xFF);
+					markLeft << ((i >= leftDifferenceStart && i <= leftDifferenceEnd) ? "~~~~" : "    ");
+				}
+
+				if (i < entryExpected.right.length())
+				{
+					uint8_t right = (uint8_t)entryExpected.right[i];
+
+					printRight << "\\x" << std::uppercase << std::setfill('0') << std::hex << std::setw(2) << ((unicode_t)right & 0xFF);
+					markRight << ((i >= rightDifferenceStart && i <= rightDifferenceEnd) ? "~~~~" : "    ");
+				}
+			}
+
+			result << "[Text]" << std::endl;
+			result << "Left:      \"" << printLeft.str() << "\"" << std::endl;
+			result << "            " << markLeft.str() << std::endl;
+			result << "Right:     \"" << printRight.str() << "\"" << std::endl;
+			result << "            " << markRight.str() << std::endl;
+
 			result << "[Result]" << std::endl;
 			result << "    Actual: " << entryActual.result << std::endl;
 			result << "  Expected: " << entryExpected.result << std::endl;
-
-			result << "[Text]" << std::endl;
-
-			std::string printLeft = printable(entryExpected.left);
-			std::string printRight = printable(entryExpected.right);
-
-			if (entryActual.result > 0)
-			{
-				const char* left = entryExpected.left.c_str();
-				const char* leftOffset = left + entryActual.result - 1;
-				const char* leftOffsetNext = utf8seek(leftOffset, entryExpected.left.length() - entryActual.result - 1, left, 1, SEEK_CUR);
-
-				int i = 0;
-			}
-
-			result << printLeft << std::endl;
-			result << printRight << std::endl;
 
 			return result;
 		}
