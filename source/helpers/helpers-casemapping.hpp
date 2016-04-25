c77d3a4f2f2c6405ab6d8d565a833ea4c6155256
Quinten Lansu <knight666+github@gmail.com>
2016-04-25 08:30:16 +0200

    Add locale parameter to helper functions.

--- source/helpers/helpers-casemapping.hpp
+++ source/helpers/helpers-casemapping.hpp
@@ -11,7 +11,7 @@
 
 #include "helpers-strings.hpp"
 
-#define EXPECT_CASEMAPPING_CODEPOINT_NUL_EQ(_codepoint, _name) { \
+#define EXPECT_CASEMAPPING_CODEPOINT_NUL_EQ(_codepoint, _name, _locale) { \
 	::helpers::CaseMappingEntry e; \
 	e.codepoint = _codepoint; \
 	e.lowercase = std::string(1, '\0'); \
@@ -20,13 +20,13 @@
 	e.name = _name; \
 	::helpers::CaseMappingEntry a; \
 	std::string text = ::helpers::utf8(_codepoint); \
-	a.lowercase = ::helpers::lowercase(text); \
-	a.uppercase = ::helpers::uppercase(text); \
-	a.titlecase = ::helpers::titlecase(text); \
+	a.lowercase = ::helpers::lowercase(text, (_locale)); \
+	a.uppercase = ::helpers::uppercase(text, (_locale)); \
+	a.titlecase = ::helpers::titlecase(text, (_locale)); \
 	EXPECT_PRED_FORMAT2(::helpers::CompareCodepoint, e, a); \
 }
 
-#define EXPECT_CASEMAPPING_CODEPOINT_EQ(_codepoint, _lowercase, _uppercase, _titlecase, _name) { \
+#define EXPECT_CASEMAPPING_CODEPOINT_EQ(_codepoint, _lowercase, _uppercase, _titlecase, _name, _locale) { \
 	::helpers::CaseMappingEntry e; \
 	e.codepoint = _codepoint; \
 	e.lowercase = _lowercase; \
@@ -35,47 +35,47 @@
 	e.name = _name; \
 	::helpers::CaseMappingEntry a; \
 	std::string text = ::helpers::utf8(_codepoint); \
-	a.lowercase = ::helpers::lowercase(text); \
-	a.uppercase = ::helpers::uppercase(text); \
-	a.titlecase = ::helpers::titlecase(text); \
+	a.lowercase = ::helpers::lowercase(text, (_locale)); \
+	a.uppercase = ::helpers::uppercase(text, (_locale)); \
+	a.titlecase = ::helpers::titlecase(text, (_locale)); \
 	EXPECT_PRED_FORMAT2(::helpers::CompareCodepoint, e, a); \
 }
 
-#define EXPECT_CASEMAPPING_EQ(_input, _lowercase, _uppercase, _titlecase) { \
+#define EXPECT_CASEMAPPING_EQ(_input, _lowercase, _uppercase, _titlecase, _locale) { \
 	::helpers::CaseMappingEntry e; \
 	e.lowercase = _lowercase; \
 	e.uppercase = _uppercase; \
 	e.titlecase = _titlecase; \
 	::helpers::CaseMappingEntry a; \
 	a.input = _input; \
-	a.lowercase = ::helpers::lowercase(_input); \
-	a.uppercase = ::helpers::uppercase(_input); \
-	a.titlecase = ::helpers::titlecase(_input); \
+	a.lowercase = ::helpers::lowercase((_input), (_locale)); \
+	a.uppercase = ::helpers::uppercase((_input), (_locale)); \
+	a.titlecase = ::helpers::titlecase((_input), (_locale)); \
 	EXPECT_PRED_FORMAT2(::helpers::CompareCaseMapping, e, a); \
 }
 
 #if UTF8_VERSION_GUARD(1, 4, 0)
-	#define EXPECT_CASEFOLDING_EQ(_codepoint, _folded, _name) { \
+	#define EXPECT_CASEFOLDING_EQ(_codepoint, _folded, _name, _locale) { \
 		::helpers::CaseFoldingEntry e; \
 		e.codePoint = _codepoint; \
 		e.folded = _folded; \
 		e.name = _name; \
 		::helpers::CaseFoldingEntry a; \
-		a.folded = ::helpers::casefold(::helpers::utf8(_codepoint)); \
+		a.folded = ::helpers::casefold(::helpers::utf8(_codepoint), (_locale)); \
 		EXPECT_PRED_FORMAT2(::helpers::CompareCaseFolding, e, a); \
 	}
 #endif
 
 namespace helpers {
 
-	std::string uppercase(const std::string& text);
+	std::string uppercase(const std::string& text, size_t locale);
 
-	std::string lowercase(const std::string& text);
+	std::string lowercase(const std::string& text, size_t locale);
 
-	std::string titlecase(const std::string& text);
+	std::string titlecase(const std::string& text, size_t locale);
 
 #if UTF8_VERSION_GUARD(1, 4, 0)
-	std::string casefold(const std::string& text);
+	std::string casefold(const std::string& text, size_t locale);
 #endif
 
 	struct CaseMappingEntry
