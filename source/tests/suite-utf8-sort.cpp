c775897c369d68fdbec3f00b312e4c5559298bb5
Quinten Lansu <knight666@gmail.com>
2018-05-16 13:44:37 +0200

    Add dictionary test.

--- source/tests/suite-utf8-sort.cpp
+++ source/tests/suite-utf8-sort.cpp
@@ -71,3 +71,29 @@ TEST(Utf8Sort, CaseDifference)
 	EXPECT_EQ(-1, utf8sort(l, ls, r, rs, UTF8_LOCALE_DEFAULT, &errors));
 	EXPECT_ERROREQ(UTF8_ERR_NONE, errors);
 }
+
+TEST(Utf8Sort, Dictionary)
+{
+	std::vector<std::string> dictionary = {
+		"CAT",
+		"CAVE",
+		"Cat",
+		"cAT",
+		"cAt",
+		"car",
+		"caT",
+		"cat"
+	};
+	std::sort(dictionary.begin(), dictionary.end(), [](const std::string& left, const std::string& right) {
+		return utf8sort(left.c_str(), left.length(), right.c_str(), right.length(), UTF8_LOCALE_DEFAULT, nullptr) == -1;
+	});
+
+	EXPECT_STREQ("car", dictionary[0].c_str());
+	EXPECT_STREQ("cat", dictionary[1].c_str());
+	EXPECT_STREQ("caT", dictionary[2].c_str());
+	EXPECT_STREQ("cAt", dictionary[3].c_str());
+	EXPECT_STREQ("cAT", dictionary[4].c_str());
+	EXPECT_STREQ("Cat", dictionary[5].c_str());
+	EXPECT_STREQ("CAT", dictionary[6].c_str());
+	EXPECT_STREQ("CAVE", dictionary[7].c_str());
+}
