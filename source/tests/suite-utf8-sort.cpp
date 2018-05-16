fa834a4c557404cbc77c24296fc9c8cd67a61f75
Quinten Lansu <knight666@gmail.com>
2018-05-16 12:55:14 +0200

    Compare lengths of equally weighted strings.

--- source/tests/suite-utf8-sort.cpp
+++ source/tests/suite-utf8-sort.cpp
@@ -1,5 +1,17 @@
 #include "tests-base.hpp"
 
+TEST(Utf8Sort, Same)
+{
+	const char* l = "amazing";
+	size_t ls = strlen(l);
+	const char* r = "amazing";
+	size_t rs = strlen(r);
+	int32_t errors = -4;
+
+	EXPECT_EQ(0, utf8sort(l, ls, r, rs, UTF8_LOCALE_DEFAULT, &errors));
+	EXPECT_ERROREQ(UTF8_ERR_NONE, errors);
+}
+
 TEST(Utf8Sort, Lesser)
 {
 	const char* l = "book";
@@ -21,5 +33,29 @@ TEST(Utf8Sort, Greater)
 	int32_t errors = 33;
 
 	EXPECT_EQ(1, utf8sort(l, ls, r, rs, UTF8_LOCALE_DEFAULT, &errors));
+	EXPECT_ERROREQ(UTF8_ERR_NONE, errors);
+}
+
+TEST(Utf8Sort, Longer)
+{
+	const char* l = "bambi";
+	size_t ls = strlen(l);
+	const char* r = "bamb";
+	size_t rs = strlen(r);
+	int32_t errors = 3;
+
+	EXPECT_EQ(1, utf8sort(l, ls, r, rs, UTF8_LOCALE_DEFAULT, &errors));
+	EXPECT_ERROREQ(UTF8_ERR_NONE, errors);
+}
+
+TEST(Utf8Sort, Shorter)
+{
+	const char* l = "ha";
+	size_t ls = strlen(l);
+	const char* r = "ham";
+	size_t rs = strlen(r);
+	int32_t errors = 3;
+
+	EXPECT_EQ(-1, utf8sort(l, ls, r, rs, UTF8_LOCALE_DEFAULT, &errors));
 	EXPECT_ERROREQ(UTF8_ERR_NONE, errors);
 }
