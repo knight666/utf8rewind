75b01dfe13596d842e24c82446d8fb8c08a840f4
Quinten Lansu <knight666+github@gmail.com>
2018-05-16 09:31:04 +0200

    Add tests for sort function.

---
+++ source/tests/suite-utf8-sort.cpp
@@ -0,0 +1,25 @@
+#include "tests-base.hpp"
+
+TEST(Utf8Sort, Lesser)
+{
+	const char* l = "book";
+	size_t ls = strlen(l);
+	const char* r = "rack";
+	size_t rs = strlen(r);
+	int32_t errors = 1877;
+
+	EXPECT_EQ(-1, utf8sort(l, ls, r, rs, UTF8_LOCALE_DEFAULT, &errors));
+	EXPECT_ERROREQ(UTF8_ERR_NONE, errors);
+}
+
+TEST(Utf8Sort, Greater)
+{
+	const char* l = "medical";
+	size_t ls = strlen(l);
+	const char* r = "branch";
+	size_t rs = strlen(r);
+	int32_t errors = 33;
+
+	EXPECT_EQ(-1, utf8sort(l, ls, r, rs, UTF8_LOCALE_DEFAULT, &errors));
+	EXPECT_ERROREQ(UTF8_ERR_NONE, errors);
+}
