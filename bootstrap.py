8d1fa6f91f729d6014d852760b3a3af0c8c61a46
Quinten Lansu <knight666+github@gmail.com>
2018-05-17 10:16:06 +0200

    Generate solution from bootstrap.

--- bootstrap.py
+++ bootstrap.py
@@ -3,18 +3,34 @@ if sys.version_info < (3, 0):
 	sys.stdout.write("Bootstrap requires Python 3.x.\n")
 	sys.exit(1)
 
+import os.path
+import subprocess
 import build.indent_print as indent_print
 
+def RunCommand(args, expected = 0, vcVars = False):
+	if vcVars:
+		vcvars_path = os.path.abspath(os.getenv('VS140COMNTOOLS') + '..\\..\\VC\\vcvarsall.bat')
+		args = [ vcvars_path, 'amd64', '&&' ] + args
+
+	with subprocess.Popen(args, shell = True, stdout = subprocess.PIPE, stderr = subprocess.STDOUT) as proc:
+		for line in iter(proc.stdout.readline, b''):
+			print('\t' + str(line.rstrip()))
+		proc.communicate()
+		print('')
+		return proc.returncode == expected
+
+def GenerateSolution():
+	print('Generating solution with GYP...')
+
+	if not RunCommand(['tools\gyp\gyp', '--depth=.', 'utf8rewind.gyp']):
+		return False
+
+	print('SUCCEEDED')
+
+	return True
+
 if __name__ == '__main__':
-	print('Strapping boots...')
-
-	indent_print.push()
-	print('blah')
-	indent_print.pop()
-
-	print('more')
 	with indent_print.guard():
-		print('stuff')
-	print('and things')
+		GenerateSolution()
 
 	exit(0)
